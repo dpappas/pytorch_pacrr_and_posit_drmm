5aff6a3f14802ff8b1062751bdea274efaf91ec8
dpappas <Zrb6.NCC>
2018-06-22 14:34:49 +0300

    message

---
+++ example_texts_to_batch.py
@@ -0,0 +1,28 @@
+
+
+from bioasq_utils import get_sents
+from pprint import pprint
+import json
+
+fpath   = '/home/dpappas/bioasq_ir_data/BioASQ-trainingDataset6b.json'
+data    = json.load(open(fpath,'r'))
+
+pprint(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
