06531293ecbfcb3ffd22d21c739d941a82a02e8a
dpappas <Zrb6.NCC>
2018-06-22 14:38:34 +0300

    message

--- example_texts_to_batch.py
+++ example_texts_to_batch.py
@@ -7,9 +7,15 @@ import json
 fpath   = '/home/dpappas/bioasq_ir_data/BioASQ-trainingDataset6b.json'
 data    = json.load(open(fpath,'r'))
 
-pprint(data)
+# pprint(data)
 
+pprint(data['questions'][0].keys())
+pprint(data['questions'][0])
 
+bod     = data['questions'][0]['body']
+print bod
+doc_ids = [ t.split('/')[-1] for t in data['questions'][0]['documents'] ]
+print doc_ids
 
 
 
