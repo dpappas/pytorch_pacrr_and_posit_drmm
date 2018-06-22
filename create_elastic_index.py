3f461b9864c51a329b97839a1e1503bc7413283f
dpappas <Zrb6.NCC>
2018-06-22 15:28:24 +0300

    message

--- create_elastic_index.py
+++ create_elastic_index.py
@@ -16,46 +16,41 @@ mapping = {
         map:{
             "properties":{
                 "AbstractText" : {
-                    "type"      : "string",
-                    "analyzer"  : "standard",
+                    "type": "text",
+                    "analyzer": "standard",
                 },
                 'ArticleTitle': {
-                    "type": "string",
+                    "type": "text",
                     "analyzer": "standard",
                 },
                 'Title': {
-                    "type": "string",
+                    "type": "text",
                     "analyzer": "standard",
                 },
                 'ArticleDate':{
                     "type"      : "date",
                     "format"    : "yyy-MM-dd HH:mm:ss||yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||yyyy/MM/dd||dd/MM/yyyy||epoch_millis||EE MMM d HH:mm:ss Z yyyy"
                 },
-
-                "filename": {
-                    "type": "string",
-                    "index": "not_analyzed",
+                'Keywords' : {
+                    "type": "text",
+                    "analyzer": "standard"
                 },
-                'date_type': {
-                    "type": "string",
-                    "index": "not_analyzed",
-                },
-                'article_ids' : {
-                    "type"      : "string",
-                    "index"  : "not_analyzed",
-                },
-                'section_title': {
-                    "type"      : "string",
-                    "analyzer"  : "standard",
-                },
-                'journal_title': {
-                    "type"      : "string",
-                    "analyzer"  : "standard",
-                },
-                'article_keywords' : {
-                    "type"      : "string",
-                    "analyzer"  : "standard",
-                },
+                "MeshHeadings": {
+                    "properties": {
+                        "UI": {
+                            "type": "keyword"
+                        },
+                        "name": {
+                            "type"      : "text",
+                            "analyzer"  : "standard",
+                            "fields": {
+                                "raw": {
+                                    "type":  "keyword"
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
