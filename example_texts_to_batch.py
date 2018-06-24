fa06b8428635448f5b0582692f3459e7f94d64e2
dpappas <Zrb6.NCC>
2018-06-24 12:31:02 +0300

    message

--- example_texts_to_batch.py
+++ example_texts_to_batch.py
@@ -10,14 +10,6 @@ data    = json.load(open(fpath,'r'))
 # pprint(data)
 sw = set(stopwords.words('english'))
 
-# pprint(data['questions'][0].keys())
-# pprint(data['questions'][0])
-#
-# bod     = data['questions'][0]['body']
-# print bod
-# doc_ids = [ t.split('/')[-1] for t in data['questions'][0]['documents'] ]
-# print doc_ids
-
 for snip in data['questions'][0]['snippets']:
     # pprint(snip)
     pmid = snip['document'].split('/')[-1]
@@ -30,6 +22,16 @@ for snip in data['questions'][0]['snippets']:
 
 
 '''
+
+# pprint(data['questions'][0].keys())
+# pprint(data['questions'][0])
+#
+# bod     = data['questions'][0]['body']
+# print bod
+# doc_ids = [ t.split('/')[-1] for t in data['questions'][0]['documents'] ]
+# print doc_ids
+
+
 laptop:
 /home/dpappas/ELK/elasticsearch-6.2.4/bin/elasticsearch
 /home/dpappas/ELK/kibana-6.2.4-linux-x86_64/bin/kibana
