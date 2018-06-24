8d9a6544d3e38b444266e6ecfda124d1d182fc29
dpappas <Zrb6.NCC>
2018-06-24 12:27:43 +0300

    message

--- example_texts_to_batch.py
+++ example_texts_to_batch.py
@@ -10,13 +10,13 @@ data    = json.load(open(fpath,'r'))
 # pprint(data)
 sw = set(stopwords.words('english'))
 
-pprint(data['questions'][0].keys())
-pprint(data['questions'][0])
-
-bod     = data['questions'][0]['body']
-print bod
-doc_ids = [ t.split('/')[-1] for t in data['questions'][0]['documents'] ]
-print doc_ids
+# pprint(data['questions'][0].keys())
+# pprint(data['questions'][0])
+#
+# bod     = data['questions'][0]['body']
+# print bod
+# doc_ids = [ t.split('/')[-1] for t in data['questions'][0]['documents'] ]
+# print doc_ids
 
 for snip in data['questions'][0]['snippets']:
     # pprint(snip)
