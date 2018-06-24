62ccde603af5c968af0d92a9c7701027e9dc06b4
dpappas <Zrb6.NCC>
2018-06-24 20:38:02 +0300

    message

--- retrieve_relevant_docs_from_elastic.py
+++ retrieve_relevant_docs_from_elastic.py
@@ -7,10 +7,15 @@ from pprint import pprint
 # fpath = '/home/DATA/Biomedical/bioasq6/bioasq6_data/bioasq6_bm25_top100/bioasq6_bm25_top100.train.pkl'
 # fpath = '/home/DATA/Biomedical/bioasq6/bioasq6_data/bioasq6_bm25_top100/bioasq6_bm25_docset_top100.full_train.pkl'
 
-fpath = '/home/DATA/Biomedical/bioasq6/bioasq6_data/bioasq6_bm25_top100/bioasq6_bm25_top100.dev.pkl'
-data = pickle.load(open(fpath,'rb'))
-pprint(data['queries'][0])
+fpath   = '/home/DATA/Biomedical/bioasq6/bioasq6_data/bioasq6_bm25_top100/bioasq6_bm25_top100.dev.pkl'
+data    = pickle.load(open(fpath,'rb'))
+d1      = data['queries'][0]
+pprint(d1)
 
 fpath = '/home/DATA/Biomedical/bioasq6/bioasq6_data/bioasq6_bm25_top100/bioasq6_bm25_docset_top100.dev.pkl'
 data = pickle.load(open(fpath,'rb'))
 pprint(data['21177106'])
+
+from bioasq_utils import get_sents, similar
+get_sents(data['24787386']['abstractText'])
+
