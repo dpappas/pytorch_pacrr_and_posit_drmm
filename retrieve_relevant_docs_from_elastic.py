66b1cb15d421eb1b34404eb847002cc1c9c74aab
dpappas <Zrb6.NCC>
2018-06-26 12:31:24 +0300

    message

--- retrieve_relevant_docs_from_elastic.py
+++ retrieve_relevant_docs_from_elastic.py
@@ -19,3 +19,28 @@ pprint(data['21177106'])
 from bioasq_utils import get_sents, similar
 get_sents(data['24787386']['abstractText'])
 
+f2 = '/home/DATA/Biomedical/bioasq6/bioasq6_data/'
+f3 = '/home/DATA/Biomedical/bioasq6/bioasq6_data/'
+
+
+
+import json
+from pprint import pprint
+f1      = '/home/DATA/Biomedical/bioasq6/bioasq6_data/BioASQ-trainingDataset6b.json'
+data    = json.load(open(f1,'r'))
+
+# query_text
+
+ddd = {}
+for quest in data['questions']:
+    # print(quest['body'])
+    if('snippets' in quest):
+        for sn in quest['snippets']:
+            pmid    = sn['document'].split('/')[-1]
+            ttt     = sn['text']
+
+
+
+
+
+
