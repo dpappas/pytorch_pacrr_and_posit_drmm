728b49ee8805e7d56fba6c1cd987623db2339ac8
dpappas <Zrb6.NCC>
2018-06-27 01:47:37 +0300

    message

--- create_batches_for_joint_task.py
+++ create_batches_for_joint_task.py
@@ -59,20 +59,20 @@ def get_index(token, t2i):
     except KeyError:
         return t2i['UNKN']
 
-def get_sim_matrix_one(qtoks, stoks, max_len_of_sents):
-    sm = np.zeros((max_len_of_sents,max_len_of_sents))
+def get_sim_matrix_one(qtoks, stoks, max_len_of_quests, max_len_of_sents):
+    sm = np.zeros((max_len_of_quests,max_len_of_sents))
     for i in range(len(qtoks)):
         for j in range(len(stoks)):
             if(qtoks[i] == stoks[j]):
                 sm[i,j] = 1.
     return sm
 
-def get_sim_matrix(item, max_len_of_sents, max_nof_sents):
+def get_sim_matrix(item, max_len_of_quests, max_len_of_sents, max_nof_sents):
     qtoks       = bioclean(item['question'])
     sim_matrix  = []
     for s in item['all_sents']:
         stoks   = bioclean(s)
-        sim_matrix.append(get_sim_matrix_one(qtoks, stoks, max_len_of_sents))
+        sim_matrix.append(get_sim_matrix_one(qtoks, stoks, max_len_of_quests, max_len_of_sents))
     sim_matrix.extend((max_nof_sents - len(sim_matrix)) * [np.zeros((max_len_of_sents, max_len_of_sents))])
     sim_matrix = np.stack(sim_matrix,0)
     return sim_matrix
@@ -113,10 +113,10 @@ for item in tqdm(all_data):
         #
         sent_y      = np.array(item['sent_sim_vec'] + ((max_nof_sents - len(item['sent_sim_vec'])) * [0]))
         #
-        quest_inds  = quest_inds + ([0] * (max_len_of_sents - len(quest_inds)))
+        quest_inds  = quest_inds + ([0] * (max_len_of_quests - len(quest_inds)))
         quest_inds  = np.array(quest_inds)
         #
-        all_sims = get_sim_matrix(item, max_len_of_sents, max_nof_sents)
+        all_sims = get_sim_matrix(item, max_len_of_quests, max_len_of_sents, max_nof_sents)
         #
         qi.append(quest_inds)
         si.append(sent_inds)
