bbb5019131f5b1a7cf9e9ea62f6be7113345dfff
dpappas <Zrb6.NCC>
2018-06-27 00:26:41 +0300

    message

--- create_batches_for_joint_task.py
+++ create_batches_for_joint_task.py
@@ -5,6 +5,7 @@ import re
 import gensim
 import operator
 import numpy as np
+from tqdm import tqdm
 import cPickle as pickle
 from pprint import pprint
 
@@ -57,6 +58,24 @@ def get_index(token, t2i):
     except KeyError:
         return t2i['UNKN']
 
+def get_sim_matrix_one(qtoks, stoks, max_len_of_sents):
+    sm = np.zeros((max_len_of_sents,max_len_of_sents))
+    for i in range(len(qtoks)):
+        for j in range(len(stoks)):
+            if(qtoks[i] == stoks[j]):
+                sm[i,j] = 1.
+    return sm
+
+def get_sim_matrix(item, max_len_of_sents, max_nof_sents):
+    qtoks       = bioclean(item['question'])
+    sim_matrix  = []
+    for s in item['all_sents']:
+        stoks   = bioclean(s)
+        sim_matrix.append(get_sim_matrix_one(qtoks, stoks, max_len_of_sents))
+    sim_matrix.extend((max_nof_sents - len(sim_matrix)) * [np.zeros((max_len_of_sents, max_len_of_sents))])
+    sim_matrix = np.stack(sim_matrix,0)
+    return sim_matrix
+
 w2v_path = '/home/DATA/Biomedical/bioasq6/bioasq6_data/word_embeddings/pubmed2018_w2v_200D.bin'
 t2i, i2t, matrix = load_w2v_embs(w2v_path)
 
@@ -67,7 +86,33 @@ max_nof_sents       = max([len(item['all_sents']) for item in all_data])
 max_len_of_sents    = 400 # basika einai 355 sta train
 non_sent            = [0] * max_len_of_sents
 qi, dy, si, sy      = [], [], [], []
-for item in all_data:
+for item in tqdm(all_data):
+    indices     = [[ get_index(token, t2i) for token in bioclean(s)] for s in item['all_sents']]
+    indices     = [s + ([0] * (max_len_of_sents - len(s))) for s in indices]
+    indices     = indices + ((max_nof_sents - len(indices)) * [non_sent])
+    sent_inds   = np.array(indices)
+    #
+    sent_y      = np.array(item['sent_sim_vec'] + ((max_nof_sents - len(item['sent_sim_vec'])) * [0]))
+    #
+    quest_inds  = [ get_index(token, t2i) for token in bioclean(item['question'])]
+    quest_inds  = quest_inds + ([0] * (max_len_of_sents - len(quest_inds)))
+    quest_inds  = np.array(quest_inds)
+    #
+    all_sims = get_sim_matrix(item, max_len_of_sents, max_nof_sents)
+    #
+    # print(all_sims.shape)
+    # print(sent_inds.shape)
+    # print(sent_y.shape)
+    # print(quest_inds.shape)
+    # print(all_sims.sum())
+    # print(20 * '-')
+    #
+
+
+'''
+non_sent            = [0] * max_len_of_sents
+qi, dy, si, sy      = [], [], [], []
+for item in tqdm(all_data):
     indices     = [[ get_index(token, t2i) for token in bioclean(s)] for s in item['all_sents']]
     indices     = [s + ([0] * (max_len_of_sents - len(s))) for s in indices]
     indices     = indices + ((max_nof_sents - len(indices)) * [non_sent])
@@ -110,7 +155,7 @@ pickle.dump(
     open('entire_train_joint_dataset.p', 'wb')
 )
 
-
+'''
 
 
 
