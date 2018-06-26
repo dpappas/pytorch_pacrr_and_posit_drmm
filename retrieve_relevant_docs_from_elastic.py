a587a07b4bd4cc0922292d37e77f48b9df40ab9a
dpappas <Zrb6.NCC>
2018-06-26 13:46:21 +0300

    message

--- retrieve_relevant_docs_from_elastic.py
+++ retrieve_relevant_docs_from_elastic.py
@@ -3,6 +3,113 @@
 import json
 import cPickle as pickle
 from pprint import pprint
+import re
+import operator
+from nltk.tokenize import sent_tokenize
+from gensim.models import KeyedVectors
+from difflib import SequenceMatcher
+
+UNK_TOKEN = '*UNK*'
+
+bioclean = lambda t: re.sub('[.,?;*!%^&_+():-\[\]{}]', '', t.replace('"', '').replace('/', '').replace('\\', '').replace("'", '').strip().lower()).split()
+
+def similar(a, b):
+    return max(
+        [
+            SequenceMatcher(None, a, b).ratio(),
+            SequenceMatcher(None, b, a).ratio()
+        ]
+    )
+
+def many_similar(one_sent, many_sents):
+    return max(
+        [
+            similar(one_sent, s)
+            for s in many_sents
+        ]
+    )
+
+def map_term2ind(w2v_path):
+    word_vectors = KeyedVectors.load_word2vec_format(w2v_path, binary=True)
+    vocabulary  = sorted(list(word_vectors.vocab.keys()))
+    term2ind = dict([t[::-1] for t in enumerate(vocabulary, start=1)])
+    term2ind[UNK_TOKEN] = max(term2ind.items(), key=operator.itemgetter(1))[1] + 1	# Index of *UNK* token
+    print('Size of voc: {0}'.format(len(vocabulary)))
+    print('Unknown terms\'s id: {0}'.format(term2ind['*UNK*']))
+    return term2ind
+
+def set_unk_tokens(tokens, term2ind):
+    clear_tokens = []
+    for t in tokens:
+        if t in term2ind:
+            clear_tokens.append(t)
+        else:
+            clear_tokens.append(UNK_TOKEN)
+    return clear_tokens
+
+def first_alpha_is_upper(sent):
+    specials = [
+        '__EU__','__SU__','__EMS__','__SMS__','__SI__',
+        '__ESB','__SSB__','__EB__','__SB__','__EI__',
+        '__EA__','__SA__','__SQ__','__EQ__','__EXTLINK',
+        '__XREF','__URI', '__EMAIL','__ARRAY','__TABLE',
+        '__FIG','__AWID','__FUNDS'
+    ]
+    for special in specials:
+        sent = sent.replace(special,'')
+    for c in sent:
+        if(c.isalpha()):
+            if(c.isupper()):
+                return True
+            else:
+                return False
+    return False
+
+def ends_with_special(sent):
+    sent = sent.lower()
+    ind = [item.end() for item in re.finditer('[\W\s]sp.|[\W\s]nos.|[\W\s]figs.|[\W\s]sp.[\W\s]no.|[\W\s][vols.|[\W\s]cv.|[\W\s]fig.|[\W\s]e.g.|[\W\s]et[\W\s]al.|[\W\s]i.e.|[\W\s]p.p.m.|[\W\s]cf.|[\W\s]n.a.', sent)]
+    if(len(ind)==0):
+        return False
+    else:
+        ind = max(ind)
+        if (len(sent) == ind):
+            return True
+        else:
+            return False
+
+def split_sentences(text):
+    sents = [l.strip() for l in sent_tokenize(text)]
+    ret = []
+    i = 0
+    while (i < len(sents)):
+        sent = sents[i]
+        while (
+            ((i + 1) < len(sents)) and
+            (
+                ends_with_special(sent) or
+                not first_alpha_is_upper(sents[i+1].strip())
+                # sent[-5:].count('.') > 1       or
+                # sents[i+1][:10].count('.')>1   or
+                # len(sent.split()) < 2          or
+                # len(sents[i+1].split()) < 2
+            )
+        ):
+            sent += ' ' + sents[i + 1]
+            i += 1
+        ret.append(sent.replace('\n',' ').strip())
+        i += 1
+    return ret
+
+def get_sents(ntext):
+    sents = []
+    for subtext in ntext.split('\n'):
+        subtext = re.sub( '\s+', ' ', subtext.replace('\n',' ') ).strip()
+        if (len(subtext) > 0):
+            ss = split_sentences(subtext)
+            sents.extend([ s for s in ss if(len(s.strip())>0)])
+    if(len(sents[-1]) == 0 ):
+        sents = sents[:-1]
+    return sents
 
 def preprocess_bioasq_data(bioasq_data_path):
     data = json.load(open(bioasq_data_path, 'r'))
@@ -35,22 +142,36 @@ bm25_scores         = pickle.load(open(bm25_scores_path, 'rb'))
 
 for quer in bm25_scores['queries']:
     for retr in quer['retrieved_documents']:
-        print(retr['doc_id'])
-
-        # if(retr['is_relevant']):
-        #     pass
+        doc_id      = retr['doc_id']
+        doc_title   = get_sents(all_abs[doc_id]['title'])
+        doc_text    = get_sents(all_abs[doc_id]['abstractText'])
+        all_sents   = doc_title+doc_text
+        if(retr['is_relevant']):
+            relevant_snippets = []
+            for rt in ddd[quer['query_text']][doc_id]:
+                relevant_snippets.extend(get_sents(rt))
+            for s in all_sents:
+                for r in relevant_snippets:
+                    similarity = similar(s,r)
+                    if(similarity >0.7):
+                        print similarity
+                        print doc_id
+                        print s
+                        print r
+                        print 20 * '-'
         # print(retr['bm25_score'])
         # print(retr['norm_bm25_score'])
         # print(retr['is_relevant'])
     break
 
+# get_sents(all_abs['12239580']['abstractText']
 
 
-from bioasq_utils import get_sents, similar
-get_sents(data['24787386']['abstractText'])
-
-f2 = '/home/DATA/Biomedical/bioasq6/bioasq6_data/'
-f3 = '/home/DATA/Biomedical/bioasq6/bioasq6_data/'
+# from bioasq_utils import get_sents, similar
+# get_sents(data['24787386']['abstractText'])
+#
+# f2 = '/home/DATA/Biomedical/bioasq6/bioasq6_data/'
+# f3 = '/home/DATA/Biomedical/bioasq6/bioasq6_data/'
 
 
 
