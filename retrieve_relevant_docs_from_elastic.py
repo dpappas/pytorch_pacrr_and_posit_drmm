d8a943b4493642e45e37282210d564a7207a686e
dpappas <Zrb6.NCC>
2018-06-26 13:18:31 +0300

    message

--- retrieve_relevant_docs_from_elastic.py
+++ retrieve_relevant_docs_from_elastic.py
@@ -4,39 +4,34 @@ import json
 import cPickle as pickle
 from pprint import pprint
 
-bioasq_data_path    = '/home/DATA/Biomedical/bioasq6/bioasq6_data/BioASQ-trainingDataset6b.json'
-data                = json.load(open(bioasq_data_path, 'r'))
-ddd                 = {}
-for quest in data['questions']:
-    if('snippets' in quest):
-        for sn in quest['snippets']:
-            pmid    = sn['document'].split('/')[-1]
-            ttt     = sn['text'].strip()
-            bod     = quest['body'].strip()
-            if(bod not in ddd):
-                ddd[bod] = {}
-            if(pmid not in ddd[bod]):
-                ddd[bod][pmid] = [ttt]
-            else:
-                ddd[bod][pmid].append(ttt)
+def preprocess_bioasq_data(bioasq_data_path):
+    data = json.load(open(bioasq_data_path, 'r'))
+    ddd = {}
+    for quest in data['questions']:
+        if ('snippets' in quest):
+            for sn in quest['snippets']:
+                pmid = sn['document'].split('/')[-1]
+                ttt = sn['text'].strip()
+                bod = quest['body'].strip()
+                if (bod not in ddd):
+                    ddd[bod] = {}
+                if (pmid not in ddd[bod]):
+                    ddd[bod][pmid] = [ttt]
+                else:
+                    ddd[bod][pmid].append(ttt)
+    return ddd
 
-abs_path    = '/home/DATA/Biomedical/bioasq6/bioasq6_data/bioasq6_bm25_top100/bioasq6_bm25_docset_top100.train.pkl'
+bioasq_data_path    = '/home/DATA/Biomedical/bioasq6/bioasq6_data/BioASQ-trainingDataset6b.json'
+ddd                 = preprocess_bioasq_data(bioasq_data_path)
+
+abs_path    = '/home/DATA/Biomedical/document_ranking/bioasq_data/bioasq_bm25_docset_top100.train.pkl'
 all_abs     = pickle.load(open(abs_path,'rb'))
 
-
-
-# fpath = '/home/DATA/Biomedical/bioasq6/bioasq6_data/bioasq6_bm25_top100/bioasq6_bm25_top100.full_train.pkl'
-# fpath = '/home/DATA/Biomedical/bioasq6/bioasq6_data/bioasq6_bm25_top100/bioasq6_bm25_docset_top100.train.pkl'
-# fpath = '/home/DATA/Biomedical/bioasq6/bioasq6_data/bioasq6_bm25_top100/bioasq6_bm25_top100.train.pkl'
-# fpath = '/home/DATA/Biomedical/bioasq6/bioasq6_data/bioasq6_bm25_top100/bioasq6_bm25_docset_top100.full_train.pkl'
-# fpath   = '/home/DATA/Biomedical/bioasq6/bioasq6_data/bioasq6_bm25_top100/bioasq6_bm25_top100.dev.pkl'
-# fpath = '/home/DATA/Biomedical/bioasq6/bioasq6_data/bioasq6_bm25_top100/bioasq6_bm25_docset_top100.dev.pkl'
+fpath = '/home/DATA/Biomedical/document_ranking/bioasq_data/bioasq_bm25_top100.train.pkl'
 
 d1      = data['queries'][0]
 pprint(d1)
 
-data = pickle.load(open(fpath,'rb'))
-pprint(data['21177106'])
 
 from bioasq_utils import get_sents, similar
 get_sents(data['24787386']['abstractText'])
