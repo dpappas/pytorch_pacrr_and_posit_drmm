892a6a23248be96e525001a17581523615733dc3
dpappas <Zrb6.NCC>
2018-06-29 15:32:37 +0300

    message

--- retrieve_relevant_docs_from_elastic.py
+++ retrieve_relevant_docs_from_elastic.py
@@ -3,46 +3,60 @@ from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk, scan
 from pprint import pprint
 from nltk.corpus import stopwords
+import json
 import re
 
-bioclean = lambda t: re.sub('[.,?;*!%^&_+():-\[\]{}]', '', t.replace('"', '').replace('/', '').replace('\\', '').replace("'", '').strip().lower()).split()
-
+stopWords   = set(stopwords.words('english'))
 index   = 'pubmed_abstracts_index_0_1'
 map     = "pubmed_abstracts_mapping_0_1"
 es      = Elasticsearch(['localhost:9200'], verify_certs=True, timeout=300, max_retries=10, retry_on_timeout=True)
 
-stopWords   = set(stopwords.words('english'))
-search_text = "What is the treatment of choice  for gastric lymphoma?"
-search_text = ' '.join([ token for token in bioclean(search_text) if(token not in stopWords)])
-print(search_text)
-
-bod     = {
-    'size' : 1000,
-    "query": {
-        "bool": {
-            "must": [
-                {
-                    "range" : {
-                        "DateCreated" : {
-                            "gte": "1900",
-                            "lte": "2017",
-                            "format": "dd/MM/yyyy||yyyy"
+def get_elk_results(search_text):
+    search_text = "What is the treatment of choice  for gastric lymphoma?"
+    search_text = ' '.join([token for token in bioclean(search_text) if (token not in stopWords)])
+    print(search_text)
+    bod = {
+        'size': 1000,
+        "query": {
+            "bool": {
+                "must": [
+                    {
+                        "range": {
+                            "DateCreated": {
+                                "gte": "1900",
+                                "lte": "2017",
+                                "format": "dd/MM/yyyy||yyyy"
+                            }
+                        }
+                    },
+                    {
+                        "query_string": {
+                            "query": search_text
                         }
                     }
-                },
-                {
-                    "query_string": {
-                        "query": search_text
-                    }
-                }
-            ]
+                ]
+            }
         }
     }
-}
+    res = es.search(index=index, doc_type=map, body=bod)
+    for item in res['hits']['hits']:
+        print(item[u'_score'])
+    print 20 * '-'
 
-res = es.search(index=index, doc_type=map, body=bod)
 
+bioclean = lambda t: re.sub('[.,?;*!%^&_+():-\[\]{}]', '', t.replace('"', '').replace('/', '').replace('\\', '').replace("'", '').strip().lower()).split()
 
-for item in res['hits']['hits']:
-    pprint(item)
-    exit()
+# bioasq_data_path    = '/home/DATA/Biomedical/bioasq6/bioasq6_data/BioASQ-trainingDataset6b.json'
+bioasq_data_path    = '/home/dpappas/BioASQ-trainingDataset6b.json'
+data = json.load(open(bioasq_data_path, 'r'))
+for quest in data['questions']:
+                pmid = sn['document'].split('/')[-1]
+                ttt = sn['text'].strip()
+                bod = quest['body'].strip()
+                if (bod not in ddd):
+                    ddd[bod] = {}
+                if (pmid not in ddd[bod]):
+                    ddd[bod][pmid] = [ttt]
+                else:
+                    ddd[bod][pmid].append(ttt)
+    return ddd
