0e1e8a5a1c2b8a2148d85d9a833f0ae275d8fc13
dpappas <Zrb6.NCC>
2018-06-29 17:18:30 +0300

    message

---
+++ elasticsearch_delete_dublicate_and_empty.py
@@ -0,0 +1,29 @@
+
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk, scan
+from pprint import pprint
+from nltk.corpus import stopwords
+import json
+import re
+
+
+index       = 'pubmed_abstracts_index_0_1'
+map         = "pubmed_abstracts_mapping_0_1"
+es          = Elasticsearch(['localhost:9200'], verify_certs=True, timeout=300, max_retries=10, retry_on_timeout=True)
+
+items   = scan( es, query=None, index=index, doc_type=map)
+m =  0
+for item in items:
+    if(
+        len(item['_source']['AbstractText'].strip()) == 0
+        or
+        len(item['_source']['ArticleTitle'].strip()) == 0
+    ):
+        es.delete(index=index, doc_type=map, id=item['_id'])
+    m += 1
+    print m
+
+
+
+
+
