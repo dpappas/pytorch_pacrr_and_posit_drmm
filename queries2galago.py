67308e41459ceee49ac59a32595605a57a21a776
dpappas <Zrb6.NCC>
2018-07-03 12:57:36 +0300

    message

--- queries2galago.py
+++ queries2galago.py
@@ -11,16 +11,16 @@ bioclean_mod = lambda t: re.sub('[.,?;*!%^&_+():-\[\]{}]', '', t.replace('"', ''
 def prerpocess(path_in, path_out):
     with open('stopwords.pkl', 'rb') as f:
         stopwords = pickle.load(f)
-
+    #
     f_in = open(path_in, 'r', encoding='utf-8')
     queries = ijson.items(f_in, 'questions.item')
-
+    #
     q_array = []
     for query in queries:
-
+        #
         tokenized_body = bioclean_mod(query['body'])
         tokenized_body = [t for t in tokenized_body if t not in stopwords]
-
+        #
         body = ' '.join(tokenized_body)
         q_array.append({"text": body, "number": query["id"]})
     with open(path_out, 'w+') as outfile:
