52b207640ab0f090f57e36637650e60a3e8adab5
dpappas <Zrb6.NCC>
2018-07-03 13:04:39 +0300

    message

--- queries2galago.py
+++ queries2galago.py
@@ -6,7 +6,11 @@ import pickle
 
 
 # Modified bioclean: also split on dashes. Works better for retrieval with galago.
-bioclean_mod = lambda t: re.sub('[.,?;*!%^&_+():-\[\]{}]', '', t.replace('"', '').replace('/', '').replace('\\', '').replace("'", '').replace("-", ' ').strip().lower()).split()
+bioclean_mod = lambda t: re.sub(
+    '[.,?;*!%^&_+():-\[\]{}]',
+    '',
+    t.replace('"', '').replace('/', '').replace('\\', '').replace("'", '').replace("-", ' ').strip().lower()
+).split()
 
 def prerpocess(path_in, path_out):
     with open('stopwords.pkl', 'rb') as f:
