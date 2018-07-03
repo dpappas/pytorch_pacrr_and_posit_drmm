85e6b595b2b5a92cef8e537999c3ab2fe3cd83d0
dpappas <Zrb6.NCC>
2018-07-03 14:14:08 +0300

    message

---
+++ sent_posit_drmm_MarginRankingLoss.py
@@ -0,0 +1,150 @@
+
+import re
+import random
+import numpy as np
+import cPickle as pickle
+from pprint import pprint
+from nltk.tokenize import sent_tokenize
+
+def get_index(token, t2i):
+    try:
+        return t2i[token]
+    except KeyError:
+        return t2i['UNKN']
+
+def first_alpha_is_upper(sent):
+    specials = [
+        '__EU__','__SU__','__EMS__','__SMS__','__SI__',
+        '__ESB','__SSB__','__EB__','__SB__','__EI__',
+        '__EA__','__SA__','__SQ__','__EQ__','__EXTLINK',
+        '__XREF','__URI', '__EMAIL','__ARRAY','__TABLE',
+        '__FIG','__AWID','__FUNDS'
+    ]
+    for special in specials:
+        sent = sent.replace(special,'')
+    for c in sent:
+        if(c.isalpha()):
+            if(c.isupper()):
+                return True
+            else:
+                return False
+    return False
+
+def ends_with_special(sent):
+    sent = sent.lower()
+    ind = [item.end() for item in re.finditer('[\W\s]sp.|[\W\s]nos.|[\W\s]figs.|[\W\s]sp.[\W\s]no.|[\W\s][vols.|[\W\s]cv.|[\W\s]fig.|[\W\s]e.g.|[\W\s]et[\W\s]al.|[\W\s]i.e.|[\W\s]p.p.m.|[\W\s]cf.|[\W\s]n.a.', sent)]
+    if(len(ind)==0):
+        return False
+    else:
+        ind = max(ind)
+        if (len(sent) == ind):
+            return True
+        else:
+            return False
+
+def split_sentences(text):
+    sents = [l.strip() for l in sent_tokenize(text)]
+    ret = []
+    i = 0
+    while (i < len(sents)):
+        sent = sents[i]
+        while (
+            ((i + 1) < len(sents)) and
+            (
+                ends_with_special(sent) or
+                not first_alpha_is_upper(sents[i+1].strip())
+                # sent[-5:].count('.') > 1       or
+                # sents[i+1][:10].count('.')>1   or
+                # len(sent.split()) < 2          or
+                # len(sents[i+1].split()) < 2
+            )
+        ):
+            sent += ' ' + sents[i + 1]
+            i += 1
+        ret.append(sent.replace('\n',' ').strip())
+        i += 1
+    return ret
+
+def get_sents(ntext):
+    if(len(ntext.strip())>0):
+        sents = []
+        for subtext in ntext.split('\n'):
+            subtext = re.sub( '\s+', ' ', subtext.replace('\n',' ') ).strip()
+            if (len(subtext) > 0):
+                ss = split_sentences(subtext)
+                sents.extend([ s for s in ss if(len(s.strip())>0)])
+        if(len(sents[-1]) == 0 ):
+            sents = sents[:-1]
+        return sents
+    else:
+        return []
+
+def get_sim_mat(stoks, qtoks):
+    sm = np.zeros((len(stoks), len(qtoks)))
+    for i in range(len(qtoks)):
+        for j in range(len(stoks)):
+            if(qtoks[i] == stoks[j]):
+                sm[j,i] = 1.
+    return sm
+
+def get_item_inds(item, question, t2i):
+    doc_title   = get_sents(item['title'])
+    doc_text    = get_sents(item['abstractText'])
+    all_sents   = doc_title + doc_text
+    all_sents   = [s for s in all_sents if(len(bioclean(s))>0)]
+    #
+    all_sims    = [get_sim_mat(bioclean(stoks), bioclean(question)) for stoks in all_sents]
+    #
+    sents_inds  = [[get_index(token, t2i) for token in bioclean(s)] for s in all_sents]
+    quest_inds  = [get_index(token, t2i) for token in bioclean(question)]
+    #
+    return sents_inds, quest_inds, all_sims
+
+bioclean = lambda t: re.sub('[.,?;*!%^&_+():-\[\]{}]', '', t.replace('"', '').replace('/', '').replace('\\', '').replace("'", '').strip().lower()).split()
+
+abs_path            = '/home/DATA/Biomedical/document_ranking/bioasq_data/bioasq_bm25_docset_top100.train.pkl'
+bm25_scores_path    = '/home/DATA/Biomedical/document_ranking/bioasq_data/bioasq_bm25_top100.train.pkl'
+#
+all_abs             = pickle.load(open(abs_path,'rb'))
+bm25_scores         = pickle.load(open(bm25_scores_path, 'rb'))
+#
+
+t2i = pickle.load(open('/home/dpappas/joint_task_list_batches/t2i.p','rb'))
+
+for quer in bm25_scores[u'queries']:
+    quest       = quer['query_text']
+    ret_pmids   = [t[u'doc_id'] for t in quer[u'retrieved_documents']]
+    good_pmids  = [t for t in ret_pmids if t in quer[u'relevant_documents']]
+    bad_pmids   = [t for t in ret_pmids if t not in quer[u'relevant_documents']]
+    for gid in good_pmids:
+        bid = random.choice(bad_pmids)
+        good_sents_inds, good_quest_inds, good_all_sims = get_item_inds(all_abs[gid], quest, t2i)
+        bad_sents_inds, bad_quest_inds, bad_all_sims    = get_item_inds(all_abs[bid], quest, t2i)
+    break
+
+#
+# print('LOADING embedding_matrix (14GB)')
+# matrix          = np.load('/home/dpappas/joint_task_list_batches/embedding_matrix.npy')
+# print('Done')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
