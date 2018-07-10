205fa2f01a5f2982819b8481e699acc96f886878
dpappas <Zrb6.NCC>
2018-07-10 13:44:12 +0300

    message

--- preprocess_w2v_for_fast_loading.py
+++ preprocess_w2v_for_fast_loading.py
@@ -9,15 +9,15 @@ vocabulary      = sorted(list(word_vectors.vocab.keys()))
 vecs            = []
 vecs            = np.array([word_vectors[word] for word in vocabulary])
 #
-pad_vec         = np.zeros(200)
-noise1          = np.random.normal(0,1,200)
-noise2          = np.random.normal(0,1,200)
 av_vec          = np.average(vecs, axis=0)
+pad_vec         = np.zeros(200)
+noise1          = np.random.normal(0, 1, 200)
+noise2          = np.random.normal(0, 1, 200)
 qunkn_vec       = av_vec + noise1
 dunkn_vec       = av_vec + noise2
 
-
-
+all_vecs        = np.concatenate([np.expand_dims(pad_vec,0), np.expand_dims(qunkn_vec,0), np.expand_dims(dunkn_vec,0), vecs], axis=0)
+all_voc         = ['PAD', 'QUNKN', 'DUNKN'] + vocabulary
 
 
 
