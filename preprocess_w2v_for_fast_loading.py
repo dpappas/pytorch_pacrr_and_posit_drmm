5c523be39c97c934cf6121c675ad9c52c881590b
dpappas <Zrb6.NCC>
2018-07-10 14:02:53 +0300

    message

--- preprocess_w2v_for_fast_loading.py
+++ preprocess_w2v_for_fast_loading.py
@@ -7,7 +7,6 @@ from sklearn.metrics.pairwise import cosine_similarity
 w2v_path        = '/home/DATA/Biomedical/other/BiomedicalWordEmbeddings/binary/biomedical-vectors-200.bin'
 word_vectors    = KeyedVectors.load_word2vec_format(w2v_path, binary=True)
 
-#
 vocabulary      = sorted(list(word_vectors.vocab.keys()))
 vecs            = []
 vecs            = np.array([word_vectors[word] for word in vocabulary])
@@ -18,7 +17,7 @@ noise1          = np.random.normal(0, 2, 200)
 noise2          = np.random.normal(0, 2, 200)
 qunkn_vec       = av_vec + noise1
 dunkn_vec       = av_vec + noise2
-print('similarity between qunkn and dunkn: {}'.format(cosine_similarity([qunkn_vec], [dunkn_vec])[0][0])
+print('similarity between qunkn and dunkn: {}'.format(cosine_similarity([qunkn_vec], [dunkn_vec])[0][0]))
 all_vecs        = np.concatenate([np.expand_dims(pad_vec,0), np.expand_dims(qunkn_vec,0), np.expand_dims(dunkn_vec,0), vecs], axis=0)
 all_voc         = ['PAD', 'QUNKN', 'DUNKN'] + vocabulary
 i2t             = dict(enumerate(all_voc))
