3800800b2307e5678f30853e4241f5b1b72d7fe5
dpappas <Zrb6.NCC>
2018-08-13 17:39:18 +0300

    message

--- posit_drmm_gensim_sents.py
+++ posit_drmm_gensim_sents.py
@@ -745,11 +745,7 @@ for run in range(5):
     print('random seed: {}'.format(my_seed))
     logger.info('random seed: {}'.format(my_seed))
     #
-    test_data, test_docs, dev_data, dev_docs, train_data, train_docs, idf, max_idf, wv = load_all_data(
-        dataloc         = dataloc,
-        w2v_bin_path    = w2v_bin_path,
-        idf_pickle_path = idf_pickle_path
-    )
+    test_data, test_docs, dev_data, dev_docs, train_data, train_docs, idf, max_idf, wv = load_all_data(dataloc=dataloc, w2v_bin_path=w2v_bin_path, idf_pickle_path=idf_pickle_path)
     #
     print('Compiling model...')
     logger.info('Compiling model...')
