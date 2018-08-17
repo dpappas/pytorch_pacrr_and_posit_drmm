c7f4503ce2573c0c7d33e6edf7385f31eb43182f
dpappas <Zrb6.NCC>
2018-08-17 17:13:05 +0300

    message

--- posit_drmm_gensim_two_losses.py
+++ posit_drmm_gensim_two_losses.py
@@ -429,7 +429,7 @@ def train_data_step1():
     return ret
 
 def train_data_step2(train_instances):
-    for quest, gid, bid, bm25s_gid, bm25s_bid in train_instances:
+    for quest, quest_id, gid, bid, bm25s_gid, bm25s_bid in train_instances:
         quest_tokens, quest_embeds              = get_embeds(tokenize(quest), wv)
         q_idfs                                  = np.array([[idf_val(qw)] for qw in quest_tokens], 'float')
         #
