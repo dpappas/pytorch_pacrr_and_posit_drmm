b65003df7ce0a828f528100a561f89e4bfe9e7fd
dpappas <Zrb6.NCC>
2018-08-27 15:23:48 +0300

    message

--- print_some_errors.py
+++ print_some_errors.py
@@ -586,7 +586,12 @@ for dato in test_data['queries']:
         doc_emit_, gs_emits_ = model.emit_one(doc1_sents_embeds=good_sents_embeds, question_embeds=quest_embeds, q_idfs=q_idfs, sents_gaf=good_sents_escores, doc_gaf=good_doc_af)
         emition                 = doc_emit_.cpu().item()
         sent_emits              = gs_emits_.squeeze(-1).cpu().tolist()
-        emit_inds               = [i[0] for i in sorted(enumerate(sent_emits), key=lambda x: x[1], reverse=True)]
+        #
+        emit_inds = []
+        temp = sorted(sent_emits, reverse=True)
+        for se in sent_emits:
+            emit_inds.append(temp.index(se))
+        #
         if(retr['is_relevant']):
             if(worst_pos is None or emition < worst_pos[0]):
                 worst_pos = [emition, quest, ssss, sent_emits, good_sent_tags, good_snips, emit_inds]
