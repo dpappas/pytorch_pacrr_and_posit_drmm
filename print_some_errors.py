24e8cf068f0127767a4e142039f5c192fae230ce
dpappas <Zrb6.NCC>
2018-08-27 13:09:06 +0300

    message

--- print_some_errors.py
+++ print_some_errors.py
@@ -403,7 +403,8 @@ class Sent_Posit_Drmm_Modeler(nn.Module):
         self.margin_loss                            = nn.MarginRankingLoss(margin=1.0)
         self.out_layer                              = nn.Linear(4, 1, bias=True)
         # self.final_layer                            = nn.Linear(self.k2, 1, bias=True)
-        self.final_layer                            = nn.Linear(6, 1, bias=True)
+        # self.final_layer                            = nn.Linear(6, 1, bias=True)
+        self.final_layer                            = nn.Linear(5, 1, bias=True)
         #
         # self.init_xavier()
         # self.init_using_value(0.1)
