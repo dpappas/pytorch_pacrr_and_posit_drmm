47dde327207306e695a9e020da959962b314d1b2
dpappas <Zrb6.NCC>
2018-08-31 18:05:35 +0300

    message

--- posit_drmm_gensim_sents_bce_2L_mesh.py
+++ posit_drmm_gensim_sents_bce_2L_mesh.py
@@ -792,7 +792,9 @@ class Sent_Posit_Drmm_Modeler(nn.Module):
         final_good_output   = self.final_layer(good_out_pp)
         # final_good_output   = good_out
         return final_good_output, gs_emits
-    def forward(self, doc1_sents_embeds, doc2_sents_embeds, question_embeds, q_idfs, sents_gaf, sents_baf, doc_gaf, doc_baf):
+    def forward(self, doc1_sents_embeds, doc2_sents_embeds, question_embeds, q_idfs, sents_gaf, sents_baf, doc_gaf, doc_baf, good_mesh_embeds, bad_mesh_embeds):
+        good_mesh_embeds    = autograd.Variable(torch.FloatTensor(good_mesh_embeds),    requires_grad=False)
+        bad_mesh_embeds     = autograd.Variable(torch.FloatTensor(bad_mesh_embeds),     requires_grad=False)
         q_idfs              = autograd.Variable(torch.FloatTensor(q_idfs), requires_grad=False)
         question_embeds     = autograd.Variable(torch.FloatTensor(question_embeds), requires_grad=False)
         doc_gaf             = autograd.Variable(torch.FloatTensor(doc_gaf), requires_grad=False)
