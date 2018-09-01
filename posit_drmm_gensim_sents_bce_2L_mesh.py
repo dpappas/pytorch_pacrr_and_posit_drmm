357781325476ea8883515b39122101acca545445
dpappas <Zrb6.NCC>
2018-09-02 01:38:17 +0300

    message

--- posit_drmm_gensim_sents_bce_2L_mesh.py
+++ posit_drmm_gensim_sents_bce_2L_mesh.py
@@ -502,8 +502,12 @@ def train_data_step2(train_instances):
                 bad_sents_embeds.append(bad_embeds)
                 bad_sents_escores.append(bad_escores)
         if(sum(good_sent_tags)>0):
+            print(bad_mesh)
             bmt, bad_mesh_embeds    = get_embeds(bad_mesh, wv)
+            print(bmt)
+            print(good_mesh)
             gmt, good_mesh_embeds   = get_embeds(good_mesh, wv)
+            print(gmt)
             yield (
                 good_sents_embeds,  bad_sents_embeds,   quest_embeds,       q_idfs,
                 good_sents_escores, bad_sents_escores,  good_doc_af,        bad_doc_af,
