dcbacc20f6351dd2c5077d1f4a7010fd9b6bbd60
dpappas <Zrb6.NCC>
2018-09-15 20:23:12 +0300

    message

--- final_models/pdrmm_w2v_2L_eval_sents.py
+++ final_models/pdrmm_w2v_2L_eval_sents.py
@@ -561,6 +561,25 @@ def back_prop(batch_costs, epoch_costs, batch_acc, epoch_acc):
     epoch_aver_acc  = sum(epoch_acc) / float(len(epoch_acc))
     return batch_aver_cost, epoch_aver_cost, batch_aver_acc, epoch_aver_acc
 
+def handle_good(docs, retr, quest, bm25s):
+    good_doc_text       = docs[retr['doc_id']]['title'] + docs[retr['doc_id']]['abstractText']
+    good_doc_af         = GetScores(quest, good_doc_text, bm25s[retr['doc_id']])
+    good_sents          = get_sents(docs[retr['doc_id']]['title']) + get_sents(docs[retr['doc_id']]['abstractText'])
+    good_sents_embeds   = []
+    good_sents_escores  = []
+    held_out_sents      = []
+    for good_text in good_sents:
+        good_tokens, good_embeds = get_embeds(tokenize(good_text), wv)
+        good_escores = GetScores(quest, good_text, bm25s[retr['doc_id']])[:-1]
+        if (len(good_embeds) > 0):
+            good_sents_embeds.append(good_embeds)
+            good_sents_escores.append(good_escores)
+            held_out_sents.append(good_text)
+    good_mesh               = get_the_mesh(docs[retr['doc_id']])
+    gmt, good_mesh_embeds   = get_embeds(good_mesh, wv)
+    return good_sents_embeds, good_sents_escores, good_doc_af, good_mesh_embeds, held_out_sents
+
+
 def eval_bioasq_snippets(prefix, data, docs):
     model.eval()
     all_bioasq_subm_data = {'questions':[]}
@@ -576,21 +595,7 @@ def eval_bioasq_snippets(prefix, data, docs):
         doc_res                     = {}
         extracted_snippets = []
         for retr in dato['retrieved_documents']:
-            good_doc_text       = docs[retr['doc_id']]['title'] + docs[retr['doc_id']]['abstractText']
-            good_doc_af         = GetScores(quest, good_doc_text, bm25s[retr['doc_id']])
-            good_sents          = get_sents(docs[retr['doc_id']]['title']) + get_sents(docs[retr['doc_id']]['abstractText'])
-            good_sents_embeds   = []
-            good_sents_escores  = []
-            held_out_sents      = []
-            for good_text in good_sents:
-                good_tokens, good_embeds = get_embeds(tokenize(good_text), wv)
-                good_escores = GetScores(quest, good_text, bm25s[retr['doc_id']])[:-1]
-                if (len(good_embeds) > 0):
-                    good_sents_embeds.append(good_embeds)
-                    good_sents_escores.append(good_escores)
-                    held_out_sents.append(good_text)
-            good_mesh               = get_the_mesh(docs[retr['doc_id']])
-            gmt, good_mesh_embeds   = get_embeds(good_mesh, wv)
+            good_sents_embeds, good_sents_escores, good_doc_af, good_mesh_embeds, held_out_sents = handle_good(docs, retr, quest, bm25s)
             #
             doc_emit_, gs_emits_    = model.emit_one(doc1_sents_embeds = good_sents_embeds, question_embeds = quest_embeds, q_idfs = q_idfs, sents_gaf = good_sents_escores, doc_gaf = good_doc_af, good_mesh_embeds = good_mesh_embeds)
             emitss  = gs_emits_[:, 0].tolist()
