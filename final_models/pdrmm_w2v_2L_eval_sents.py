a98c78da397097997ad31bc671257ec23f013c29
dpappas <Zrb6.NCC>
2018-09-17 17:54:45 +0300

    message

--- final_models/pdrmm_w2v_2L_eval_sents.py
+++ final_models/pdrmm_w2v_2L_eval_sents.py
@@ -715,6 +715,7 @@ def fix_text_for_matching(the_text):
     the_text = the_text.replace('Alzheimer\'s', 'Alzheimers')
     the_text = the_text.strip()
     the_text = the_text.replace('Background Niraparib is an oral poly', 'Niraparib is an oral poly')
+    the_text = the_text.replace('or intramuscular interferon beta-1a (30 ìg) once a week.', 'or intramuscular interferon beta-1a (30 mg) once a week.')
     return the_text
 
 def prepare_gold_dato(gold_dato):
