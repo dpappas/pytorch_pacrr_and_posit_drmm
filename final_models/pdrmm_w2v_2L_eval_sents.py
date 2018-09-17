e031772f3343c1d55c43224ec9f040926b0d818b
dpappas <Zrb6.NCC>
2018-09-17 16:17:25 +0300

    message

--- final_models/pdrmm_w2v_2L_eval_sents.py
+++ final_models/pdrmm_w2v_2L_eval_sents.py
@@ -687,12 +687,12 @@ def prep_bioasq_subm_dato(extracted_snippets, dato, doc_res):
 
 def fix_text_for_matching(the_text):
     the_text = the_text.replace('á',            'α')
-    the_text = the_text.replace('BACKGROUND:',  '')
+    the_text = the_text.replace('BACKGROUND:',  ' ')
     the_text = the_text.replace('&quot;',       '"')
-    the_text = the_text.replace('METHODS:',     '')
-    the_text = the_text.replace('OBJECTIVE:',   '')
-    the_text = the_text.replace('RESULTS:',     '')
-    the_text = the_text.replace('CONCLUSIONS:', '')
+    the_text = the_text.replace('METHODS:',     ' ')
+    the_text = the_text.replace('OBJECTIVE:',   ' ')
+    the_text = the_text.replace('RESULTS:',     ' ')
+    the_text = the_text.replace('CONCLUSIONS:', ' ')
     the_text = the_text.strip()
     return the_text
 
