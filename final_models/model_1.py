81b8abaad7268ea63e4ba9490b4539501d21c569
dpappas <Zrb6.NCC>
2018-09-23 14:26:01 +0300

    message

--- final_models/model_1.py
+++ final_models/model_1.py
@@ -1001,7 +1001,10 @@ for run in range(5):
     print('random seed: {}'.format(my_seed))
     logger.info('random seed: {}'.format(my_seed))
     #
-    (test_data, test_docs, dev_data, dev_docs, train_data, train_docs, idf, max_idf, wv, bioasq6_data) = load_all_data(dataloc=dataloc, w2v_bin_path=w2v_bin_path, idf_pickle_path=idf_pickle_path)
+    (
+        test_data, test_docs, dev_data, dev_docs, train_data,
+        train_docs, idf, max_idf, wv, bioasq6_data
+    ) = load_all_data(dataloc=dataloc, w2v_bin_path=w2v_bin_path, idf_pickle_path=idf_pickle_path)
     #
     print('Compiling model...')
     logger.info('Compiling model...')
