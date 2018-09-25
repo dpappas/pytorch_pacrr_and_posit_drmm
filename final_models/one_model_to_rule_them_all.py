3504f989806a22ef31ab3203fd3dd37da62adab0
dpappas <Zrb6.NCC>
2018-09-25 23:30:17 +0300

    message

--- final_models/one_model_to_rule_them_all.py
+++ final_models/one_model_to_rule_them_all.py
@@ -1108,3 +1108,7 @@ for run in range(5):
             save_checkpoint(epoch, model, best_dev_map, optimizer, filename=odir+'best_checkpoint.pth.tar')
         print('epoch:{} epoch_dev_map:{} best_dev_map:{} test_map:{}'.format(epoch + 1, epoch_dev_map, best_dev_map, test_map))
         logger.info('epoch:{} epoch_dev_map:{} best_dev_map:{} test_map:{}'.format(epoch + 1, epoch_dev_map, best_dev_map, test_map))
+
+
+
+
