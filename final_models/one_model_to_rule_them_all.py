e252628f32a83b7d38b97b979ec7a94aa9fa2b64
dpappas <Zrb6.NCC>
2018-10-15 14:41:27 +0300

    message

--- final_models/one_model_to_rule_them_all.py
+++ final_models/one_model_to_rule_them_all.py
@@ -711,17 +711,41 @@ def do_for_some_retrieved(docs, dato, retr_docs, data_for_revision, ret_data, al
 
 def get_one_map(prefix, data, docs):
     model.eval()
-    ret_data                = {'questions': []}
-    all_bioasq_subm_data    = {"questions": []}
-    all_bioasq_subm_data_known = {"questions": []}
-    all_bioasq_gold_data    = {'questions': []}
-    data_for_revision       = {}
+    #
+    ret_data                    = {'questions': []}
+    all_bioasq_subm_data        = {"questions": []}
+    all_bioasq_subm_data_known  = {"questions": []}
+    all_bioasq_gold_data        = {'questions': []}
+    data_for_revision           = {}
+    #
+    ret_data_2                    = {'questions': []}
+    all_bioasq_subm_data_2        = {"questions": []}
+    all_bioasq_subm_data_known_2  = {"questions": []}
+    all_bioasq_gold_data_2        = {'questions': []}
+    data_for_revision_2           = {}
     for dato in tqdm(data['queries']):
         all_bioasq_gold_data['questions'].append(bioasq6_data[dato['query_id']])
+        #
         data_for_revision, ret_data, all_bioasq_subm_data, all_bioasq_subm_data_known = do_for_some_retrieved(
-            docs, dato, dato['retrieved_documents'], data_for_revision,
-            ret_data, all_bioasq_subm_data, all_bioasq_subm_data_known
+            docs,
+            dato,
+            dato['retrieved_documents'],
+            data_for_revision,
+            ret_data,
+            all_bioasq_subm_data,
+            all_bioasq_subm_data_known
         )
+        # for retr in get_pseudo_retrieved(dato):
+        data_for_revision_2, ret_data_2, all_bioasq_subm_data_2, all_bioasq_subm_data_known_2 = do_for_some_retrieved(
+            docs,
+            dato,
+            get_pseudo_retrieved(dato),
+            data_for_revision_2,
+            ret_data_2,
+            all_bioasq_subm_data_2,
+            all_bioasq_subm_data_known_2
+        )
+
     #
     bioasq_snip_res = get_bioasq_res(prefix, all_bioasq_gold_data, all_bioasq_subm_data_known, data_for_revision)
     pprint(bioasq_snip_res)
