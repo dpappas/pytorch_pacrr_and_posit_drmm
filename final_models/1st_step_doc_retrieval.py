0337d065e453fe4ec28b674ed211522a3e496f14
dpappas <Zrb6.NCC>
2018-10-29 11:32:23 +0200

    message

--- final_models/1st_step_doc_retrieval.py
+++ final_models/1st_step_doc_retrieval.py
@@ -1097,7 +1097,7 @@ models = [
 ]
 models = dict([(item[0], item[1:]) for item in models])
 
-which_model = 'Doc_Ret_Model_02'
+which_model = 'Doc_Ret_Model_04'
 
 hdlr = None
 for run in range(5):
