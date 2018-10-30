0840eb3a0465375d13cd4e3acd1cd16a40f7a565
dpappas <Zrb6.NCC>
2018-10-30 12:32:14 +0200

    message

--- final_models/one_model_to_rule_them_all_2.py
+++ final_models/one_model_to_rule_them_all_2.py
@@ -311,7 +311,7 @@ def do_for_some_retrieved(docs, dato, retr_docs, data_for_revision, ret_data, al
         if (dato['query_id'] not in data_for_revision):
             data_for_revision[dato['query_id']] = {
                 'query_text': dato['query_text'],
-                'snippets': {retr['doc_id']: all_emits}
+                'snippets'  : {retr['doc_id']: all_emits}
             }
         else:
             data_for_revision[dato['query_id']]['snippets'][retr['doc_id']] = all_emits
