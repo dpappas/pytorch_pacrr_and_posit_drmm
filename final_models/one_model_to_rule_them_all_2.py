1925a4221bfe770d7799b68015e8e32b37ba8534
dpappas <Zrb6.NCC>
2018-10-30 12:50:15 +0200

    message

--- final_models/one_model_to_rule_them_all_2.py
+++ final_models/one_model_to_rule_them_all_2.py
@@ -316,9 +316,7 @@ def do_for_some_retrieved(docs, dato, retr_docs, data_for_revision, ret_data, al
     #
     doc_res, extracted_snippets, extracted_snippets_known_rel_num = {}, [], []
     for retr in retr_docs:
-        datum = prep_data(quest_text, docs[retr['doc_id']], retr['norm_bm25_score'], wv, gold_snips,
-            idf, max_idf, use_sent_tokenizer=False
-        )
+        datum = prep_data(quest_text, docs[retr['doc_id']], retr['norm_bm25_score'], wv, gold_snips, idf, max_idf, use_sent_tokenizer=False)
         doc_emit_, gs_emits_    = model.emit_one(
             doc1_sents_embeds   = datum['good_sents_embeds'],
             question_embeds     = quest_embeds,
