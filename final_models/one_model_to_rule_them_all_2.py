a9563878b6c00e5aa65cd302894d84d45192a212
dpappas <Zrb6.NCC>
2018-11-02 14:46:28 +0200

    message

--- final_models/one_model_to_rule_them_all_2.py
+++ final_models/one_model_to_rule_them_all_2.py
@@ -514,13 +514,13 @@ def prep_data(quest, the_doc, the_bm25, wv, good_snips, idf, max_idf, use_sent_t
             good_mesh_escores.append(good_escores)
     ####
     return {
-        'good_sents_embeds'     : good_sents_embeds,
-        'good_sents_escores'    : good_sents_escores,
-        'good_doc_af'           : good_doc_af,
-        'good_sent_tags'        : good_sent_tags,
-        'good_mesh_embeds'      : good_mesh_embeds,
-        'good_mesh_escores'     : good_mesh_escores,
-        'held_out_sents'        : held_out_sents,
+        'sents_embeds'     : good_sents_embeds,
+        'sents_escores'    : good_sents_escores,
+        'doc_af'           : good_doc_af,
+        'sent_tags'        : good_sent_tags,
+        'mesh_embeds'      : good_mesh_embeds,
+        'mesh_escores'     : good_mesh_escores,
+        'held_out_sents'   : held_out_sents,
     }
 
 def train_data_step1(train_data):
@@ -544,20 +544,20 @@ def train_data_step2(instances, docs, wv, bioasq6_data, idf, max_idf, use_sent_t
         #
         good_snips              = get_snips(quest_id, gid, bioasq6_data)
         datum                   = prep_data(quest_text, docs[gid], bm25s_gid, wv, good_snips, idf, max_idf, use_sent_tokenizer)
-        good_sents_embeds       = datum['good_sents_embeds']
-        good_sents_escores      = datum['good_sents_escores']
-        good_mesh_escores       = datum['good_mesh_escores']
-        good_mesh_embeds        = datum['good_mesh_embeds']
-        good_doc_af             = datum['good_doc_af']
-        good_sent_tags          = datum['good_sent_tags']
+        good_sents_embeds       = datum['sents_embeds']
+        good_sents_escores      = datum['sents_escores']
+        good_mesh_escores       = datum['mesh_escores']
+        good_mesh_embeds        = datum['mesh_embeds']
+        good_doc_af             = datum['doc_af']
+        good_sent_tags          = datum['sent_tags']
         #
         datum                   = prep_data(quest_text, docs[bid], bm25s_bid, wv, [], idf, max_idf, use_sent_tokenizer)
-        bad_sents_embeds        = datum['good_sents_embeds']
-        bad_sents_escores       = datum['good_sents_escores']
-        bad_mesh_escores        = datum['good_mesh_escores']
-        bad_mesh_embeds         = datum['good_mesh_embeds']
-        bad_doc_af              = datum['good_doc_af']
-        bad_sent_tags           = [0] * len(datum['good_sent_tags'])
+        bad_sents_embeds        = datum['sents_embeds']
+        bad_sents_escores       = datum['sents_escores']
+        bad_mesh_escores        = datum['mesh_escores']
+        bad_mesh_embeds         = datum['mesh_embeds']
+        bad_doc_af              = datum['doc_af']
+        bad_sent_tags           = [0] * len(datum['sent_tags'])
         #
         quest_tokens, quest_embeds  = get_embeds(tokenize(quest_text), wv)
         q_idfs                      = np.array([[idf_val(qw, idf, max_idf)] for qw in quest_tokens], 'float')
