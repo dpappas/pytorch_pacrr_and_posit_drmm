3a858279c30594a9da077b1e9364245a7f08bcd3
dpappas <Zrb6.NCC>
2018-11-07 17:32:16 +0200

    message

--- final_models/one_model_to_rule_them_all_2.py
+++ final_models/one_model_to_rule_them_all_2.py
@@ -1302,3 +1302,30 @@ otidipote den exei identifier
 
 '''
 
+
+'''
+Petros
+give the htmls to peter for annotation
+keep the original text and keep what metamap thought
+
+Sotiris
+
+
+Me
+remove the bias on snippet extraction before sigmoid    -- DONE
+add average similarity score as 3rd feature             -- DONE
+add oracle scores for snippets                          -- DONE
+compare to polivios scores                              -- pending
+
+Train first doc retrieval and then snippet extraction   -- pending (DONE TRAINING. TESTING pending)
+
+select 10 best snippets overall.
+oracle return just 10. Not more!
+add new dataset + trec robust (FIND english paragraph questions) + 5-fold cross validation
+
+softmax sta scores twn docs + harmonic mean (F1)        FOR SNIPPET EXTRACTION
+
+PRINT DEV SCORES
+
+'''
+
