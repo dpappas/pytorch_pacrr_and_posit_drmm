17a080b0c341a447c4aebe2baf1179b6411c8d44
dpappas <Zrb6.NCC>
2018-11-08 12:48:48 +0200

    message

--- final_models/print_results_2.py
+++ final_models/print_results_2.py
@@ -46,14 +46,15 @@ if(os.path.exists(fpath)):
                 res['test']['f1_snip']          = data[5]
                 res['test']['map_snip']         = data[6]
                 res['test']['gmap_snip']        = data[7]
-                print '{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(
-                    res['test']['map_doc'], res['test']['map_doc_bioasq'], res['test']['f1_snip'],
-                    res['test']['map_snip'], res['test']['gmap_snip'], res['test']['known_f1_snip'],
-                    res['test']['known_map_snip'], res['test']['known_gmap_snip'], res['dev']['map_doc'],
-                    res['dev']['map_doc_bioasq'], res['dev']['f1_snip'], res['dev']['map_snip'],
-                    res['dev']['gmap_snip'], res['dev']['known_f1_snip'], res['dev']['known_map_snip'],
-                    res['dev']['known_gmap_snip'], res['epoch'],
+                print '{}\t{}\t{}\t{}\t{}\t{}'.format(
+                    res['test']['f1_snip'],         res['test']['map_snip'],        res['test']['gmap_snip'],
+                    res['test']['known_f1_snip'],   res['test']['known_map_snip'],  res['test']['known_gmap_snip']
                 )
+                print '{}\t{}\t{}\t{}\t{}\t{}'.format(
+                    res['dev']['f1_snip'],          res['dev']['map_snip'],         res['dev']['gmap_snip'],
+                    res['dev']['known_f1_snip'],    res['dev']['known_map_snip'],   res['dev']['known_gmap_snip']
+                )
+                print('')
             elif('dev known MAP documents' in lines[l]):
                 data = lines[l:l+8]
                 data = [float(t.strip().split()[-1]) for t in data]
