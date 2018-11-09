c76346efe625dd08e6bc91a8b8491e19af525ccf
dpappas <Zrb6.NCC>
2018-11-09 15:05:43 +0200

    message

--- final_models/joint_model_extract_results.py
+++ final_models/joint_model_extract_results.py
@@ -1010,8 +1010,10 @@ hdlr    = None
 
 
 test_these_models = [
-'Model_41', 'Model_42', 'Model_43', 'Model_44', 'Model_45', 'Model_46', 'Model_47', 'Model_48',
-'Model_49', 'Model_50', 'Model_51', 'Model_52', 'Model_53', 'Model_54', 'Model_55', 'Model_56'
+'Model_41', 'Model_42', 'Model_43', 'Model_44',
+'Model_45', 'Model_46', 'Model_47', 'Model_48',
+'Model_49', 'Model_50', 'Model_51', 'Model_52',
+'Model_53', 'Model_54', 'Model_55', 'Model_56'
 ]
 for which_model in test_these_models:
     odir            = 'this_is_me_testing_{}'.format(which_model)
