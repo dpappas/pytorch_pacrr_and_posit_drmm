c6ee96cca2325cba050a904dacb9d2fc9a2b5198
dpappas <Zrb6.NCC>
2018-11-09 14:13:46 +0200

    message

--- final_models/joint_model_extract_results.py
+++ final_models/joint_model_extract_results.py
@@ -1009,6 +1009,11 @@ models = [
 models = dict([(item[0], item[1:]) for item in models])
 
 hdlr    = None
+
+
+
+
+
 odir    = 'this_is_me_testing_{}'.format(which_model)
 odir    = os.path.join(odd, odir)
 print odir
@@ -1016,22 +1021,17 @@ if(not os.path.exists(odir)):
     os.makedirs(odir)
 
 logger, hdlr    = init_the_logger(hdlr)
-#
 (test_data, test_docs, dev_data, dev_docs, train_data, train_docs, idf, max_idf, wv, bioasq6_data
 ) = load_all_data(dataloc=dataloc, w2v_bin_path=w2v_bin_path, idf_pickle_path=idf_pickle_path)
-
 gc.collect()
-
 print('Compiling model...')
 logger.info('Compiling model...')
 model = Sent_Posit_Drmm_Modeler(embedding_dim=embedding_dim, k_for_maxpool=k_for_maxpool, context_method=models[which_model][0], sentence_out_method=models[which_model][1], mesh_style=models[which_model][2])
-
 if(use_cuda):
     model = model.cuda()
 
 params = model.parameters()
 print_params(model)
-
 for run in range(5):
     logger.info('RUN: {}'.format(run))
     print('RUN: {}'.format(run))
