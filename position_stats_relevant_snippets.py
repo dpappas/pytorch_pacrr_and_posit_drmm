a802cb3fd163ac34e7ad4c5f2f62cd4d43984e5a
dpappas <Zrb6.NCC>
2018-11-13 12:02:48 +0200

    message

--- position_stats_relevant_snippets.py
+++ position_stats_relevant_snippets.py
@@ -170,15 +170,15 @@ def print_params(model):
 def tokenize(x):
   return bioclean(x)
 
-def idf_val(w):
+def idf_val(w, idf, max_idf):
     if w in idf:
         return idf[w]
     return max_idf
 
-def get_words(s):
+def get_words(s, idf, max_idf):
     sl  = tokenize(s)
     sl  = [s for s in sl]
-    sl2 = [s for s in sl if idf_val(s) >= 2.0]
+    sl2 = [s for s in sl if idf_val(s, idf, max_idf) >= 2.0]
     return sl, sl2
 
 def get_embeds(tokens, wv):
@@ -191,7 +191,7 @@ def get_embeds(tokens, wv):
 
 def load_idfs(idf_path, words):
     print('Loading IDF tables')
-    # logger.info('Loading IDF tables')
+    #
     # with open(dataloc + 'idf.pkl', 'rb') as f:
     with open(idf_path, 'rb') as f:
         idf = pickle.load(f)
@@ -205,7 +205,7 @@ def load_idfs(idf_path, words):
             max_idf = idf[w]
     idf = None
     print('Loaded idf tables with max idf {}'.format(max_idf))
-    # logger.info('Loaded idf tables with max idf {}'.format(max_idf))
+    #
     return ret, max_idf
 
 def uwords(words):
@@ -222,16 +222,16 @@ def ubigrams(words):
     prevw = w
   return [w for w in uw]
 
-def query_doc_overlap(qwords, dwords):
+def query_doc_overlap(qwords, dwords, idf, max_idf):
     # % Query words in doc.
     qwords_in_doc = 0
     idf_qwords_in_doc = 0.0
     idf_qwords = 0.0
     for qword in uwords(qwords):
-      idf_qwords += idf_val(qword)
+      idf_qwords += idf_val(qword, idf, max_idf)
       for dword in uwords(dwords):
         if qword == dword:
-          idf_qwords_in_doc += idf_val(qword)
+          idf_qwords_in_doc += idf_val(qword, idf, max_idf)
           qwords_in_doc     += 1
           break
     if len(qwords) <= 0:
@@ -249,11 +249,11 @@ def query_doc_overlap(qwords, dwords):
     idf_bigrams = 0.0
     for qword in ubigrams(qwords):
       wrds = qword.split('_')
-      idf_bigrams += idf_val(wrds[0]) * idf_val(wrds[1])
+      idf_bigrams += idf_val(wrds[0], idf, max_idf) * idf_val(wrds[1], idf, max_idf)
       for dword in ubigrams(dwords):
         if qword == dword:
           qwords_bigrams_in_doc += 1
-          idf_qwords_bigrams_in_doc += (idf_val(wrds[0]) * idf_val(wrds[1]))
+          idf_qwords_bigrams_in_doc += (idf_val(wrds[0], idf, max_idf) * idf_val(wrds[1], idf, max_idf))
           break
     if len(qwords) <= 0:
       qwords_bigrams_in_doc_val = 0.0
@@ -268,10 +268,10 @@ def query_doc_overlap(qwords, dwords):
             idf_qwords_in_doc_val,
             idf_qwords_bigrams_in_doc_val]
 
-def GetScores(qtext, dtext, bm25):
-    qwords, qw2 = get_words(qtext)
-    dwords, dw2 = get_words(dtext)
-    qd1         = query_doc_overlap(qwords, dwords)
+def GetScores(qtext, dtext, bm25, idf, max_idf):
+    qwords, qw2 = get_words(qtext, idf, max_idf)
+    dwords, dw2 = get_words(dtext, idf, max_idf)
+    qd1         = query_doc_overlap(qwords, dwords, idf, max_idf)
     bm25        = [bm25]
     return qd1[0:3] + bm25
 
@@ -284,7 +284,12 @@ def GetWords(data, doc_text, words):
       doc_id = data['queries'][i]['retrieved_documents'][j]['doc_id']
       dtext = (
               doc_text[doc_id]['title'] + ' <title> ' + doc_text[doc_id]['abstractText'] +
-              ' '.join(get_the_mesh(doc_text[doc_id]))
+              ' '.join(
+                  [
+                      ' '.join(mm) for mm in
+                      get_the_mesh(doc_text[doc_id])
+                  ]
+              )
       )
       dwds = tokenize(dtext)
       for w in dwds:
