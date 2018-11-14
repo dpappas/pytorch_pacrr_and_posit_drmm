3c0923ffef9594af656f1866ea3ba595486bc542
dpappas <Zrb6.NCC>
2018-11-14 17:36:59 +0200

    message

--- final_models/print_results_2.py
+++ final_models/print_results_2.py
@@ -36,7 +36,20 @@ def print_version(version):
         with open(fpath) as f:
             lines = f.readlines()
             for l in range(len(lines)):
-                if('{} test known MAP documents'.format(version) in lines[l]):
+                print(lines[l].strip())
+                if('{} dev known MAP documents'.format(version) in lines[l]):
+                    data = lines[l:l+8]
+                    data = [float(t.strip().split()[-1]) for t in data]
+                    #
+                    res['dev']['map_doc_bioasq']    = data[0]
+                    res['dev']['known_f1_snip']     = data[1]
+                    res['dev']['known_map_snip']    = data[2]
+                    res['dev']['known_gmap_snip']   = data[3]
+                    res['dev']['f1_snip']           = data[5]
+                    res['dev']['map_snip']          = data[6]
+                    res['dev']['gmap_snip']         = data[7]
+                    #
+                elif('{} test known MAP documents'.format(version) in lines[l]):
                     data = lines[l:l+8]
                     data = [float(t.strip().split()[-1]) for t in data]
                     #
@@ -52,20 +65,6 @@ def print_version(version):
                     devs.append((res['dev']['f1_snip'], res['dev']['map_snip'], res['dev']['gmap_snip'],
                                  res['dev']['known_f1_snip'], res['dev']['known_map_snip'],
                                  res['dev']['known_gmap_snip']))
-                elif('{} dev known MAP documents' in lines[l]):
-                    data = lines[l:l+8]
-                    data = [float(t.strip().split()[-1]) for t in data]
-                    #
-                    pprint(data)
-                    #
-                    res['dev']['map_doc_bioasq']    = data[0]
-                    res['dev']['known_f1_snip']     = data[1]
-                    res['dev']['known_map_snip']    = data[2]
-                    res['dev']['known_gmap_snip']   = data[3]
-                    res['dev']['f1_snip']           = data[5]
-                    res['dev']['map_snip']          = data[6]
-                    res['dev']['gmap_snip']         = data[7]
-                    #
     print fpath
     print 'test'
     print '\n'.join('\t'.join(str(e) for e in t) for t in tests)
