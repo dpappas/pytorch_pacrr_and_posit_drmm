d29569cc5e41ba4ba794b2111c758631b21d5431
dpappas <Zrb6.NCC>
2018-11-14 17:13:40 +0200

    message

--- final_models/print_results_2.py
+++ final_models/print_results_2.py
@@ -11,7 +11,7 @@ from pprint import pprint
 # fpath = '/home/dpappas/MODELS_OUTPUTS/this_is_me_testing_Model_44/model.log'
 
 # fpath = '/home/dpappas/MODELS_OUTPUTS/this_is_me_testing_Model_45/model.log'
-fpath = '/home/dpappas/MODELS_OUTPUTS/this_is_me_testing_Model_46/model.log'
+# fpath = '/home/dpappas/MODELS_OUTPUTS/this_is_me_testing_Model_46/model.log'
 # fpath = '/home/dpappas/MODELS_OUTPUTS/this_is_me_testing_Model_47/model.log'
 # fpath = '/home/dpappas/MODELS_OUTPUTS/this_is_me_testing_Model_48/model.log'
 
@@ -25,7 +25,7 @@ fpath = '/home/dpappas/MODELS_OUTPUTS/this_is_me_testing_Model_46/model.log'
 # fpath = '/home/dpappas/MODELS_OUTPUTS/this_is_me_testing_Model_55/model.log'
 # fpath = '/home/dpappas/MODELS_OUTPUTS/this_is_me_testing_Model_56/model.log'
 
-# fpath = '/home/dpappas/this_is_me_testing_Model_46/model.log'
+fpath = '/home/dpappas/this_is_me_testing_Model_46/model.log'
 
 def print_version(version):
     tests, devs = [], []
