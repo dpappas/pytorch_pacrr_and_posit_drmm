dee8d56674ee97b75de4b75bddf0326e3a668129
dpappas <Zrb6.NCC>
2018-11-14 15:30:33 +0200

    message

--- final_models/joint_model_extract_results.py
+++ final_models/joint_model_extract_results.py
@@ -1043,13 +1043,21 @@ class Sent_Posit_Drmm_Modeler(nn.Module):
 
 use_cuda = torch.cuda.is_available()
 use_cuda            = False
-#
-# laptop
-w2v_bin_path        = '/home/dpappas/for_ryan/fordp/pubmed2018_w2v_30D.bin'
-idf_pickle_path     = '/home/dpappas/for_ryan/fordp/idf.pkl'
-dataloc             = '/home/dpappas/for_ryan/'
-eval_path           = '/home/dpappas/for_ryan/eval/run_eval.py'
-retrieval_jar_path  = '/home/dpappas/NetBeansProjects/my_bioasq_eval_2/dist/my_bioasq_eval_2.jar'
+
+# # laptop
+# w2v_bin_path        = '/home/dpappas/for_ryan/fordp/pubmed2018_w2v_30D.bin'
+# idf_pickle_path     = '/home/dpappas/for_ryan/fordp/idf.pkl'
+# dataloc             = '/home/dpappas/for_ryan/'
+# eval_path           = '/home/dpappas/for_ryan/eval/run_eval.py'
+# retrieval_jar_path  = '/home/dpappas/NetBeansProjects/my_bioasq_eval_2/dist/my_bioasq_eval_2.jar'
+# odd                 = '/home/dpappas/'
+
+# cslab241
+w2v_bin_path        = '/home/dpappas/for_ryan/pubmed2018_w2v_30D.bin'
+idf_pickle_path     = '/home/dpappas/for_ryan/idf.pkl'
+dataloc             = '/home/DATA/Biomedical/document_ranking/bioasq_data/'
+eval_path           = '/home/DATA/Biomedical/document_ranking/eval/run_eval.py'
+retrieval_jar_path  = '/home/dpappas/bioasq_eval/dist/my_bioasq_eval_2.jar'
 odd                 = '/home/dpappas/'
 
 # # atlas
@@ -1100,8 +1108,8 @@ hdlr    = None
 test_these_models = [
 # 'Model_39',
 # 'Model_40',
-'Model_45',
-# 'Model_46',
+# 'Model_45',
+'Model_46',
 # 'Model_41', 'Model_42', 'Model_43', 'Model_44',
 # 'Model_47', 'Model_48',
 # 'Model_49', 'Model_50', 'Model_51', 'Model_52',
