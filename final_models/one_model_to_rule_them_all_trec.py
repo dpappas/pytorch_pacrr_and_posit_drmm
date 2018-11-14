979b983a78b7a883a6b43cc8abf8332806d82ed6
dpappas <Zrb6.NCC>
2018-11-14 12:45:35 +0200

    message

--- final_models/one_model_to_rule_them_all_trec.py
+++ final_models/one_model_to_rule_them_all_trec.py
@@ -877,21 +877,21 @@ def get_one_map(prefix, data, docs, use_sent_tokenizer):
 def load_all_data(dataloc, w2v_bin_path, idf_pickle_path):
     print('loading pickle data')
     #
-    with open(os.path.join(dataloc,'BioASQ-trainingDataset6b.json'), 'r') as f:
+    with open(os.path.join(dataloc, 'BioASQ-trainingDataset6b.json'), 'r') as f:
         bioasq6_data = json.load(f)
         bioasq6_data = dict( (q['id'], q) for q in bioasq6_data['questions'] )
     #
-    with open(os.path.join(dataloc,'bioasq_bm25_top100.test.pkl'), 'rb') as f:
+    with open(os.path.join(dataloc, 'rob04_bm25_top100.test.s1.pkl'), 'rb') as f:
         test_data = pickle.load(f)
-    with open(os.path.join(dataloc,'bioasq_bm25_docset_top100.test.pkl'), 'rb') as f:
+    with open(os.path.join(dataloc, 'rob04_bm25_docset_top100.test.s1.pkl'), 'rb') as f:
         test_docs = pickle.load(f)
-    with open(os.path.join(dataloc,'bioasq_bm25_top100.dev.pkl'), 'rb') as f:
+    with open(os.path.join(dataloc, 'rob04_bm25_top100.dev.s1.pkl'), 'rb') as f:
         dev_data = pickle.load(f)
-    with open(os.path.join(dataloc,'rob04_bm25_docset_top100.dev.s1.pkl'), 'rb') as f:
+    with open(os.path.join(dataloc, 'rob04_bm25_docset_top100.dev.s1.pkl'), 'rb') as f:
         dev_docs = pickle.load(f)
-    with open(os.path.join(dataloc,'bioasq_bm25_top100.train.pkl'), 'rb') as f:
+    with open(os.path.join(dataloc, 'rob04_bm25_top100.train.s1.pkl'), 'rb') as f:
         train_data = pickle.load(f)
-    with open(os.path.join(dataloc,'bioasq_bm25_docset_top100.train.pkl'), 'rb') as f:
+    with open(os.path.join(dataloc, 'rob04_bm25_docset_top100.train.s1.pkl'), 'rb') as f:
         train_docs = pickle.load(f)
     print('loading words')
     #
