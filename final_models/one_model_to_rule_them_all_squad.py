c8c011719aa30885430eeabf7ffb5610aabd64ba
dpappas <Zrb6.NCC>
2018-11-15 12:15:11 +0200

    message

--- final_models/one_model_to_rule_them_all_squad.py
+++ final_models/one_model_to_rule_them_all_squad.py
@@ -1,18 +1,47 @@
 
 import os
-from gensim.test.utils import datapath, get_tmpfile
+import json
+import cPickle as pickle
 from gensim.models import KeyedVectors
-from gensim.scripts.glove2word2vec import glove2word2vec
 
-glove_fpath = '/media/dpappas/dpappas_data/EMBEDDINGS/glove.6B.50d.txt'
+def load_all_data(dataloc, w2v_txt_path, idf_pickle_path):
+    print('loading pickle data')
+    #
+    with open(dataloc+'BioASQ-trainingDataset6b.json', 'r') as f:
+        bioasq6_data = json.load(f)
+        bioasq6_data = dict( (q['id'], q) for q in bioasq6_data['questions'] )
+    #
+    with open(dataloc + 'bioasq_bm25_top100.test.pkl', 'rb') as f:
+        test_data = pickle.load(f)
+    with open(dataloc + 'bioasq_bm25_docset_top100.test.pkl', 'rb') as f:
+        test_docs = pickle.load(f)
+    with open(dataloc + 'bioasq_bm25_top100.dev.pkl', 'rb') as f:
+        dev_data = pickle.load(f)
+    with open(dataloc + 'bioasq_bm25_docset_top100.dev.pkl', 'rb') as f:
+        dev_docs = pickle.load(f)
+    with open(dataloc + 'bioasq_bm25_top100.train.pkl', 'rb') as f:
+        train_data = pickle.load(f)
+    with open(dataloc + 'bioasq_bm25_docset_top100.train.pkl', 'rb') as f:
+        train_docs = pickle.load(f)
+    print('loading words')
+    #
+    words           = {}
+    GetWords(train_data, train_docs, words)
+    GetWords(dev_data,   dev_docs,   words)
+    GetWords(test_data,  test_docs,  words)
+    # mgmx
+    print('loading idfs')
+    idf, max_idf    = load_idfs(idf_pickle_path, words)
+    print('loading w2v')
+    wv              = KeyedVectors.load_word2vec_format(w2v_bin_path, binary=True)
+    wv              = dict([(word, wv[word]) for word in wv.vocab.keys() if(word in words)])
+    return test_data, test_docs, dev_data, dev_docs, train_data, train_docs, idf, max_idf, wv, bioasq6_data
+
+
+
 w2v_fpath   = "/media/dpappas/dpappas_data/EMBEDDINGS/word2vec.6B.50d.txt"
 
-if os.path.exists(w2v_fpath):
-    tmp_file = get_tmpfile(w2v_fpath)
-else:
-    glove_file = datapath(glove_fpath)
-    tmp_file = get_tmpfile(w2v_fpath)
-    glove2word2vec(glove_file, tmp_file)
 
-model = KeyedVectors.load_word2vec_format(tmp_file)
-print model.wv['this is a test'.split()].shape
+
+
+
