5e0704ebb839b60e30123b0c6165a21de912502f
dpappas <Zrb6.NCC>
2018-11-15 12:09:38 +0200

    message

--- glove_to_w2v_gensim.py
+++ glove_to_w2v_gensim.py
@@ -1,15 +1,18 @@
 
-
+import os
 from gensim.test.utils import datapath, get_tmpfile
 from gensim.models import KeyedVectors
 from gensim.scripts.glove2word2vec import glove2word2vec
 
-glove_file = datapath('/media/dpappas/dpappas_data/EMBEDDINGS/glove.6B.50d.txt')
-tmp_file = get_tmpfile("/media/dpappas/dpappas_data/EMBEDDINGS/word2vec.6B.50d.txt")
-glove2word2vec(glove_file, tmp_file)
+glove_fpath = '/media/dpappas/dpappas_data/EMBEDDINGS/glove.6B.50d.txt'
+w2v_fpath   = "/media/dpappas/dpappas_data/EMBEDDINGS/word2vec.6B.50d.txt"
+
+if os.path.exists(w2v_fpath):
+    tmp_file = get_tmpfile(w2v_fpath)
+else:
+    glove_file = datapath(glove_fpath)
+    tmp_file = get_tmpfile(w2v_fpath)
+    glove2word2vec(glove_file, tmp_file)
 
 model = KeyedVectors.load_word2vec_format(tmp_file)
-
 print model.wv['this is a test'.split()].shape
-
-
