3053ff64ad724456a0d0c2abf65922c1f1431a56
dpappas <Zrb6.NCC>
2018-11-15 12:34:55 +0200

    message

--- final_models/one_model_to_rule_them_all_squad.py
+++ final_models/one_model_to_rule_them_all_squad.py
@@ -1,46 +1,41 @@
 
 import os
 import json
+from pprint import pprint
 import cPickle as pickle
 from gensim.models import KeyedVectors
 
-def load_all_data(dataloc, w2v_txt_path, idf_pickle_path):
-    print('loading pickle data')
+def load_all_data(dataloc):
+    print('loading json data')
     #
-    with open(dataloc+'BioASQ-trainingDataset6b.json', 'r') as f:
-        bioasq6_data = json.load(f)
-        bioasq6_data = dict( (q['id'], q) for q in bioasq6_data['questions'] )
+    train_data  = json.load(open(os.path.join(dataloc,'train-v1.1.json')))['data']
+    print len(train_data)
+    pprint(train_data[0]['title'])
+    pprint(train_data[0]['paragraphs'][0])
+    exit()
+    dev_data    = json.load(open(os.path.join(dataloc,'dev-v1.1.json')))['data']
+    print len(dev_data)
+    test_data   = json.load(open(os.path.join(dataloc,'dev-v1.1.json')))['data']
+    print len(test_data)
+    exit()
     #
-    with open(dataloc + 'bioasq_bm25_top100.test.pkl', 'rb') as f:
-        test_data = pickle.load(f)
-    with open(dataloc + 'bioasq_bm25_docset_top100.test.pkl', 'rb') as f:
-        test_docs = pickle.load(f)
-    with open(dataloc + 'bioasq_bm25_top100.dev.pkl', 'rb') as f:
-        dev_data = pickle.load(f)
-    with open(dataloc + 'bioasq_bm25_docset_top100.dev.pkl', 'rb') as f:
-        dev_docs = pickle.load(f)
-    with open(dataloc + 'bioasq_bm25_top100.train.pkl', 'rb') as f:
-        train_data = pickle.load(f)
-    with open(dataloc + 'bioasq_bm25_docset_top100.train.pkl', 'rb') as f:
-        train_docs = pickle.load(f)
-    print('loading words')
-    #
-    words           = {}
-    GetWords(train_data, train_docs, words)
-    GetWords(dev_data,   dev_docs,   words)
-    GetWords(test_data,  test_docs,  words)
+    # words           = {}
+    # GetWords(train_data, train_docs, words)
+    # GetWords(dev_data,   dev_docs,   words)
+    # GetWords(test_data,  test_docs,  words)
     # mgmx
     # print('loading idfs')
     # idf, max_idf    = load_idfs(idf_pickle_path, words)
     print('loading w2v')
+    w2v_txt_path    = os.path.join(dataloc, 'word2vec.6B.50d.txt')
     wv              = KeyedVectors.load_word2vec_format(w2v_txt_path, binary=False)
     wv              = dict([(word, wv[word]) for word in wv.vocab.keys() if(word in words)])
     # return test_data, test_docs, dev_data, dev_docs, train_data, train_docs, idf, max_idf, wv, bioasq6_data
-    return test_data, test_docs, dev_data, dev_docs, train_data, train_docs, wv, bioasq6_data
 
-
-
-w2v_fpath   = "/media/dpappas/dpappas_data/EMBEDDINGS/word2vec.6B.50d.txt"
+# -v1.1.json
+# /home/dpappas/for_ryan/squad_ir_data/'
+dataloc     = '/home/dpappas/for_ryan/squad_ir_data/'
+load_all_data(dataloc)
 
 
 
