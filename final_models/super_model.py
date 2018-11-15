51ea8efdbcf2c86eec47d49143f6584c870f3894
dpappas <Zrb6.NCC>
2018-11-16 01:13:54 +0200

    message

--- final_models/super_model.py
+++ final_models/super_model.py
@@ -1048,12 +1048,13 @@ class Sent_Posit_Drmm_Modeler(nn.Module):
             sent_add_feats      = torch.cat([gaf, sent_emit.unsqueeze(-1)])
             res.append(sent_add_feats)
         res = torch.stack(res)
-        if(self.sentence_out_method == 'MLP'):
-            res = self.sent_out_layer(res).squeeze(-1)
-        else:
-            res = self.apply_sent_res_bigru(res)
-        # ret = self.get_max(res).unsqueeze(0)
-        ret = self.get_kmax(res, k2)
+        #
+        res = self.sent_out_layer_1(res)
+        res = self.sent_out_activ_1(res)
+        res = self.sent_out_layer_2(res).squeeze(-1)
+        print(res)
+        print(res.size())
+        #
         res = torch.sigmoid(res)
         return ret, res
     def get_max(self, res):
