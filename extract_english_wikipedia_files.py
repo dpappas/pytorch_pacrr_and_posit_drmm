755d812f89557d759099d4661bbb92d48ffe69bc
dpappas <Zrb6.NCC>
2018-11-15 13:17:53 +0200

    message

---
+++ extract_english_wikipedia_files.py
@@ -0,0 +1,30 @@
+
+
+# wget https://dumps.wikimedia.org/other/static_html_dumps/current/en/wikipedia-en-html.tar.7z
+#
+# 7z x -so yourfile.tar.7z | tar xf - -C target_dir
+# 7z x -so wikipedia-en-html.tar.7z | tar xf - -C wikipedia-en-htmls
+#
+#
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
