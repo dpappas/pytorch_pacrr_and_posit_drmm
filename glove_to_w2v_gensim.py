335a771b1cec4afdd1b9c113236e2164cd2c0ac0
dpappas <Zrb6.NCC>
2018-11-15 12:02:39 +0200

    message

---
+++ glove_to_w2v_gensim.py
@@ -0,0 +1,16 @@
+
+
+from gensim.test.utils import datapath, get_tmpfile
+from gensim.models import KeyedVectors
+
+glove_file = datapath('/media/dpappas/dpappas_data/EMBEDDINGS/glove.6B.50d.txt')
+tmp_file = get_tmpfile("/media/dpappas/dpappas_data/EMBEDDINGS/word2vec.6B.50d.txt")
+
+from gensim.scripts.glove2word2vec import glove2word2vec
+glove2word2vec(glove_file, tmp_file)
+
+model = KeyedVectors.load_word2vec_format(tmp_file)
+
+
+
+
