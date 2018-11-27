47d66302260cd7dace33ed4de7dbc8c4fa55e035
dpappas <Zrb6.NCC>
2018-11-27 16:33:08 +0200

    message

--- get_elmo_embeddings.py
+++ get_elmo_embeddings.py
@@ -7,9 +7,11 @@
 
 
 from allennlp.modules.elmo import Elmo, batch_to_ids
-options_file    = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_options.json"
-weight_file     = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5"
-elmo            = Elmo(options_file, weight_file, 2, dropout=0)
+# options_file    = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_options.json"
+# weight_file     = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5"
+options_file    = "/home/dpappas/bioasq_all/elmo_weights/options.json"
+weight_file     = "/home/dpappas/bioasq_all/elmo_weights/weights.hdf5"
+elmo            = Elmo(options_file, weight_file, 1, dropout=0)
 
 # use batch_to_ids to convert sentences to character ids
 sentences       = [['First', 'sentence', '.'], ['Another', '.']]
