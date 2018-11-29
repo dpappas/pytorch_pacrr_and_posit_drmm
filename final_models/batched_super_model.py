5ea98872433e2f9e4b94e18a63cd76d2541c741c
dpappas <Zrb6.NCC>
2018-11-29 18:17:19 +0200

    message

--- final_models/batched_super_model.py
+++ final_models/batched_super_model.py
@@ -1458,6 +1458,11 @@ grep 'test MAP snippets:' super_model_20_11_2018_two_losses_run_0/model.log
 - seed documents ? 
 - scopus gia citation graphs
 - two weeks same hour
-- Review to flow process kai eidame afto to doc kai ta 
+- Review to flow process kai eidame afto to doc kai ta leme pali se 2 vdomades idia wra
+- kelly -> pico template gia na kanoume querries 
+- 
+- 
+- 
+
 
 '''
