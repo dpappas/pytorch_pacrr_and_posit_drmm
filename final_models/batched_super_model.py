1db319cd8c98d24c6e4363078c36602332cdf924
dpappas <Zrb6.NCC>
2018-11-30 01:15:48 +0200

    message

--- final_models/batched_super_model.py
+++ final_models/batched_super_model.py
@@ -841,7 +841,14 @@ def do_for_some_retrieved(docs, dato, retr_docs, data_for_revision, ret_data, us
             quest_lens          = [quest_embeds.shape[0]]
         )
         doc_emit_, gs_emits_    = doc_emit_[0], gs_emits_[0]
-        doc_res, extracted_from_one, all_emits = do_for_one_retrieved(doc_emit_, gs_emits_, datum['held_out_sents'], retr, doc_res, gold_snips)
+        doc_res, extracted_from_one, all_emits = do_for_one_retrieved(
+            doc_emit_,
+            gs_emits_,
+            datum['held_out_sents'],
+            retr,
+            doc_res,
+            gold_snips
+        )
         # is_relevant, the_sent_score, ncbi_pmid_link, the_actual_sent_text
         extracted_snippets.extend(extracted_from_one)
         #
