f20262971ba9ce379e09b30fdade1de7304f3c35
dpappas <Zrb6.NCC>
2018-11-29 17:29:54 +0200

    message

--- final_models/batched_super_model.py
+++ final_models/batched_super_model.py
@@ -752,18 +752,13 @@ def train_one(epoch, bioasq6_data, two_losses, use_sent_tokenizer):
         snip_loss_1             = (sn_d1_l + sn_d2_l)/2.0
         # print(snip_loss_1)
         losses_weights          = [0.5, 0.5]
+        print(snip_loss_1, cost_)
         cost_ = (losses_weights[0] * snip_loss_1) + (losses_weights[1] * cost_)
         # print(cost_)
         #
         cost_ = (losses_weights[0] * snip_loss_1) + (losses_weights[1] * cost_)
         exit()
         #
-        good_sent_tags, bad_sent_tags       = datum['good_sent_tags'], datum['bad_sent_tags']
-        if(two_losses):
-            sn_d1_l, sn_d2_l, sn_d3_l, sn_d4_l  = get_two_snip_losses(good_sent_tags, gs_emits_, bs_emits_)
-            snip_loss_1                         = sn_d1_l + sn_d2_l
-            losses_weights                      = [0.5, 0.5]
-            cost_                               = (losses_weights[0] * snip_loss_1) + (losses_weights[1] * cost_)
         #
         batch_acc.append(float(doc1_emit_ > doc2_emit_))
         epoch_acc.append(float(doc1_emit_ > doc2_emit_))
