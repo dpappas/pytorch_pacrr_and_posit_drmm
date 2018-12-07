b6e17065790f357efed3fd776078810a4624fbba
dpappas <Zrb6.NCC>
2018-12-07 11:54:11 +0200

    message

--- final_models/BCNN.py
+++ final_models/BCNN.py
@@ -398,7 +398,7 @@ def train_data_step2(instances, docs, wv, bioasq6_data, idf, max_idf):
         quest_tokens, quest_embeds  = get_embeds(tokenize(quest_text), wv)
         q_idfs                      = np.array([[idf_val(qw, idf, max_idf)] for qw in quest_tokens], 'float')
         #
-        if(use_sent_tokenizer == False or sum(good_sent_tags)>0):
+        if(sum(good_sent_tags)>0):
             yield {
                 'good_sents_embeds'     : good_sents_embeds,
                 'good_sents_escores'    : good_sents_escores,
