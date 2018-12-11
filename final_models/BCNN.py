79ae57faf6c9b36ff1ea3ac51a958db34c908764
dpappas <Zrb6.NCC>
2018-12-11 16:25:14 +0200

    message

--- final_models/BCNN.py
+++ final_models/BCNN.py
@@ -1162,26 +1162,26 @@ class BCNN(nn.Module):
         emit                = F.softmax(mlp_out, dim=-1)[:,1]
         return cost, emit
 
-# laptop
-w2v_bin_path        = '/home/dpappas/for_ryan/fordp/pubmed2018_w2v_30D.bin'
-idf_pickle_path     = '/home/dpappas/for_ryan/fordp/idf.pkl'
-dataloc             = '/home/dpappas/for_ryan/'
-eval_path           = '/home/dpappas/for_ryan/eval/run_eval.py'
-retrieval_jar_path  = '/home/dpappas/NetBeansProjects/my_bioasq_eval_2/dist/my_bioasq_eval_2.jar'
-use_cuda            = True
-odd                 = '/home/dpappas/'
-get_embeds          = get_embeds_use_unk
-
-# # atlas , cslab243
-# w2v_bin_path        = '/home/dpappas/bioasq_all/pubmed2018_w2v_30D.bin'
-# idf_pickle_path     = '/home/dpappas/bioasq_all/idf.pkl'
-# dataloc             = '/home/dpappas/bioasq_all/bioasq_data/'
-# eval_path           = '/home/dpappas/bioasq_all/eval/run_eval.py'
-# retrieval_jar_path  = '/home/dpappas/bioasq_all/dist/my_bioasq_eval_2.jar'
+# # laptop
+# w2v_bin_path        = '/home/dpappas/for_ryan/fordp/pubmed2018_w2v_30D.bin'
+# idf_pickle_path     = '/home/dpappas/for_ryan/fordp/idf.pkl'
+# dataloc             = '/home/dpappas/for_ryan/'
+# eval_path           = '/home/dpappas/for_ryan/eval/run_eval.py'
+# retrieval_jar_path  = '/home/dpappas/NetBeansProjects/my_bioasq_eval_2/dist/my_bioasq_eval_2.jar'
 # use_cuda            = True
 # odd                 = '/home/dpappas/'
 # get_embeds          = get_embeds_use_unk
-# # get_embeds          = get_embeds_use_only_unk
+
+# atlas , cslab243
+w2v_bin_path        = '/home/dpappas/bioasq_all/pubmed2018_w2v_30D.bin'
+idf_pickle_path     = '/home/dpappas/bioasq_all/idf.pkl'
+dataloc             = '/home/dpappas/bioasq_all/bioasq_data/'
+eval_path           = '/home/dpappas/bioasq_all/eval/run_eval.py'
+retrieval_jar_path  = '/home/dpappas/bioasq_all/dist/my_bioasq_eval_2.jar'
+use_cuda            = True
+odd                 = '/home/dpappas/'
+get_embeds          = get_embeds_use_unk
+# get_embeds          = get_embeds_use_only_unk
 
 embedding_dim       = 30
 additional_feats    = 10
@@ -1199,7 +1199,7 @@ print(avgdl, mean, deviation)
 # model_type          = 'BCNN'
 model_type          = 'PDRMM'
 optim_type          = 'ADAM'
-lr                  = 0.01
+lr                  = 0.001
 
 model, optimizer    = setup_optim_model()
 hdlr                = None
