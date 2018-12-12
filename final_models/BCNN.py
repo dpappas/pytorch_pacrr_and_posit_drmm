75c6836623b3c04d915d0db0f58dfa76651843c3
dpappas <Zrb6.NCC>
2018-12-12 12:44:20 +0200

    message

--- final_models/BCNN.py
+++ final_models/BCNN.py
@@ -1207,8 +1207,11 @@ print(avgdl, mean, deviation)
 model_type          = 'PDRMM'
 optim_type          = 'ADAM'
 lr                  = 0.001
-
 model, optimizer    = setup_optim_model()
+resume_from_path    = '/home/dpappas/PDRMM_ADAM_001_run_0/best_checkpoint.pth.tar'
+load_model_from_checkpoint(model, resume_from_path)
+print(model.out_conv.weight[0].sum())
+
 hdlr                = None
 for run in range(1):
     setup_random(run)
