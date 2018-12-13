bd42f0e9fd786b5d1789c3712e156fdc967bf80e
dpappas <Zrb6.NCC>
2018-12-13 20:50:19 +0200

    message

--- play_with_convolutions.py
+++ play_with_convolutions.py
@@ -5,7 +5,7 @@ emb_size    = 100
 ngram       = 3
 input       = torch.autograd.Variable(torch.randn(20, 1, 50, emb_size))
 m           = torch.nn.Conv2d(1, emb_size, kernel_size=(ngram, emb_size), stride=(1, 1), padding=(ngram-1, 0))
-output      = m(input)
+output      = m(input).transpose(1, 3)[:,:,:input.size(-2),:]
 print(input.size())
 print(output.size())
 
@@ -15,7 +15,7 @@ emb_size    = 100
 input       = input.squeeze(1).transpose(-1,-2)
 print(input.size())
 m           = torch.nn.Conv1d(emb_size, emb_size, ngram, padding=ngram-1, bias=True)
-output      = m(input)
+output      = m(input)[:,:,:input.size(-1)]
 print(output.size())
 
 
