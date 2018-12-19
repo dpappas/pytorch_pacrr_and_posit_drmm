b9f54178b3e319229bdc7b4f0518b8b8715d1cd7
dpappas <Zrb6.NCC>
2018-12-19 14:16:39 +0200

    message

--- final_models/BCNN.py
+++ final_models/BCNN.py
@@ -1541,19 +1541,19 @@ class ABCNN3_PDRMM(nn.Module):
         the_input_global_pool   = F.avg_pool1d(the_input, the_input.size(-1), stride=None)
         return the_input_global_pool.squeeze(-1)
     def forward(self, doc1_sents_embeds, question_embeds, q_idfs, sents_gaf, sents_labels):
-        q_idfs              = autograd.Variable(torch.FloatTensor(q_idfs),              requires_grad=False)
-        sents_labels        = autograd.Variable(torch.LongTensor(sents_labels),     requires_grad=False)
-        sents_gaf           = autograd.Variable(torch.FloatTensor(sents_gaf),       requires_grad=False)
-        question_embeds     = autograd.Variable(torch.FloatTensor(question_embeds), requires_grad=False).unsqueeze(0).transpose(-1, -2)
+        q_idfs                  = autograd.Variable(torch.FloatTensor(q_idfs),              requires_grad=False)
+        sents_labels            = autograd.Variable(torch.LongTensor(sents_labels),     requires_grad=False)
+        sents_gaf               = autograd.Variable(torch.FloatTensor(sents_gaf),       requires_grad=False)
+        question_embeds         = autograd.Variable(torch.FloatTensor(question_embeds), requires_grad=False).unsqueeze(0).transpose(-1, -2)
         if(use_cuda):
-            sents_labels    = sents_labels.cuda()
-            sents_gaf       = sents_gaf.cuda()
-            question_embeds = question_embeds.cuda()
-            q_idfs          = q_idfs.cuda()
+            sents_labels        = sents_labels.cuda()
+            sents_gaf           = sents_gaf.cuda()
+            question_embeds     = question_embeds.cuda()
+            q_idfs              = q_idfs.cuda()
         #
-        quest_global_pool   = F.avg_pool1d(question_embeds, question_embeds.size(-1), stride=None)
+        quest_global_pool       = F.avg_pool1d(question_embeds, question_embeds.size(-1), stride=None)
         #
-        mlp_in = []
+        mlp_in                  = []
         for i in range(len(doc1_sents_embeds)):
             sent_embed          = autograd.Variable(torch.FloatTensor(doc1_sents_embeds[i]), requires_grad=False).unsqueeze(0).transpose(-1,-2)
             if (use_cuda):
@@ -1579,35 +1579,22 @@ class ABCNN3_PDRMM(nn.Module):
             #
             cs2                 = self.my_cosine_sim(quest_window_pool.transpose(-1, -2), sent_window_pool.transpose(-1, -2))
             cs2                 = self.pooling_method(cs2.squeeze(0))[:cs0.size(0)]
-            if(check_if_any_nan(cs2)):
-                print('quest_window_pool')
-                print(quest_window_pool)
-                print(sent_window_pool.size())
-                print(20 * '-')
-                print('sent_window_pool')
-                print(sent_window_pool)
-                print(sent_window_pool.size())
-                print(20 * '-')
-                print('cs2')
-                print(cs2)
-                print(20 * '-')
-                exit()
             #
-            q_weights       = torch.cat([quest_window_pool.squeeze(0).transpose(-2, -1)[:q_idfs.size(0)], q_idfs], -1)
-            q_weights       = self.q_weights_mlp(q_weights).squeeze(-1)
-            q_weights       = F.softmax(q_weights, dim=-1)
-            sent_emit       = self.get_output([cs0, cs1, cs2, cs3], q_weights)
+            q_weights           = torch.cat([quest_window_pool.squeeze(0).transpose(-2, -1)[:q_idfs.size(0)], q_idfs], -1)
+            q_weights           = self.q_weights_mlp(q_weights).squeeze(-1)
+            q_weights           = F.softmax(q_weights, dim=-1)
+            sent_emit           = self.get_output([cs0, cs1, cs2, cs3], q_weights)
             #
             mlp_in.append(torch.cat([sim1, sim2, sim3, sent_emit.unsqueeze(-1), sents_gaf[i]], dim=-1))
             #
-        mlp_in              = torch.stack(mlp_in, dim=0)
+        mlp_in                  = torch.stack(mlp_in, dim=0)
         #
-        mlp_out             = self.linear_out(mlp_in)
+        mlp_out                 = self.linear_out(mlp_in)
         #
-        mlp_out             = F.log_softmax(mlp_out, dim=-1)
-        cost                = F.nll_loss(mlp_out, sents_labels, weight=None, reduction='elementwise_mean')
+        mlp_out                 = F.log_softmax(mlp_out, dim=-1)
+        cost                    = F.nll_loss(mlp_out, sents_labels, weight=None, reduction='elementwise_mean')
         #
-        emit                = F.softmax(mlp_out, dim=-1)[:,1]
+        emit                    = F.softmax(mlp_out, dim=-1)[:,1]
         return cost, emit
 
 # # laptop
