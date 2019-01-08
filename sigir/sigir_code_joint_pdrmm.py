ec903c4c216ea0b3a64fa6a7676b2bcfb94c5da8
dpappas <Zrb6.NCC>
2019-01-08 14:51:28 +0200

    message

--- sigir/sigir_code_joint_pdrmm.py
+++ sigir/sigir_code_joint_pdrmm.py
@@ -1347,16 +1347,16 @@ class Sent_Posit_Drmm_Modeler(nn.Module):
 
 use_cuda = torch.cuda.is_available()
 
-# laptop
-w2v_bin_path        = '/home/dpappas/for_ryan/fordp/pubmed2018_w2v_30D.bin'
-idf_pickle_path     = '/home/dpappas/for_ryan/fordp/idf.pkl'
-dataloc             = '/home/dpappas/for_ryan/'
-eval_path           = '/home/dpappas/for_ryan/eval/run_eval.py'
-retrieval_jar_path  = '/home/dpappas/NetBeansProjects/my_bioasq_eval_2/dist/my_bioasq_eval_2.jar'
-# use_cuda            = False
-odd                 = '/home/dpappas/'
-# get_embeds          = get_embeds_use_unk
-# get_embeds          = get_embeds_use_only_unk
+# # laptop
+# w2v_bin_path        = '/home/dpappas/for_ryan/fordp/pubmed2018_w2v_30D.bin'
+# idf_pickle_path     = '/home/dpappas/for_ryan/fordp/idf.pkl'
+# dataloc             = '/home/dpappas/for_ryan/'
+# eval_path           = '/home/dpappas/for_ryan/eval/run_eval.py'
+# retrieval_jar_path  = '/home/dpappas/NetBeansProjects/my_bioasq_eval_2/dist/my_bioasq_eval_2.jar'
+# # use_cuda            = False
+# odd                 = '/home/dpappas/'
+# # get_embeds          = get_embeds_use_unk
+# # get_embeds          = get_embeds_use_only_unk
 
 # # cslab241
 # w2v_bin_path        = '/home/dpappas/for_ryan/pubmed2018_w2v_30D.bin'
@@ -1366,14 +1366,14 @@ odd                 = '/home/dpappas/'
 # retrieval_jar_path  = '/home/dpappas/bioasq_eval/dist/my_bioasq_eval_2.jar'
 # odd                 = '/home/dpappas/'
 
-# # atlas , cslab243
-# w2v_bin_path        = '/home/dpappas/bioasq_all/pubmed2018_w2v_30D.bin'
-# idf_pickle_path     = '/home/dpappas/bioasq_all/idf.pkl'
-# dataloc             = '/home/dpappas/bioasq_all/bioasq_data/'
-# eval_path           = '/home/dpappas/bioasq_all/eval/run_eval.py'
-# retrieval_jar_path  = '/home/dpappas/bioasq_all/dist/my_bioasq_eval_2.jar'
-# odd                 = '/home/dpappas/'
-# # get_embeds          = get_embeds_use_unk
+# atlas , cslab243
+w2v_bin_path = '/home/dpappas/bioasq_all/pubmed2018_w2v_30D.bin'
+idf_pickle_path = '/home/dpappas/bioasq_all/idf.pkl'
+dataloc = '/home/dpappas/bioasq_all/bioasq_data/'
+eval_path = '/home/dpappas/bioasq_all/eval/run_eval.py'
+retrieval_jar_path = '/home/dpappas/bioasq_all/dist/my_bioasq_eval_2.jar'
+odd = '/home/dpappas/'
+# get_embeds          = get_embeds_use_unk
 # get_embeds          = get_embeds_use_only_unk
 
 # # gpu_server_1
