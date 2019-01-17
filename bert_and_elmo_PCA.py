ca02a791b11c907b87b375c5153eca611f814287
dpappas <Zrb6.NCC>
2019-01-17 13:52:03 +0200

    message

--- bert_and_elmo_PCA.py
+++ bert_and_elmo_PCA.py
@@ -16,6 +16,60 @@ for f in tqdm(os.listdir(diri), ascii=True):
     d = pickle.load(open(fpath, 'rb'))
     #
     if (mat is None):
+        mat = np.concatenate(d['title_sent_elmo_embeds'] + d['abs_sent_elmo_embeds'], axis=0)
+    else:
+        mat = np.concatenate([mat] + d['title_sent_elmo_embeds'] + d['abs_sent_elmo_embeds'], axis=0)
+    if (mat.shape[0] > 1000):
+        transformer.partial_fit(mat)
+        mat = None
+
+filename = '/home/dpappas/bioasq_all/pca_elmo_transformer.sav'
+pickle.dump(transformer, open(filename, 'wb'))
+
+#####################
+
+import os
+import pickle
+from tqdm import tqdm
+from sklearn.decomposition import IncrementalPCA
+
+diri = '/home/dpappas/bioasq_all/bert_elmo_embeds/'
+filename = '/home/dpappas/bioasq_all/pca_bert_transformer.sav'
+transformer = pickle.load(open(filename, 'rb'))
+
+odiri = '/home/dpappas/bioasq_all/bert_embeds_after_pca/'
+if (not os.path.exists(odiri)):
+    os.makedirs(odiri)
+
+for f in tqdm(os.listdir(diri), ascii=True):
+    fpath = os.path.join(diri, f)
+    opath = os.path.join(odiri, f)
+    if (not os.path.exists(opath)):
+        d = pickle.load(open(fpath, 'rb'))
+        od = {
+            'title_elmo_average_embeds': [transformer.transform(m) for m in d['title_sent_elmo_embeds']],
+            'abs_elmo_average_embeds': [transformer.transform(m) for m in d['abs_sent_elmo_embeds']],
+            'mesh_elmo_average_embeds': [transformer.transform(m) for m in d['mesh_elmo_embeds']],
+        }
+        pickle.dump(od, open(opath, 'wb'), protocol=2)
+
+all_qs = pickle.load(open('/home/dpappas/bioasq_all/all_quest_embeds.p', 'rb'))
+all_qs_pca = {}
+
+for quest in tqdm(all_qs.keys(), ascii=True):
+    all_qs_pca[quest] = [transformer.transform(m) for m in all_qs[quest]['title_sent_elmo_embeds']]
+
+pickle.dump(all_qs_pca, open('/home/dpappas/bioasq_all/all_quest_elmo_embeds_after_pca.p', 'wb'), protocol=2)
+
+exit()
+
+transformer = IncrementalPCA(n_components=50)
+for f in tqdm(os.listdir(diri), ascii=True):
+    m += 1
+    fpath = os.path.join(diri, f)
+    d = pickle.load(open(fpath, 'rb'))
+    #
+    if (mat is None):
         mat = np.concatenate(d['title_bert_average_embeds'] + d['abs_bert_average_embeds'], axis=0)
     else:
         mat = np.concatenate([mat] + d['title_bert_average_embeds'] + d['abs_bert_average_embeds'], axis=0)
@@ -75,3 +129,9 @@ for quest in tqdm(all_qs.keys(), ascii=True):
     all_qs_pca[quest] = [transformer.transform(m) for m in all_qs[quest]['title_bert_average_embeds']]
 
 pickle.dump(all_qs_pca, open('/home/dpappas/bioasq_all/all_quest_bert_embeds_after_pca.p', 'wb'), protocol=2)
+
+python3
+.6
+import pickle
+
+d = pickle.load(open('/home/dpappas/bioasq_all/bert_elmo_embeds/1002140.p', 'rb'))
