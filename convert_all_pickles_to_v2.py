7ae0bed5c6f11e0cd2c94c836a36a0f223510dd8
dpappas <Zrb6.NCC>
2019-01-26 23:28:47 +0200

    message

--- convert_all_pickles_to_v2.py
+++ convert_all_pickles_to_v2.py
@@ -3,9 +3,10 @@ import pickle
 import os
 from tqdm import tqdm
 
-diri = '/home/dpappas/bioasq_all/bert_elmo_embeds/'
-
-for f in tqdm(os.listdir(diri)):
+diri    = '/home/dpappas/bioasq_all/bert_elmo_embeds/'
+pbar    = tqdm(os.listdir(diri))
+for f in pbar:
+    pbar.set_description(f)
     d = pickle.load(open(os.path.join(diri, f), 'rb'))
     pickle.dump(d, open(os.path.join(diri, f), 'wb'), protocol=2)
 
