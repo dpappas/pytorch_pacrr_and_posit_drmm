1e45bcdebb27dc7955bc1378b02f7c2aefd83b10
dpappas <Zrb6.NCC>
2019-02-15 22:59:43 +0200

    message

--- final_models/classic_pdrmm.py
+++ final_models/classic_pdrmm.py
@@ -889,18 +889,14 @@ for run in range(0, 5):
     #
     best_dev_map, test_map = None, None
     for epoch in range(max_epoch):
-        train_one(epoch + 1)
+        # train_one(epoch + 1)
         epoch_dev_map = get_one_map('dev', dev_data, dev_docs)
         if (best_dev_map is None or epoch_dev_map >= best_dev_map):
-            best_dev_map = epoch_dev_map
-            test_map = get_one_map('test', test_data, test_docs)
-            save_checkpoint(epoch, model, best_dev_map, optimizer,
-                            filename=os.path.join(odir, 'best_checkpoint.pth.tar'))
-        print('epoch:{:02d} epoch_dev_map:{:.4f} best_dev_map:{:.4f} test_map:{:.4f}'.format(epoch + 1, epoch_dev_map,
-                                                                                             best_dev_map, test_map))
-        logger.info(
-            'epoch:{:02d} epoch_dev_map:{:.4f} best_dev_map:{:.4f} test_map:{:.4f}'.format(epoch + 1, epoch_dev_map,
-                                                                                           best_dev_map, test_map))
+            best_dev_map    = epoch_dev_map
+            test_map        = get_one_map('test', test_data, test_docs)
+            save_checkpoint(epoch, model, best_dev_map, optimizer, filename=os.path.join(odir, 'best_checkpoint.pth.tar'))
+        print('epoch:{:02d} epoch_dev_map:{:.4f} best_dev_map:{:.4f} test_map:{:.4f}'.format(epoch + 1, epoch_dev_map, best_dev_map, test_map))
+        logger.info('epoch:{:02d} epoch_dev_map:{:.4f} best_dev_map:{:.4f} test_map:{:.4f}'.format(epoch + 1, epoch_dev_map, best_dev_map, test_map))
 
 
 '''
