723cae2a272a6e6954f7c520a1ace8accdf9a76b
dpappas <Zrb6.NCC>
2019-02-21 16:45:02 +0200

    message

--- sigir/print_worse_results.py
+++ sigir/print_worse_results.py
@@ -6,6 +6,7 @@ from tqdm import tqdm
 
 fpath = '/home/dpappas/v3 dev_data_for_revision.json'
 gpath = '/home/dpappas/v3 dev_gold_bioasq.json'
+opath = '/home/dpappas/some_data_for_revision.json'
 
 edata = json.load(open(fpath))
 
@@ -15,19 +16,56 @@ for item in tqdm(data['questions']):
     all_snips           = [sn['text'] for sn in item['snippets']]
     gdata[item['id']]   = all_snips
 
+def snip_is_relevant(one_sent, gold_snips):
+    # print one_sent
+    # pprint(gold_snips)
+    return int(
+        any(
+            [
+                (one_sent.encode('ascii', 'ignore')  in gold_snip.encode('ascii','ignore'))
+                or
+                (gold_snip.encode('ascii', 'ignore') in one_sent.encode('ascii','ignore'))
+                for gold_snip in gold_snips
+            ]
+        )
+    )
+
+all_out_data = []
 for qid, val in tqdm(edata.items()):
     qtext = val['query_text']
     for pmid in val['snippets']:
         for snip in val['snippets'][pmid]:
-            g = snip[0]
-            e = snip[1]
+            gold_truth = snip[0]
+            emit = snip[1]
             sent = snip[-1]
-            if(g-e>.90):
-                snips_containing    = [sn for sn in gdata[qid] if(sn in sent or sent in sn)]
-                sn                  = snips_containing[0] if(len(snips_containing)>0) else 'NONE'
-                print('{} | {} | {} | {} | {} | {}'.format(qtext, pmid, g, e, sent, sn))
-                # print(20 * '-')
+            if(gold_truth-emit>.90):
+                for gold_snip in gdata[qid]:
+                    if(sent in snip or snip in sent):
+                        all_out_data.append(
+                            {
+                                'question_text' : qtext,
+                                'pmid'          : pmid,
+                                'gold_truth'    : gold_truth,
+                                'emit'          : emit,
+                                'sent'          : sent,
+                                'gold_snip'     : gold_snip
+                            }
+                        )
+            elif(emit-gold_truth>.80):
+                all_out_data.append(
+                    {
+                        'question_text': qtext,
+                        'pmid': pmid,
+                        'gold_truth': gold_truth,
+                        'emit': emit,
+                        'sent': sent,
+                        'gold_snip': 'NONE FOUND'
+                    }
+                )
 
+with open(opath, 'w') as f:
+    f.write(json.dumps(all_out_data, indent=4, sort_keys=True))
+    f.close()
 
 
 
