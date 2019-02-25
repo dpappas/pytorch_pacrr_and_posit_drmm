3fd47f2e8ef08b7dd8edee8d197fdb58c838dd6a
dpappas <Zrb6.NCC>
2019-02-25 14:58:33 +0200

    message

--- get_embeddings_distances.py
+++ get_embeddings_distances.py
@@ -3,12 +3,14 @@
 
 from gensim.models import KeyedVectors
 
-w2v_bin_path        = '/home/dpappas/for_ryan/fordp/pubmed2018_w2v_30D.bin'
-wv_from_bin = KeyedVectors.load_word2vec_format(w2v_bin_path, binary=True)
+w2v_bin_path    = '/home/dpappas/for_ryan/fordp/pubmed2018_w2v_30D.bin'
+wv_from_bin     = KeyedVectors.load_word2vec_format(w2v_bin_path, binary=True)
 
-
-
-
+wv_from_bin.most_similar('birth')
+print(wv_from_bin.distance('birth', 'birth'))
+print(wv_from_bin.distance('birth', 'child'))
+print(wv_from_bin.distance('birth', 'infants'))
+print(wv_from_bin.distance('birth', 'chair'))
 
 
 
