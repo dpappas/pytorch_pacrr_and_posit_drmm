9ac680e61353b00d9fbcf0a813f2773db5585c67
dpappas <Zrb6.NCC>
2019-02-25 13:21:08 +0200

    message

--- convert_bert_model_to_pytorch.py
+++ convert_bert_model_to_pytorch.py
@@ -2,11 +2,11 @@
 import os
 from pytorch_pretrained_bert.convert_tf_checkpoint_to_pytorch import convert_tf_checkpoint_to_pytorch
 
-if (not os.path.exists('/home/dpappas/Downloads/F_BERT/Biobert/pubmed_pmc_470k/pytorch_model.bin')):
+if (not os.path.exists('/home/dpappas/bioasq_all/F_BERT/Biobert/pubmed_pmc_470k/pytorch_model.bin')):
     convert_tf_checkpoint_to_pytorch(
-        '/home/dpappas/Downloads/F_BERT/Biobert/pubmed_pmc_470k/biobert_model.ckpt',
-        '/home/dpappas/Downloads/F_BERT/Biobert/pubmed_pmc_470k/bert_config.json',
-        '/home/dpappas/Downloads/F_BERT/Biobert/pubmed_pmc_470k/pytorch_model.bin'
+        '/home/dpappas/bioasq_all/F_BERT/Biobert/pubmed_pmc_470k/biobert_model.ckpt',
+        '/home/dpappas/bioasq_all/F_BERT/Biobert/pubmed_pmc_470k/bert_config.json',
+        '/home/dpappas/bioasq_all/F_BERT/Biobert/pubmed_pmc_470k/pytorch_model.bin'
     )
 
 
