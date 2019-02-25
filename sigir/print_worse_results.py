b76e4dda912d4fdccbc33b8e94eae78fd17a8fa3
dpappas <Zrb6.NCC>
2019-02-25 14:01:11 +0200

    message

--- sigir/print_worse_results.py
+++ sigir/print_worse_results.py
@@ -32,14 +32,6 @@ def get_one_bad():
                     'sent': sent,
                     'gold_snip': 'NONE FOUND'
                 }
-            # pprint({
-            #         'question_text': qtext,
-            #         'pmid': pmid,
-            #         'gold_truth': gold_truth,
-            #         'emit': emit,
-            #         'sent': sent,
-            #         'gold_snip': 'NONE FOUND'
-            #     })
     return None
 
 def get_one_good():
@@ -62,18 +54,58 @@ def get_one_good():
                     )
     return None
 
+def get_all_bad():
+    ret = []
+    for snip in val['snippets'][pmid]:
+        gold_truth = snip[0]
+        emit = snip[1]
+        sent = snip[-1]
+        if(emit-gold_truth>.70):
+            ret.append(
+                {
+                    'question_text': qtext,
+                    'pmid': pmid,
+                    'gold_truth': gold_truth,
+                    'emit': emit,
+                    'sent': sent,
+                    'gold_snip': 'NONE FOUND'
+                }
+            )
+    return ret
+
+def get_all_good():
+    ret = []
+    for snip in val['snippets'][pmid]:
+        gold_truth  = snip[0]
+        emit        = snip[1]
+        sent        = snip[-1]
+        if(gold_truth-emit>.80):
+            for gold_snip in gdata[qid]:
+                if(sent in gold_snip or gold_snip in sent):
+                    ret.append(
+                        {
+                            'question_text' : qtext,
+                            'pmid'          : pmid,
+                            'gold_truth'    : gold_truth,
+                            'emit'          : emit,
+                            'sent'          : sent,
+                            'gold_snip'     : gold_snip
+                        }
+                    )
+    return ret
+
 import pickle
 dataloc = '/home/dpappas/for_ryan/'
 with open(dataloc + 'bioasq_bm25_top100.dev.pkl', 'rb') as f:
     dev_data = pickle.load(f)
 
-pprint(
-    [
-        d for d in dev_data['queries']
-        if(d['query_id'] == '58a93877ee23e0236b000001')
-    ]
-)
-exit()
+# pprint(
+#     [
+#         d for d in dev_data['queries']
+#         if(d['query_id'] == '58a93877ee23e0236b000001')
+#     ]
+# )
+# exit()
 
 fpath = '/home/dpappas/v3 dev_data_for_revision.json'
 gpath = '/home/dpappas/v3 dev_gold_bioasq.json'
@@ -101,8 +133,6 @@ for qid, val in tqdm(edata.items()):
         if(g is not None):
             all_out_data_good.append(g)
 
-# exit()
-
 with open(opath, 'w') as f:
     all_out_data = {
         'GT_1': all_out_data_good,
@@ -111,4 +141,21 @@ with open(opath, 'w') as f:
     f.write(json.dumps(all_out_data, indent=4, sort_keys=True))
     f.close()
 
+############################
 
+all_out_data_good   = []
+all_out_data_bad    = []
+for qid, val in tqdm(edata.items()):
+    qtext = val['query_text']
+    for pmid in val['snippets']:
+        all_out_data_bad.extend(get_all_bad())
+        all_out_data_good.extend(get_all_good())
+
+opath = '/home/dpappas/all_data_for_revision.json'
+with open(opath, 'w') as f:
+    all_out_data = {
+        'GT_1': all_out_data_good,
+        'GT_0': all_out_data_bad
+    }
+    f.write(json.dumps(all_out_data, indent=4, sort_keys=True))
+    f.close()
