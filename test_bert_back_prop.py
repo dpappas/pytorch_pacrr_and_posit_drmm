f38a204603d0ece240063e24e81191e7a3661b8c
dpappas <Zrb6.NCC>
2019-02-25 11:26:21 +0200

    message

--- test_bert_back_prop.py
+++ test_bert_back_prop.py
@@ -41,6 +41,15 @@ class InputFeatures(object):
         self.segment_ids = segment_ids
         self.label_id = label_id
 
+def fix_bert_tokens(tokens):
+    ret = []
+    for t in tokens:
+        if (t.startswith('##')):
+            ret[-1] = ret[-1] + t[2:]
+        else:
+            ret.append(t)
+    return ret
+
 def _truncate_seq_pair(tokens_a, tokens_b, max_length):
     """Truncates a sequence pair in place to the maximum length."""
     ####
@@ -66,46 +75,21 @@ def convert_examples_to_features(examples, label_list, max_seq_length, tokenizer
         tokens_b = None
         if example.text_b:
             tokens_b = tokenizer.tokenize(example.text_b)
-            # Modifies `tokens_a` and `tokens_b` in place so that the total
-            # length is less than the specified length.
-            # Account for [CLS], [SEP], [SEP] with "- 3"
             _truncate_seq_pair(tokens_a, tokens_b, max_seq_length - 3)
         else:
-            # Account for [CLS] and [SEP] with "- 2"
             if len(tokens_a) > max_seq_length - 2:
                 tokens_a = tokens_a[:(max_seq_length - 2)]
         ####
-        # The convention in BERT is:
-        # (a) For sequence pairs:
-        #  tokens:   [CLS] is this jack ##son ##ville ? [SEP] no it is not . [SEP]
-        #  type_ids: 0   0  0    0    0     0       0 0    1  1  1  1   1 1
-        # (b) For single sequences:
-        #  tokens:   [CLS] the dog is hairy . [SEP]
-        #  type_ids: 0   0   0   0  0     0 0
-        #
-        # Where "type_ids" are used to indicate whether this is the first
-        # sequence or the second sequence. The embedding vectors for `type=0` and
-        # `type=1` were learned during pre-training and are added to the wordpiece
-        # embedding vector (and position vector). This is not *strictly* necessary
-        # since the [SEP] token unambigiously separates the sequences, but it makes
-        # it easier for the model to learn the concept of sequences.
-        #
-        # For classification tasks, the first vector (corresponding to [CLS]) is
-        # used as as the "sentence vector". Note that this only makes sense because
-        # the entire model is fine-tuned.
         tokens          = ["[CLS]"] + tokens_a + ["[SEP]"]
         segment_ids     = [0] * len(tokens)
         ####
         if tokens_b:
-            tokens += tokens_b + ["[SEP]"]
+            tokens      += tokens_b + ["[SEP]"]
             segment_ids += [1] * (len(tokens_b) + 1)
         input_ids       = tokenizer.convert_tokens_to_ids(tokens)
         ####
-        # The mask has 1 for real tokens and 0 for padding tokens. Only real
-        # tokens are attended to.
         input_mask      = [1] * len(input_ids)
         ####
-        # Zero-pad up to the sequence length.
         padding         = [0] * (max_seq_length - len(input_ids))
         input_ids       += padding
         input_mask      += padding
@@ -115,16 +99,8 @@ def convert_examples_to_features(examples, label_list, max_seq_length, tokenizer
         assert len(input_mask)  == max_seq_length
         assert len(segment_ids) == max_seq_length
         ####
-        label_id = label_map[example.label]
-        if ex_index < 5:
-            logger.info("*** Example ***")
-            logger.info("guid: %s" % (example.guid))
-            logger.info("tokens: %s" % " ".join([str(x) for x in tokens]))
-            logger.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
-            logger.info("input_mask: %s" % " ".join([str(x) for x in input_mask]))
-            logger.info("segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
-            logger.info("label: %s (id = %d)" % (example.label, label_id))
-        in_f = InputFeatures(
+        label_id    = label_map[example.label]
+        in_f        = InputFeatures(
             input_ids=input_ids, input_mask=input_mask, segment_ids=segment_ids, label_id=label_id
         )
         in_f.tokens = tokens
@@ -132,33 +108,43 @@ def convert_examples_to_features(examples, label_list, max_seq_length, tokenizer
     return features
 
 bert_model      = 'bert-base-uncased'
-max_seq_length  = 200
+max_seq_length  = 50
 eval_batch_size = 1
 cache_dir       = '/home/dpappas/bert_cache/'
 label_list      = ['0', '1']
 no_cuda         = False
-device          = torch.device("cuda" if torch.cuda.is_available() and not no_cuda else "cpu")
+# device          = torch.device("cpu")
+device          = torch.device("cuda")
 
 model = BertForSequenceClassification.from_pretrained(
-    bert_model,
-    cache_dir=PYTORCH_PRETRAINED_BERT_CACHE / 'distributed_{}'.format(-1),
-    num_labels=2
-    # , cache_dir=cache_dir
+    bert_model, cache_dir=PYTORCH_PRETRAINED_BERT_CACHE / 'distributed_{}'.format(-1), num_labels=2
 )
 model.to(device)
 
 tokenizer       = BertTokenizer.from_pretrained(bert_model, do_lower_case=True, cache_dir=cache_dir)
-
-eval_examples   = [
-    InputExample(
-        guid    ='example_dato_1',
-        text_a  ='what is your name?',
-        # text_b='my name is kostakis',
-        text_b=None,
-        label   ='1'
-    )
-]
+eval_examples   = [InputExample(guid='example_dato_1', text_a='my name is kostakis', text_b=None, label='1')]
 eval_features   = convert_examples_to_features(eval_examples, label_list, max_seq_length, tokenizer)
+
+for eval_feat in tqdm(eval_features, desc="Evaluating"):
+    input_ids   = torch.tensor([eval_feat.input_ids], dtype=torch.long).to(device)
+    input_mask  = torch.tensor([eval_feat.input_mask], dtype=torch.long).to(device)
+    segment_ids = torch.tensor([eval_feat.segment_ids], dtype=torch.long).to(device)
+    label_ids   = torch.tensor([eval_feat.label_id], dtype=torch.long).to(device)
+    tokens      = eval_feat.tokens
+    print(tokens)
+    print(fix_bert_tokens(tokens))
+    with torch.no_grad():
+        tmp_eval_loss       = model(input_ids, segment_ids, input_mask, label_ids)
+        logits              = model(input_ids, segment_ids, input_mask)
+        token_embeds, pooled_output   = model.bert(input_ids, segment_ids, input_mask, output_all_encoded_layers=False)
+        print(token_embeds.size())
+        print(pooled_output.size())
+        tok_inds = [i for i in range(len(tokens)) if(not tokens[i].startswith('##'))]
+        print(pooled_output[tok_inds,:].size())
+
+exit()
+
+
 all_input_ids   = torch.tensor([f.input_ids     for f in eval_features], dtype=torch.long)
 all_input_mask  = torch.tensor([f.input_mask    for f in eval_features], dtype=torch.long)
 all_segment_ids = torch.tensor([f.segment_ids   for f in eval_features], dtype=torch.long)
@@ -170,6 +156,7 @@ model.eval()
 
 for input_ids, input_mask, segment_ids, label_ids in tqdm(eval_dataloader, desc="Evaluating"):
     input_ids   = input_ids.to(device)
+    print(input_ids.size())
     input_mask  = input_mask.to(device)
     segment_ids = segment_ids.to(device)
     label_ids   = label_ids.to(device)
