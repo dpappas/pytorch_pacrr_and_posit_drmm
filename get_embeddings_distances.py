dcb46cb8d8068558a748265630495d0f67dbc556
dpappas <Zrb6.NCC>
2019-02-25 14:56:28 +0200

    message

--- get_embeddings_distances.py
+++ get_embeddings_distances.py
@@ -1,11 +1,10 @@
 
 
 
+from gensim.models import KeyedVectors
 
-
-
-
-
+w2v_bin_path        = '/home/dpappas/for_ryan/fordp/pubmed2018_w2v_30D.bin'
+wv_from_bin = KeyedVectors.load_word2vec_format(w2v_bin_path, binary=True)
 
 
 
