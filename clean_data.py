35bbcdd518704038446af43b0bd83b050b71b141
dpappas <Zrb6.NCC>
2019-02-25 16:54:58 +0200

    message

--- clean_data.py
+++ clean_data.py
@@ -108,6 +108,7 @@ for qid in tqdm(bioasq6_data):
         for d in bioasq6_data[qid]['documents']
         if(d in ret_pmids)
     ]
+    bioasq6_data[qid]['snippets']   = ret_pmids
     if(len(bioasq6_data[qid]['documents']) != 0):
         bioasq6_data_2[qid] = bioasq6_data[qid]
     else:
