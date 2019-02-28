24253e625dba05e22bdbe9322de1664d09627a03
dpappas <Zrb6.NCC>
2019-02-28 14:34:38 +0200

    message

--- gaussian_noise_example.py
+++ gaussian_noise_example.py
@@ -1,42 +1,22 @@
 
 import torch
-import torch.nn as nn
 
 use_cuda    = torch.cuda.is_available()
 device      = torch.device("cuda") if(use_cuda) else torch.device("cpu")
+mean, std   = 0., 0.05
 
-class GaussianNoise(nn.Module):
-    """Gaussian noise regularizer.
-    Args:
-        sigma (float, optional): relative standard deviation used to generate the
-            noise. Relative means that it will be multiplied by the magnitude of
-            the value your are adding the noise to. This means that sigma can be
-            the same regardless of the scale of the vector.
-        is_relative_detach (bool, optional): whether to detach the variable before
-            computing the scale of the noise. If `False` then the scale of the noise
-            won't be seen as a constant but something to optimize: this will bias the
-            network to generate vectors with smaller values.
-    """
-    #
-    def __init__(self, sigma=0.1, is_relative_detach=True):
-        super().__init__()
-        self.sigma = sigma
-        self.is_relative_detach = is_relative_detach
-        self.noise = torch.tensor(0).to(device)
-    #
-    def forward(self, x):
-        if self.training and self.sigma != 0:
-            scale = self.sigma * x.detach() if self.is_relative_detach else self.sigma * x
-            sampled_noise = self.noise.repeat(*x.size()).normal_() * scale
-            x = x + sampled_noise
-        return x
+embeddings  = torch.zeros(5, 10).float()
+embeddings  = embeddings.to(device)
+noise       = torch.zeros(1, embeddings.size(-1)).data.normal_(mean, std)
+noise       = noise.to(device)
+print(embeddings)
+print(noise)
+print(embeddings.size())
+print(noise.size())
 
-
-
-
-
-
-
+noisy_embeds = embeddings + noise.expand_as(embeddings)
+print(noisy_embeds)
+print(noisy_embeds.size())
 
 
 
