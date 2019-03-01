708f42de36bf6eb88c19133426cbec0af5d938ec
dpappas <Zrb6.NCC>
2019-03-01 14:44:26 +0200

    message

--- galago/do_everything.sh
+++ galago/do_everything.sh
@@ -3,9 +3,16 @@ python3.6 queries2galago.py \
 /home/DATA/bioasq7/BioASQ-training7b/trainining7b.json \
 ./bioasq7b_all_galago_queries.json
 
+/home/DATA/Biomedical/document_ranking/bioasq_data/document_retrieval/galago-3.10-bin/bin/galago \
+batch-search \
+--index=pubmed_only_abstract_galago_index \
+--verbose=False \
+--requested=2000 \
+--scorer=bm25 \
+--defaultTextPart=postings.krovetz \
+--mode=threaded \
+./bioasq7b_all_galago_queries.json \
+> ./bioasq7b_bm25_retrieval.all.txt
 
-
-
-
-
+# grep " 2000 " bioasq7b_bm25_retrieval.all.txt | wc -l # to see progress
 
