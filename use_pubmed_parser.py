ea6c251b6340fc5116c32cc4c20b7201a02591ab
dpappas <Zrb6.NCC>
2019-03-04 16:09:02 +0200

    message

--- use_pubmed_parser.py
+++ use_pubmed_parser.py
@@ -6,20 +6,21 @@ import  pubmed_parser as pp
 from    pprint import pprint
 from    tqdm import tqdm
 
-client              = pymongo.MongoClient("localhost", 27017, maxPoolSize=50)
+diri                = '/home/DATA/pubmed_baseline_2018/'
 db_name             = 'pubmedBaseline2019'
 collection_name     = 'articles'
+#############################
+client              = pymongo.MongoClient("localhost", 27017, maxPoolSize=50)
 mongo_collection    = client[db_name][collection_name]
 mongo_collection.drop()
 mongo_collection.create_index("pmid", unique=True)
-
-diri            = '/home/dpappas/for_ryan/'
+#############################
 all_xml_gzs     = [os.path.join(diri, f) for f in os.listdir(diri) if f.endswith('.xml.gz')]
 all_xml_gzs     = sorted(all_xml_gzs)
 #############################
 for path in tqdm(all_xml_gzs):
     dicts_out   = pp.parse_medline_xml(path, year_info_only=False, nlm_category=False)
-    for item in dicts_out:
+    for item in tqdm(dicts_out):
         if(len(item['abstract'].strip()) > 0 and len(item['title'].strip()) > 0):
             mongo_datum = {
                 'abstractText'      : item['abstract'],
