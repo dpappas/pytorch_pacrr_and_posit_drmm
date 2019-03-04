821cfde1c9aa088a706edf58a8d097b25f7aac63
dpappas <Zrb6.NCC>
2019-03-04 15:48:40 +0200

    message

--- use_pubmed_parser.py
+++ use_pubmed_parser.py
@@ -5,14 +5,15 @@ import pubmed_parser as pp
 from pprint import pprint
 
 #############################
-path        = '/home/dpappas/for_ryan/pubmed19n0376.xml.gz'
+# path        = '/home/dpappas/for_ryan/pubmed19n0376.xml.gz'
 # path        = '/home/dpappas/for_ryan/pubmed19n0403.xml.gz'
-# path        = '/home/dpappas/for_ryan/pubmed19n0437.xml.gz'
+path        = '/home/dpappas/for_ryan/pubmed19n0437.xml.gz'
 #############################
-dicts_out   = pp.parse_medline_xml(path)
-pprint(dicts_out[-1])
 dicts_out   = pp.parse_medline_xml(path, year_info_only=False, nlm_category=False)
-pprint(dicts_out[-1])
+for item in dicts_out:
+    if(len(item['abstract'].strip())>0):
+        pprint(item)
+        break
 #############################
 
 
