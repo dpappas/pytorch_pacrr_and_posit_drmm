35aaccaa2b7e0148aa0a6b2d9d1f99b1a1da185f
dpappas <Zrb6.NCC>
2019-03-04 15:46:08 +0200

    message

--- use_pubmed_parser.py
+++ use_pubmed_parser.py
@@ -3,7 +3,11 @@
 
 import pubmed_parser as pp
 from pprint import pprint
-path        = '/home/dpappas/for_ryan/pubmed19n0437.xml.gz'
+
+#############################
+path        = '/home/dpappas/for_ryan/pubmed19n0376.xml.gz'
+# path        = '/home/dpappas/for_ryan/pubmed19n0403.xml.gz'
+# path        = '/home/dpappas/for_ryan/pubmed19n0437.xml.gz'
 #############################
 dicts_out   = pp.parse_medline_xml(path)
 pprint(dicts_out[-1])
