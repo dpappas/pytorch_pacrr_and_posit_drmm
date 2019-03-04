22e4f0328ed632ae0f98fe01fa393a190d2dc8cf
dpappas <Zrb6.NCC>
2019-03-04 16:07:42 +0200

    message

--- use_pubmed_parser.py
+++ use_pubmed_parser.py
@@ -1,43 +1,44 @@
 
 
 
-import  pymongo
+import  os, pymongo
 import  pubmed_parser as pp
 from    pprint import pprint
+from    tqdm import tqdm
 
-# client              = pymongo.MongoClient("localhost", 27017, maxPoolSize=50)
-# db_name             = 'pubmedBaseline2019'
-# collection_name     = 'articles'
-# mongo_collection    = client[db_name][collection_name]
-# mongo_collection.drop()
-# mongo_collection.create_index("pmid", unique=True)
+client              = pymongo.MongoClient("localhost", 27017, maxPoolSize=50)
+db_name             = 'pubmedBaseline2019'
+collection_name     = 'articles'
+mongo_collection    = client[db_name][collection_name]
+mongo_collection.drop()
+mongo_collection.create_index("pmid", unique=True)
 
+diri            = '/home/dpappas/for_ryan/'
+all_xml_gzs     = [os.path.join(diri, f) for f in os.listdir(diri) if f.endswith('.xml.gz')]
+all_xml_gzs     = sorted(all_xml_gzs)
 #############################
-# path        = '/home/dpappas/for_ryan/pubmed19n0376.xml.gz'
-# path        = '/home/dpappas/for_ryan/pubmed19n0403.xml.gz'
-path        = '/home/dpappas/for_ryan/pubmed19n0437.xml.gz'
+for path in tqdm(all_xml_gzs):
+    dicts_out   = pp.parse_medline_xml(path, year_info_only=False, nlm_category=False)
+    for item in dicts_out:
+        if(len(item['abstract'].strip()) > 0 and len(item['title'].strip()) > 0):
+            mongo_datum = {
+                'abstractText'      : item['abstract'],
+                'title'             : item['title'],
+                'journalName'       : item['journal'],
+                'keywords'          : item['keywords'],
+                'meshHeadingsList'  : [mesh.strip() for mesh in item['mesh_terms'].split(';')],
+                'pmid'              : item['pmid'],
+                'country'           : item['country'],
+                'author'            : item['author'],
+                'publicationDate'   : item['pubdate'],
+            }
+            res = mongo_collection.insert_one(mongo_datum)
+            pprint(res)
 #############################
-dicts_out   = pp.parse_medline_xml(path, year_info_only=False, nlm_category=False)
-for item in dicts_out:
-    if(len(item['abstract'].strip()) > 0 and len(item['title'].strip()) > 0):
-        mongo_datum = {
-            'abstractText'      : item['abstract'],
-            'title'             : item['title'],
-            'journalName'       : item['journal'],
-            'keywords'          : item['keywords'],
-            'meshHeadingsList'  : [mesh.strip() for mesh in item['mesh_terms'].split(';')],
-            'pmid'              : item['pmid'],
-            'country'           : item['country'],
-            'author'            : item['author'],
-            'publicationDate'   : item['pubdate'],
-        }
-        pprint(mongo_datum)
-
-#############################
-
-# pprint(client['pubmedBaseline2018']['articles'].find_one())
 
 '''
+# pprint(client['pubmedBaseline2018']['articles'].find_one())
+
 u'abstractText'
 u'journalName'
 u'keywords'
