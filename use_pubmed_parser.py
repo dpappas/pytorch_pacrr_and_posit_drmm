9326ad7134505ece5f550524557df1fd3952884a
dpappas <Zrb6.NCC>
2019-03-04 15:44:38 +0200

    message

--- use_pubmed_parser.py
+++ use_pubmed_parser.py
@@ -4,9 +4,12 @@
 import pubmed_parser as pp
 from pprint import pprint
 path        = '/home/dpappas/for_ryan/pubmed19n0437.xml.gz'
-# dict_out    = pp.parse_pubmed_xml(path)
+#############################
 dicts_out   = pp.parse_medline_xml(path)
-pprint(dicts_out[0])
+pprint(dicts_out[-1])
+dicts_out   = pp.parse_medline_xml(path, year_info_only=False, nlm_category=False)
+pprint(dicts_out[-1])
+#############################
 
 
 
