bc37eee6a7013353ab00565c76fc769afc97dee7
dpappas <Zrb6.NCC>
2019-03-04 11:56:38 +0200

    message

--- BioASQ7_competition/train_w2v_jpdrmm.py
+++ BioASQ7_competition/train_w2v_jpdrmm.py
@@ -1366,12 +1366,14 @@ w2v_bin_path        = '/home/dpappas/for_ryan/fordp/pubmed2018_w2v_30D.bin'
 idf_pickle_path     = '/home/dpappas/for_ryan/fordp/idf.pkl'
 dataloc             = '/home/dpappas/for_ryan/bioasq7/BioASQ-training7b/'
 #####################
-bioasq7_data        = json.load(open(os.path.join(dataloc,   'trainining7b.json')))
-train_docs          = pickle.load(open(os.path.join(dataloc, 'bioasq_bm25_docset_top100.all.pkl'), 'rb'))
-train_data          = pickle.load(open(os.path.join(dataloc, 'bioasq_bm25_top100.all.pkl'), 'rb'))
+bioasq7_data            = json.load(open(os.path.join(dataloc,   'trainining7b.json')))
+train_docs              = pickle.load(open(os.path.join(dataloc, 'bioasq_bm25_docset_top100.all.pkl'), 'rb'))
+train_data              = pickle.load(open(os.path.join(dataloc, 'bioasq_bm25_top100.all.pkl'), 'rb'))
+train_data['queries']   = [q for q in train_data['queries'] if(len(q['retrieved_documents']) > 0)]
 #####################
 train_data          = RemoveBadYears(train_data, train_docs, True)
 train_data          = RemoveTrainLargeYears(train_data, train_docs)
+
 #####################
 words               = {}
 GetWords(train_data, train_docs, words)
