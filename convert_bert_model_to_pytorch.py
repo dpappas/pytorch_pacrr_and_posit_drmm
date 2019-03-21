8de3f962dd6ecd98294e21939fbc0c064edf88de
dvpappas <dvpappas89@gmail.com>
2019-03-21 13:16:30 +0200

    message

--- convert_bert_model_to_pytorch.py
+++ convert_bert_model_to_pytorch.py
@@ -2,11 +2,11 @@
 import os
 from pytorch_pretrained_bert.convert_tf_checkpoint_to_pytorch import convert_tf_checkpoint_to_pytorch
 
-if (not os.path.exists('/home/dpappas/bioasq_all/F_BERT/Biobert/pubmed_pmc_470k/pytorch_model.bin')):
-    convert_tf_checkpoint_to_pytorch(
-        '/home/dpappas/bioasq_all/F_BERT/Biobert/pubmed_pmc_470k/biobert_model.ckpt',
-        '/home/dpappas/bioasq_all/F_BERT/Biobert/pubmed_pmc_470k/bert_config.json',
-        '/home/dpappas/bioasq_all/F_BERT/Biobert/pubmed_pmc_470k/pytorch_model.bin'
-    )
+# in_dir          = '/media/dpappas/dpappas_data/biobert/pubmed_pmc_470k/'
+in_dir          = '/media/dpappas/dpappas_data/biobert/biobert_pubmed/'
+out_bin_path    = os.path.join(in_dir, 'pytorch_model.bin')
+
+if (not os.path.exists(out_bin_path)):
+    convert_tf_checkpoint_to_pytorch(os.path.join(in_dir, 'biobert_model.ckpt'), os.path.join(in_dir, 'bert_config.json'), out_bin_path)
 
 
