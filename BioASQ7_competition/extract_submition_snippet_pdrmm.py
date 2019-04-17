aa184d3cec2dbe75b3e5a3a58ab9e9de6e46f85d
dvpappas <dvpappas89@gmail.com>
2019-04-17 19:59:19 +0300

    message

--- BioASQ7_competition/extract_submition_snippet_pdrmm.py
+++ BioASQ7_competition/extract_submition_snippet_pdrmm.py
@@ -593,7 +593,7 @@ def do_for_some_retrieved(docs, dato, retr_docs, data_for_revision, ret_data, us
         else:
             data_for_revision[dato['query_id']]['snippets'][retr['doc_id']] = all_emits
     #
-    doc_res                                 = sorted(doc_res.items(), key=lambda x: x[1], reverse=True)
+    # doc_res                                 = sorted(doc_res.items(), key=lambda x: x[1], reverse=True)
     the_doc_scores                          = dict([("http://www.ncbi.nlm.nih.gov/pubmed/{}".format(pm[0]), pm[1]) for pm in doc_res[:10]])
     doc_res                                 = ["http://www.ncbi.nlm.nih.gov/pubmed/{}".format(pm[0]) for pm in doc_res]
     emitions['documents']                   = doc_res[:100]
