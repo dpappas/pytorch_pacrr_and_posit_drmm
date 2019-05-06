e10e574831e0bbd8d3a7f5f400f2e297f7505642
dvpappas <dvpappas89@gmail.com>
2019-05-06 19:39:14 +0300

    message

--- bulk_insert_wiki_json_gz.py
+++ bulk_insert_wiki_json_gz.py
@@ -46,22 +46,43 @@ def send_to_elk(actions):
             else:
                 flag = False
 
+# print(clean_the_damn_thing(dato['source_text']))
+
 def clean_the_damn_thing(tt):
     tt = tt.replace('&nbsp;', ' ').replace('<sub>', '').replace('</sub>', '').replace('<blockquote>', '').replace('</blockquote>', '')
+    ####################################################################
+    tt = re.sub(r'\[\[File:.*?\]\]',        r'',    tt, flags=re.DOTALL)
+    tt = re.sub(r'\[\[Category:.*?\]\]',    r'',    tt, flags=re.DOTALL)
+    # ####################################################################
+    tt = re.sub(r'\[\[([^\|]+?)\]\]',       r'\1',  tt, flags=re.DOTALL)
+    tt = re.sub(r'\[\[(.*?)\|(.*?)\]\]',    r'\2',  tt, flags=re.DOTALL)
+    # ####################################################################
+    tt = re.sub(r'<--.+?-->',               r'',    tt, flags=re.DOTALL)
+    ####################################################################
+    for i in range(4):
+        for item in re.findall(r'<ref.*?</ref>', tt):
+            if(item.count('<ref')==1):
+                tt = tt.replace(item, '').strip()
+        for item in re.findall(r'<ref.*?/>', tt):
+            if(item.count('<ref')==1):
+                tt = tt.replace(item, '').strip()
     ####################################################################
     for i in range(4):
         tt = re.sub(r'\{\{([^\{]+?)\}\}',   r'',    tt, flags=re.DOTALL)
     for i in range(4):
         tt = re.sub(r'\{\|[^\{]+?\|\}',     r'',    tt, flags=re.DOTALL)
     ####################################################################
-    tt = re.sub(r'\[\[File:.*?\]\]',        r'',    tt, flags=re.DOTALL)
-    tt = re.sub(r'\[\[Category:.*?\]\]',    r'',    tt, flags=re.DOTALL)
-    ####################################################################
-    tt = re.sub(r'<ref .*?/>',              r'',    tt, flags=re.DOTALL)
-    tt = re.sub(r'<ref.*?</ref>',           r'',    tt, flags=re.DOTALL)
-    tt = re.sub(r'\[\[([^\|]+?)\]\]',       r'\1',  tt, flags=re.DOTALL)
-    tt = re.sub(r'\[\[(.*?)\|(.*?)\]\]',    r'\2',  tt, flags=re.DOTALL)
+    # tt = re.sub(r'==(.*?)==', '',  tt, flags=re.DOTALL)
+    # ####################################################################
+    for i in range(10):
+        tt = tt.replace('\n\n\n', '\n\n')
+    # tt = re.sub(r'<ref [^\<].+?/>',          r'',    tt, flags=re.DOTALL)
+    # tt = re.sub(r'<ref.*?</ref>',         r'',    tt, flags=re.DOTALL)
+    # re.findall(r'<ref [^\<].+?/>', clean_the_damn_thing(dato['source_text']))
+    # re.findall(r'<ref.*?</ref>', clean_the_damn_thing(dato['source_text']))
     return tt.strip()
+
+# pprint(re.findall(r'<ref .*?</ref>', clean_the_damn_thing(dato['source_text'])))
 
 es          = Elasticsearch(['localhost:9200'], verify_certs=True, timeout=150, max_retries=10, retry_on_timeout=True)
 index       = 'en_wikipedia_json_gz'
@@ -70,29 +91,66 @@ b_size      = 200
 actions     = []
 fpath       = '/media/dpappas/dpappas_data/enwiki-20181112-cirrussearch-content.json.gz'
 # fpath       = '/media/dpappas/dpappas_data/elwiki-20190211-cirrussearch-content.json.gz'
-proc        = subprocess.Popen(["zcat", fpath], stdout=subprocess.PIPE)
 
+proc        = subprocess.Popen(["zcat", fpath], stdout=subprocess.PIPE)
 for line in iter(proc.stdout.readline, ''):
     line    = line.rstrip()
     dato    = json.loads(line)
     if('source_text' in dato):
-        print(clean_the_damn_thing(dato['source_text']))
-        print(20 * '-')
+        print(dato['title'])
+        # print(dato['opening_text'])
+        paragraphs = re.split(r'\s\s+', dato['text'], flags=re.DOTALL)
+        paragraphs = [par.strip() for par in paragraphs if(par is not None and len(par.strip()) > 0)]
+        for par in paragraphs:
+            print(par)
+            print(5 * '-')
+        print(20 * '=')
         # re.sub(r'\{\|.+?\|\}', r'', tt, re.DOTALL)
         # re.findall(r'\{\|.+?\|\}', tt, re.DOTALL)
-        if('Norton Water Tower'.lower() in dato['title'].lower()):
+        if('Battle of Diamond Rock'.lower() in dato['title'].lower()):
             break
+    # temp    = create_an_action(dato)
+    # actions.append(temp)
+    # if(len(actions) >= b_size):
+    #     send_to_elk(actions)
+    #     actions = []
 
-    temp    = create_an_action(dato)
-    actions.append(temp)
-    if(len(actions) >= b_size):
-        send_to_elk(actions)
-        actions = []
 
 if(len(actions) > 0):
     send_to_elk(actions)
     actions = []
 
+'''
+
+proc        = subprocess.Popen(["zcat", fpath], stdout=subprocess.PIPE)
+for line in iter(proc.stdout.readline, ''):
+    line    = line.rstrip()
+    dato    = json.loads(line)
+    if('source_text' in dato):
+        print(dato['title'])
+        clean_text = clean_the_damn_thing(dato['source_text'])
+        paragraphs = []
+        for p in re.split(r'===(.*?)===', clean_text, flags=re.DOTALL):
+            for p2 in re.split(r'==(.*?)==', p, flags=re.DOTALL):
+                for p3 in re.split(r'\s\s\s*', p2, flags=re.DOTALL):
+                    if(p3 is not None and len(p3.strip()) > 0 and len(p3.strip().split()) > 5):
+                        paragraphs.append(p3.strip())
+        # paragraphs = re.split(r'\s\s+', clean_text)
+        for par in paragraphs:
+            print(par)
+            print(5 * '-')
+        print(20 * '=')
+        # re.sub(r'\{\|.+?\|\}', r'', tt, re.DOTALL)
+        # re.findall(r'\{\|.+?\|\}', tt, re.DOTALL)
+        if('Battle of Diamond Rock'.lower() in dato['title'].lower()):
+            break
+    # temp    = create_an_action(dato)
+    # actions.append(temp)
+    # if(len(actions) >= b_size):
+    #     send_to_elk(actions)
+    #     actions = []
+
+'''
 
 '''
 curl -X GET "harvester2.ilsp.gr:9200/_search" -H 'Content-Type: application/json' -d'
