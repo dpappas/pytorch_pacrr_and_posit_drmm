2034065bc4227cbf23ce1253f8a044ecf77b0bc0
dvpappas <dvpappas89@gmail.com>
2019-05-20 15:32:26 +0300

    message

--- BioASQ7_competition/t1.sh
+++ BioASQ7_competition/t1.sh
@@ -3,42 +3,41 @@ python3.6 sig.py \
 "/home/dpappas/bioasq_all/bioasq7/data/test_batch_1/BioASQ-task7bPhaseB-testset1" \
 "/home/dpappas/bioasq_all/bioasq7/document_results/test_batch_1/jpdrmm.json" \
 "/home/dpappas/bioasq_all/bioasq7/document_results/test_batch_1/pdrmm.json" \
-"MAP documents"
-
+"MAP documents" \
+"sig_jpdrmm_pdrmm_docs_1.txt" &
 python3.6 sig.py \
 "/home/dpappas/bioasq_all/bioasq7/data/test_batch_1/BioASQ-task7bPhaseB-testset1" \
 "/home/dpappas/bioasq_all/bioasq7/document_results/test_batch_1/bert.json" \
 "/home/dpappas/bioasq_all/bioasq7/document_results/test_batch_1/JBERT.json" \
-"MAP documents"
-
-########################################################################################
-
+"MAP documents" \
+"sig_bert_jbert_docs_1.txt" &
 python3.6 sig.py \
 "/home/dpappas/bioasq_all/bioasq7/data/test_batch_1/BioASQ-task7bPhaseB-testset1" \
 "/home/dpappas/bioasq_all/bioasq7/document_results/test_batch_1/jpdrmm.json" \
 "/home/dpappas/bioasq_all/bioasq7/snippet_results/test_batch_1/pdrmm_pdrmm.json" \
-"MAP snippets"
-
+"MAP snippets" \
+"sig_jpdrmm_pdrmmpdrmm_snips_1.txt" &
 python3.6 sig.py \
 "/home/dpappas/bioasq_all/bioasq7/data/test_batch_1/BioASQ-task7bPhaseB-testset1" \
 "/home/dpappas/bioasq_all/bioasq7/snippet_results/test_batch_1/bert_pdrmm.json" \
 "/home/dpappas/bioasq_all/bioasq7/document_results/test_batch_1/JBERT.json" \
-"MAP snippets"
-
+"MAP snippets" \
+"sig_bertpdrmm_JBERT_snips_1.txt" &
 python3.6 sig.py \
 "/home/dpappas/bioasq_all/bioasq7/data/test_batch_1/BioASQ-task7bPhaseB-testset1" \
 "/home/dpappas/bioasq_all/bioasq7/document_results/test_batch_1/JBERT_F." \
 "/home/dpappas/bioasq_all/bioasq7/document_results/test_batch_1/JBERT.json" \
-"MAP snippets"
-
+"MAP snippets" \
+"sig_JBERTF_JBERT_snips_1.txt" &
 python3.6 sig.py \
 "/home/dpappas/bioasq_all/bioasq7/data/test_batch_1/BioASQ-task7bPhaseB-testset1" \
 "/home/dpappas/bioasq_all/bioasq7/snippet_results/test_batch_1/pdrmm_bcnn.json" \
 "/home/dpappas/bioasq_all/bioasq7/snippet_results/test_batch_1/pdrmm_pdrmm.json" \
-"MAP snippets"
-
+"MAP snippets" \
+"sig_pdrmmbcnn_pdrmmpdrmm_snips_1.txt" &
 python3.6 sig.py \
 "/home/dpappas/bioasq_all/bioasq7/data/test_batch_1/BioASQ-task7bPhaseB-testset1" \
 "/home/dpappas/bioasq_all/bioasq7/snippet_results/test_batch_1/bert_bcnn.json" \
 "/home/dpappas/bioasq_all/bioasq7/snippet_results/test_batch_1/bert_pdrmm.json" \
-"MAP snippets"
+"MAP snippets" \
+"sig_bertbcnn_bertpdrmm_snips_1.txt"
--- BioASQ7_competition/t3.sh
+++ BioASQ7_competition/t3.sh
@@ -3,42 +3,41 @@ python3.6 sig.py \
 "/home/dpappas/bioasq_all/bioasq7/data/test_batch_3/BioASQ-task7bPhaseB-testset3" \
 "/home/dpappas/bioasq_all/bioasq7/document_results/test_batch_3/jpdrmm.json" \
 "/home/dpappas/bioasq_all/bioasq7/document_results/test_batch_3/pdrmm.json" \
-"MAP documents"
-
+"MAP documents" \
+"sig_jpdrmm_pdrmm_docs_3.txt" &
 python3.6 sig.py \
 "/home/dpappas/bioasq_all/bioasq7/data/test_batch_3/BioASQ-task7bPhaseB-testset3" \
 "/home/dpappas/bioasq_all/bioasq7/document_results/test_batch_3/bert.json" \
 "/home/dpappas/bioasq_all/bioasq7/document_results/test_batch_3/JBERT.json" \
-"MAP documents"
-
-########################################################################################
-
+"MAP documents" \
+"sig_bert_jbert_docs_3.txt" &
 python3.6 sig.py \
 "/home/dpappas/bioasq_all/bioasq7/data/test_batch_3/BioASQ-task7bPhaseB-testset3" \
 "/home/dpappas/bioasq_all/bioasq7/document_results/test_batch_3/jpdrmm.json" \
 "/home/dpappas/bioasq_all/bioasq7/snippet_results/test_batch_3/pdrmm_pdrmm.json" \
-"MAP snippets"
-
+"MAP snippets" \
+"sig_jpdrmm_pdrmmpdrmm_snips_3.txt" &
 python3.6 sig.py \
 "/home/dpappas/bioasq_all/bioasq7/data/test_batch_3/BioASQ-task7bPhaseB-testset3" \
 "/home/dpappas/bioasq_all/bioasq7/snippet_results/test_batch_3/bert_pdrmm.json" \
 "/home/dpappas/bioasq_all/bioasq7/document_results/test_batch_3/JBERT.json" \
-"MAP snippets"
-
+"MAP snippets" \
+"sig_bertpdrmm_JBERT_snips_3.txt" &
 python3.6 sig.py \
 "/home/dpappas/bioasq_all/bioasq7/data/test_batch_3/BioASQ-task7bPhaseB-testset3" \
 "/home/dpappas/bioasq_all/bioasq7/document_results/test_batch_3/JBERT_F." \
 "/home/dpappas/bioasq_all/bioasq7/document_results/test_batch_3/JBERT.json" \
-"MAP snippets"
-
+"MAP snippets" \
+"sig_JBERTF_JBERT_snips_3.txt" &
 python3.6 sig.py \
 "/home/dpappas/bioasq_all/bioasq7/data/test_batch_3/BioASQ-task7bPhaseB-testset3" \
 "/home/dpappas/bioasq_all/bioasq7/snippet_results/test_batch_3/pdrmm_bcnn.json" \
 "/home/dpappas/bioasq_all/bioasq7/snippet_results/test_batch_3/pdrmm_pdrmm.json" \
-"MAP snippets"
-
+"MAP snippets" \
+"sig_pdrmmbcnn_pdrmmpdrmm_snips_3.txt" &
 python3.6 sig.py \
 "/home/dpappas/bioasq_all/bioasq7/data/test_batch_3/BioASQ-task7bPhaseB-testset3" \
 "/home/dpappas/bioasq_all/bioasq7/snippet_results/test_batch_3/bert_bcnn.json" \
 "/home/dpappas/bioasq_all/bioasq7/snippet_results/test_batch_3/bert_pdrmm.json" \
-"MAP snippets"
+"MAP snippets" \
+"sig_bertbcnn_bertpdrmm_snips_3.txt"
