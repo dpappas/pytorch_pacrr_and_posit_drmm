80a939178d2fcac2b40ffc238774d36883129f3e
dvpappas <dvpappas89@gmail.com>
2019-05-21 12:21:45 +0300

    message

--- BioASQ7_competition/print_examples.py
+++ BioASQ7_competition/print_examples.py
@@ -8,7 +8,7 @@ from pprint import pprint
 
 bioclean = lambda t: re.sub('[.,?;*!%^&_+():-\[\]{}]', '', t.replace('"', '').replace('/', '').replace('\\', '').replace("'", '').strip().lower()).split()
 
-batch   = 1
+batch   = 3
 d       = json.load(open('/home/dpappas/test_jpdrmm_high_batch{}/v3 test_data_for_revision.json'.format(batch)))
 d1      = json.load(open('/home/dpappas/bioasq_all/bioasq7/document_results/test_batch_{}/jpdrmm.json'.format(batch)))
 d2      = json.load(open('/home/dpappas/bioasq_all/bioasq7/data/test_batch_{}/BioASQ-task7bPhaseB-testset{}'.format(batch, batch)))
@@ -69,10 +69,32 @@ for q in data1:
 pprint(data1[u'Please list the 4 genes involved in Sanfilippo syndrome, also known as mucopolysaccharidosis III (MPS-III).'])
 pprint(data2[u'Please list the 4 genes involved in Sanfilippo syndrome, also known as mucopolysaccharidosis III (MPS-III).'])
 
-pprint(data1['What is the mechanism of action of anlotinib'])
-pprint(data2['What is the mechanism of action of anlotinib'])
+pprint(data1[u'What is the mechanism of action of anlotinib?'])
+pprint(data2[u'What is the mechanism of action of anlotinib?'])
+
+A = set(data1[u'What is the mechanism of action of anlotinib?'])
+B = set(data2[u'What is the mechanism of action of anlotinib?'])
+
+for plink in A - B:
+    print(plink)
+    pprint(data1[u'What is the mechanism of action of anlotinib?'][plink])
+    print(20 * '-')
+
+for plink in B - A:
+    print(plink)
+    pprint(data2[u'What is the mechanism of action of anlotinib?'][plink])
+    print(20 * '-')
 
 
+for plink in A & B:
+    if(
+        len(data2[u'What is the mechanism of action of anlotinib?'][plink]) >
+        len(data1[u'What is the mechanism of action of anlotinib?'][plink])
+    ):
+        print(plink)
+        pprint(data1[u'What is the mechanism of action of anlotinib?'][plink])
+        pprint(data2[u'What is the mechanism of action of anlotinib?'][plink])
+        print(20 * '-')
 
 
 
