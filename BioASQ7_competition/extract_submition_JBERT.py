91774f79cd97ff2e17a306f36ff85fa53b73d137
dvpappas <dvpappas89@gmail.com>
2019-05-30 16:51:01 +0300

    message

--- BioASQ7_competition/extract_submition_JBERT.py
+++ BioASQ7_competition/extract_submition_JBERT.py
@@ -1014,17 +1014,17 @@ retrieval_jar_path          = '/home/dpappas/bioasq_all/dist/my_bioasq_eval_2.ja
 ###########################################################
 bert_all_words_path         = '/home/dpappas/bioasq_all/bert_all_words.pkl'
 ###########################################################
-batch                       = 4
+batch                       = 5
 f_in1                       = '/home/dpappas/bioasq_all/bioasq7/data/test_batch_{}/BioASQ-task7bPhaseA-testset{}'.format(batch, batch)
 f_in2                       = '/home/dpappas/bioasq_all/bioasq7/data/test_batch_{}/bioasq7_bm25_top100/bioasq7_bm25_top100.test.pkl'.format(batch)
 f_in3                       = '/home/dpappas/bioasq_all/bioasq7/data/test_batch_{}/bioasq7_bm25_top100/bioasq7_bm25_docset_top100.test.pkl'.format(batch)
+odir                        = './test_frozen_jbert_batch{}/'.format(batch)
 ###########################################################
 # bert_resume_from            = '/media/dpappas/dpappas_data/models_out/bioasq7_JBERT_2L_0p01_run_0/best_bert_checkpoint.pth.tar'
 # model_resume_from           = '/media/dpappas/dpappas_data/models_out/bioasq7_JBERT_2L_0p01_run_0/best_checkpoint.pth.tar'
 # odir                        = './test_jbert_batch{}/'.format(batch)
 bert_resume_from            = '/home/dpappas/frozen_bioasq7_JBERT_2L_0p01_run_0/best_bert_checkpoint.pth.tar'
 model_resume_from           = '/home/dpappas/frozen_bioasq7_JBERT_2L_0p01_run_0/best_checkpoint.pth.tar'
-odir                        = './test_frozen_jbert_batch{}/'.format(batch)
 ###########################################################
 idf_pickle_path             = '/home/dpappas/bioasq_all/idf.pkl'
 ###########################################################
