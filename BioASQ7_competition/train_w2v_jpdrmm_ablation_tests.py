eaca2263d84693ee9bb01ac30d8d330d8eb8f5c9
dvpappas <dvpappas89@gmail.com>
2019-06-24 15:11:12 +0300

    message

--- BioASQ7_competition/train_w2v_jpdrmm_ablation_tests.py
+++ BioASQ7_competition/train_w2v_jpdrmm_ablation_tests.py
@@ -1527,6 +1527,11 @@ CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 1 1 0 1 0
 CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 1 0 1 1 0
 CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 0 1 1 1 0
 ###########################################################
+# no OH sim
+CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 0 0 1 1 1
+CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 0 1 0 1 1
+CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 0 1 1 0 1
+###########################################################
 
 
 
