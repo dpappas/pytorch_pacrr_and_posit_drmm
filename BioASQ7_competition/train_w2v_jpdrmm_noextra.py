bf13eff0f6b96ad0ceb07ccdecd013f1cfbaf130
dvpappas <dvpappas89@gmail.com>
2019-06-24 13:32:38 +0300

    message

--- BioASQ7_competition/train_w2v_jpdrmm_noextra.py
+++ BioASQ7_competition/train_w2v_jpdrmm_noextra.py
@@ -1458,3 +1458,7 @@ for run in range(run_from, run_to):
             logger.info('early stop in epoch {} . waited for {} epochs'.format(epoch, early_stop))
             break
 
+'''
+CUDA_VISIBLE_DEVICES=1 python3.6 train_noallextra.py  
+'''
+
