20ddf997398dcfa07fb6d1da00b2b4177fbc5d5f
dvpappas <dvpappas89@gmail.com>
2019-06-24 14:57:50 +0300

    message

--- BioASQ7_competition/train_w2v_jpdrmm_ablation_tests.py
+++ BioASQ7_competition/train_w2v_jpdrmm_ablation_tests.py
@@ -1497,11 +1497,13 @@ CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 0 1 1 1 1
 CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 0 1 1 1 1 1
 CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 0 1 1 1 1 1 1
 ###########################################################
+CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 1 1 1 0 0
 CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 1 1 0 1 0
 CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 1 0 1 1 0
 CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 0 1 1 1 0
 CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 0 1 1 1 1 0
 CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 0 1 1 1 1 1 0
+###########################################################
 
 
 
