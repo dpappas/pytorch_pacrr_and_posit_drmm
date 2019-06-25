ebdd6c4bbfae05354064ee85d8eaf4b9218fbd48
dvpappas <dvpappas89@gmail.com>
2019-06-25 11:32:23 +0300

    message

--- BioASQ7_competition/extract_ablation.py
+++ BioASQ7_competition/extract_ablation.py
@@ -1221,28 +1221,31 @@ print(test_map)
 
 '''
 
-CUDA_VISIBLE_DEVICES=0 python3.6 extract_ablation.py 1 1111101 &
-CUDA_VISIBLE_DEVICES=0 python3.6 extract_ablation.py 2 1111101 &
-CUDA_VISIBLE_DEVICES=0 python3.6 extract_ablation.py 3 1111101 &
-CUDA_VISIBLE_DEVICES=0 python3.6 extract_ablation.py 4 1111101 &
-CUDA_VISIBLE_DEVICES=0 python3.6 extract_ablation.py 5 1111101 
-
-CUDA_VISIBLE_DEVICES=0 python3.6 extract_ablation.py 1 1111011 &
-CUDA_VISIBLE_DEVICES=0 python3.6 extract_ablation.py 2 1111011 &
-CUDA_VISIBLE_DEVICES=0 python3.6 extract_ablation.py 3 1111011 &
-CUDA_VISIBLE_DEVICES=0 python3.6 extract_ablation.py 4 1111011 &
-CUDA_VISIBLE_DEVICES=0 python3.6 extract_ablation.py 5 1111011
+CUDA_VISIBLE_DEVICES=1 python3.6 extract_ablation.py 1 1101111 &
+CUDA_VISIBLE_DEVICES=1 python3.6 extract_ablation.py 2 1101111 &
+CUDA_VISIBLE_DEVICES=1 python3.6 extract_ablation.py 3 1101111 &
+CUDA_VISIBLE_DEVICES=1 python3.6 extract_ablation.py 4 1101111 &
+CUDA_VISIBLE_DEVICES=1 python3.6 extract_ablation.py 5 1101111
 
 
-java \
--Xmx10G \
--cp \
-/home/dpappas/bioasq_all/dist/my_bioasq_eval_2.jar \
-evaluation.EvaluatorTask1b \
--phaseA \
--e \
-5 \
+java -Xmx10G -cp /home/dpappas/bioasq_all/dist/my_bioasq_eval_2.jar evaluation.EvaluatorTask1b -phaseA -e 5 \
 "/home/dpappas/bioasq_all/bioasq7/data/test_batch_1/BioASQ-task7bPhaseB-testset1" \
-"/home/dpappas/test_ablation_1111011_batch1/v3 test_emit_bioasq.json"
+"/home/dpappas/test_ablation_1101111_batch1/v3 test_emit_bioasq.json"
+
+java -Xmx10G -cp /home/dpappas/bioasq_all/dist/my_bioasq_eval_2.jar evaluation.EvaluatorTask1b -phaseA -e 5 \
+"/home/dpappas/bioasq_all/bioasq7/data/test_batch_2/BioASQ-task7bPhaseB-testset2" \
+"/home/dpappas/test_ablation_1101111_batch2/v3 test_emit_bioasq.json"
+
+java -Xmx10G -cp /home/dpappas/bioasq_all/dist/my_bioasq_eval_2.jar evaluation.EvaluatorTask1b -phaseA -e 5 \
+"/home/dpappas/bioasq_all/bioasq7/data/test_batch_3/BioASQ-task7bPhaseB-testset3" \
+"/home/dpappas/test_ablation_1101111_batch3/v3 test_emit_bioasq.json"
+
+java -Xmx10G -cp /home/dpappas/bioasq_all/dist/my_bioasq_eval_2.jar evaluation.EvaluatorTask1b -phaseA -e 5 \
+"/home/dpappas/bioasq_all/bioasq7/data/test_batch_4/BioASQ-task7bPhaseB-testset4" \
+"/home/dpappas/test_ablation_1101111_batch4/v3 test_emit_bioasq.json"
+
+java -Xmx10G -cp /home/dpappas/bioasq_all/dist/my_bioasq_eval_2.jar evaluation.EvaluatorTask1b -phaseA -e 5 \
+"/home/dpappas/bioasq_all/bioasq7/data/test_batch_5/BioASQ-task7bPhaseB-testset5" \
+"/home/dpappas/test_ablation_1101111_batch5/v3 test_emit_bioasq.json"
 
 '''
