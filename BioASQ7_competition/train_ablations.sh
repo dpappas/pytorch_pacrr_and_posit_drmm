761c81a819bcb62acff79eac5a930ee3b1161918
dvpappas <dvpappas89@gmail.com>
2019-07-01 17:10:27 +0300

    message

--- BioASQ7_competition/train_ablations.sh
+++ BioASQ7_competition/train_ablations.sh
@@ -2,38 +2,26 @@
 
 ##############################################
 
-CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 1 1 1 1 1
-
-##############################################
-
+CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 1 1 1 1 1 &
 CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 1 1 1 1 0 &
 CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 1 1 1 0 1 &
 CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 1 1 0 1 1 &
-CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 1 0 1 1 1 &
+CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 1 0 1 1 1
 CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 0 1 1 1 1 &
 CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 0 1 1 1 1 1 &
-CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 0 1 1 1 1 1 1
-
-##############################################
-
+CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 0 1 1 1 1 1 1 &
 CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 1 1 1 0 0 &
-CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 1 1 0 1 0 &
+CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 1 1 0 1 0
 CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 1 0 1 1 0 &
 CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 0 1 1 1 0 &
 CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 0 1 1 1 1 0 &
-CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 0 1 1 1 1 1 0
-
-##############################################
-
-CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 1 1 0 0 1 &
+CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 0 1 1 1 1 1 0 &
+CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 1 1 0 0 1
 CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 1 0 1 0 1 &
 CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 1 0 1 1 0 1 &
 CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 0 1 1 1 0 1 &
-CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 0 1 1 1 1 0 1
-
-##############################################
-
-CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 0 0 1 1 1 1 1 &
+CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 0 1 1 1 1 0 1 &
+CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 0 0 1 1 1 1 1
 CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 0 0 0 1 1 1 1 &
 CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 0 0 1 0 1 1 1 &
 CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 0 0 1 1 0 1 1 &
