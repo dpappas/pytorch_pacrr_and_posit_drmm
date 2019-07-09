8091f8935f82c214358962edaee29c2e3d98025b
dvpappas <dvpappas89@gmail.com>
2019-07-09 12:21:27 +0300

    message

--- BioASQ7_competition/ablation_all.sh
+++ BioASQ7_competition/ablation_all.sh
@@ -12,12 +12,6 @@ CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 1 0 1 1 1 1 0 &
 CUDA_VISIBLE_DEVICES=1 python3.6 ablation.py 0 1 1 1 1 1 0
 
 
-
-
-
-
-
-
 java -Xmx10G -cp /home/dpappas/bioasq_all/dist/my_bioasq_eval_2.jar evaluation.EvaluatorTask1b -phaseA -e 5 \
 "/home/dpappas/bioasq_all/bioasq7/data/test_batch_1/BioASQ-task7bPhaseB-testset1" \
 "/media/dpappas/dpappas_data/models_out/test_ablation_1111100_batch1/v3 test_emit_bioasq.json" | grep "^MAP documents:\|^MAP snippets:"
