eb219421eef8ad8676509a54cf5dd75581ce072c
dvpappas <dvpappas89@gmail.com>
2019-07-17 12:24:18 +0300

    message

---
+++ NaturalQuestions/create_elk_index.py
@@ -0,0 +1,32 @@
+
+from elasticsearch import Elasticsearch
+
+index   = 'natural_questions_0_1'
+map     = 'natural_questions_map_0_1'
+lang    = 'english'
+
+elastic_con = Elasticsearch(['localhost:9200'], verify_certs=True, timeout=150, max_retries=10, retry_on_timeout=True)
+elastic_con.indices.delete(index=index, ignore=[400,404])
+
+mapping = {
+    "settings": {"analysis": {"analyzer": {"default": {"type": "english"}}}},
+    "mappings":{
+        map:{
+            "properties":{
+                'document_url'    : {"type": "keyword"},
+                'paragraph_index' : {"type": "long"},
+                'paragraph_text'  : {"type": "text", "analyzer": 'english'}},
+                'document_title'  : {
+                    "type": "text",
+                    "analyzer": 'english',
+                    "fields": {
+                        "raw": {
+                            "type": "keyword"
+                        }
+                    }
+                }
+        }
+    }
+}
+
+print(elastic_con.indices.create(index = index, ignore=400, body=mapping))
