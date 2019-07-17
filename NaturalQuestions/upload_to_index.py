afee102f920c3ca14942c3e4b732dfbb147fc964
dvpappas <dvpappas89@gmail.com>
2019-07-17 12:40:33 +0300

    message

--- NaturalQuestions/upload_to_index.py
+++ NaturalQuestions/upload_to_index.py
@@ -2,6 +2,8 @@
 import gzip, re, json, hashlib
 from pprint import pprint
 from bs4 import BeautifulSoup
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
 
 def clean_soup(html):
     #######################################################
@@ -63,6 +65,40 @@ def clean_soup(html):
     #######################
     return soupa
 
+def create_an_action(elk_dato, id):
+    elk_dato['_id']      = id
+    elk_dato['_op_type'] = 'index'
+    elk_dato['_index']   = index
+    elk_dato['_type']    = doc_type
+    return elk_dato
+
+def upload_to_elk(finished=False):
+    global actions
+    global b_size
+    global seen_tweet_ids
+    if(len(actions) >= b_size) or (len(actions)>0 and finished):
+        flag = True
+        while (flag):
+            try:
+                result = bulk(es, iter(actions))
+                pprint(result)
+                flag = False
+            except Exception as e:
+                if ('ConnectionTimeout' in str(e) or 'rejected execution of' in str(e)):
+                    print('Retrying')
+                else:
+                    print(e)
+                    flag = False
+        actions         = []
+        seen_tweet_ids  = []
+
+es = Elasticsearch(['localhost:9200'], verify_certs=True, timeout=150, max_retries=10, retry_on_timeout=True)
+
+index       = 'natural_questions_0_1'
+doc_type    = 'natural_questions_map_0_1'
+actions     = []
+b_size      = 200
+
 # nq_jsonl = '/media/dpappas/dpappas_data/natural_questions/natural_questions/v1.0/sample/nq-dev-sample.jsonl.gz'
 nq_jsonl = '/media/dpappas/dpappas_data/natural_questions/natural_questions/v1.0/sample/nq-train-sample.jsonl.gz'
 with open(nq_jsonl, 'rb') as fileobj:
@@ -78,14 +114,18 @@ with open(nq_jsonl, 'rb') as fileobj:
         for i in range(len(all_ps)):
             textt   = '{}:{}'.format(dd['document_title'], i)
             result  = hashlib.md5(textt.encode()).hexdigest()
-            datum   = {
+            jdata   = {
                 '_id'               : result,
                 'paragraph_index'   : i,
                 'paragraph_text'    : all_ps[i],
                 'document_title'    : dd['document_title'],
                 'document_url'      : dd['document_url']
             }
-            pprint(datum)
+            ######################################
+            actions.append(create_an_action(jdata, jdata['_id']))
+            upload_to_elk(finished=False)
+            seen_tweet_ids.append(jdata['id_str'])
+            ######################################
     fileobj.close()
 
 
