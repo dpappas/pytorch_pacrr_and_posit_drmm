e6e27a73ae8569863e92b79e0433409209fb3050
dvpappas <dvpappas89@gmail.com>
2019-07-18 13:53:20 +0300

    message

---
+++ NaturalQuestions/create_question_index.py
@@ -0,0 +1,29 @@
+
+from elasticsearch import Elasticsearch
+from pprint import pprint
+
+index   = 'natural_questions_Q_0_1'
+map     = 'natural_questions_Q_map_0_1'
+lang    = 'english'
+
+elastic_con = Elasticsearch(['localhost:9200'], verify_certs=True, timeout=150, max_retries=10, retry_on_timeout=True)
+elastic_con.indices.delete(index=index, ignore=[400,404])
+
+mapping = {
+    "settings": {"analysis": {"analyzer": {"default": {"type": "english"}}}},
+    "mappings":{
+        map:{
+            "properties": {
+                'example_id'        : {"type": "keyword"},
+                'dataset'           :  {"type": "keyword"},
+                'document_url'      : {"type": "keyword"},
+                'document_title'    : {"type": "text", "analyzer": 'english', "fields": {"raw": {"type": "keyword"}}},
+                'question'          : {"type": "text", "analyzer": 'english', "fields": {"raw": {"type": "keyword"}}},
+                'long_answer'       : {"type": "text", "analyzer": 'english', "fields": {"raw": {"type": "keyword"}}},
+                'short_answer'      : {"type": "text", "analyzer": 'english', "fields": {"raw": {"type": "keyword"}}}
+            }
+        }
+    }
+}
+
+pprint(elastic_con.indices.create(index = index, ignore=400, body=mapping))
