2c7f9ee05832d5f364091ccd8efaa38c9be7c4c8
dvpappas <dvpappas89@gmail.com>
2019-08-11 19:25:22 +0300

    message

--- prepare_a_demo_using_ELK/prepare_training_data.py
+++ prepare_a_demo_using_ELK/prepare_training_data.py
@@ -6,6 +6,13 @@ import  re
 from elasticsearch import Elasticsearch
 from tqdm import tqdm
 import pickle
+##########
+
+# import spacy
+# from scispacy.abbreviation import AbbreviationDetector
+# from scispacy.umls_linking import UmlsEntityLinker
+# nlp = spacy.load("en_core_sci_md")
+
 
 # recall: 0.554446184347
 def get_first_n_1(question_tokens, n):
@@ -262,7 +269,7 @@ def get_first_n_5(question_tokens, n, idf_scores):
     res         = es.search(index=doc_index, body=bod, request_timeout=120)
     return res['hits']['hits']
 
-# recall:
+# recall: 0.562218834681
 def get_first_n_6(question_tokens, n, idf_scores):
     question    = ' '.join(question_tokens)
     ################################################
@@ -270,6 +277,117 @@ def get_first_n_6(question_tokens, n, idf_scores):
     for q_tok, idf_score in zip(question_tokens, idf_scores):
         the_shoulds.append({"match": {"AbstractText": {"query": q_tok, "boost": idf_score}}})
         the_shoulds.append({"match": {"ArticleTitle": {"query": q_tok, "boost": idf_score}}})
+    ################################################
+    bod         = {
+        "size": n,
+        "query": {
+            "bool" : {
+                "must": [
+                    {
+                        "range": {
+                            "DateCompleted": {
+                                "gte": "1800",
+                                "lte": "2017",
+                                "format": "dd/MM/yyyy||yyyy"
+                            }
+                        }
+                    }
+                ],
+                "should": [
+                    {
+                        "match": {
+                            "AbstractText": {
+                                "query": question,
+                                "boost": sum(idf_scores)
+                            }
+                        }
+                    },
+                    {
+                        "match": {
+                            "ArticleTitle": {
+                                "query": question,
+                                "boost": sum(idf_scores)
+                            }
+                        }
+                    },
+                    {
+                        "multi_match": {
+                            "query": question,
+                            "type": "most_fields",
+                            "fields": ["AbstractText", "ArticleTitle"],
+                            "operator": "and",
+                            "boost": sum(idf_scores)
+                        }
+                    },
+                   {
+                       "multi_match": {
+                           "query"                : question,
+                           "type"                 : "most_fields",
+                           "fields"               : ["AbstractText", "ArticleTitle"],
+                           "minimum_should_match" : "50%"
+                       }
+                   }
+                ]+the_shoulds,
+                "minimum_should_match": 1,
+            }
+        }
+    }
+    res         = es.search(index=doc_index, body=bod, request_timeout=120)
+    return res['hits']['hits']
+
+# recall: 0.567039063307
+def get_first_n_7(question_tokens, n):
+    question    = ' '.join(question_tokens)
+    ################################################
+    the_shoulds = []
+    for q_tok in question_tokens:
+        the_shoulds.append({"match": {"AbstractText": {"query": q_tok, "boost": 1}}})
+    ################################################
+    bod         = {
+        "size": n,
+        "query": {
+            "bool" : {
+                "must": [
+                    {
+                        "range": {
+                            "DateCompleted": {
+                                "gte": "1800",
+                                "lte": "2017",
+                                "format": "dd/MM/yyyy||yyyy"
+                            }
+                        }
+                    }
+                ],
+                "should": [
+                    {"match": {"AbstractText": {"query": question, "boost": 1}}},
+                    {"match": {"ArticleTitle": {"query": question, "boost": 1}}},
+                    {"multi_match": {"query": question, "type": "most_fields", "fields": ["AbstractText", "ArticleTitle"], "operator": "and", "boost": 1}},
+                    {"multi_match": {"query": question, "type": "most_fields", "fields": ["AbstractText", "ArticleTitle"], "minimum_should_match": "50%"}}
+                ]+the_shoulds,
+                "minimum_should_match": 1,
+            }
+        }
+    }
+    res         = es.search(index=doc_index, body=bod, request_timeout=120)
+    return res['hits']['hits']
+
+# recall: 0.572181183288
+def get_first_n_8(question_tokens, n, idf_scores):
+    question    = ' '.join(question_tokens)
+    ################################################
+    the_shoulds = []
+    for q_tok, idf_score in zip(question_tokens, idf_scores):
+        the_shoulds.append({"match": {"AbstractText": {"query": q_tok, "boost": idf_score}}})
+    if(len(question_tokens) > 1):
+        the_shoulds.append(
+            {
+                "span_near": {
+                    "clauses": [{"span_term": {"AbstractText": w}} for w in question_tokens],
+                    "slop": 5,
+                    "in_order": False
+                }
+            }
+        )
     ################################################
     bod         = {
         "size": n,
@@ -391,7 +509,7 @@ for question in tqdm(training_data['questions']):
     idf_scores  = [idf_val(w, idf, max_idf) for w in qtext]
     ########################################
     retrieved_pmids = []
-    for retr_doc in get_first_n_6(qtext, fetch_no, idf_scores):
+    for retr_doc in get_first_n_8(qtext, fetch_no, idf_scores):
         retrieved_pmids.append(u'http://www.ncbi.nlm.nih.gov/pubmed/{}'.format(retr_doc['_source']['pmid']))
         # print(5 * '-')
         # print(retr_doc['_score'])
