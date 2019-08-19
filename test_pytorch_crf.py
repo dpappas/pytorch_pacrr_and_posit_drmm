e4c0876b2ef729face51cd9916117d456747eb4c
dvpappas <dvpappas89@gmail.com>
2019-08-19 18:14:45 +0300

    message

--- test_pytorch_crf.py
+++ test_pytorch_crf.py
@@ -9,7 +9,8 @@ model       = CRF(num_tags)
 seq_length = 3  # maximum sequence length in a batch
 batch_size = 1  # number of samples in the batch
 emissions   = torch.randn(seq_length, batch_size, num_tags)
-tags        = torch.tensor([[0], [1], [1]], dtype=torch.long)  # (seq_length, batch_size)
+tags        = torch.ones((seq_length, batch_size), dtype=torch.long)
+# tags        = torch.tensor([[0], [1], [1]], dtype=torch.long)  # (seq_length, batch_size)
 model(emissions, tags)
 
 model.decode(emissions)
@@ -18,3 +19,5 @@ model.decode(emissions)
 or 
 https://github.com/mtreviso/linear-chain-crf/blob/master/bilstm_crf.py
 '''
+
+
