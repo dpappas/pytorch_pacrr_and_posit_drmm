a5a3d8f1fbbc3ca7505bc0dcebc374cb1973b722
dvpappas <dvpappas89@gmail.com>
2019-08-22 13:26:36 +0300

    message

---
+++ BioASQ7_competition/bert_based/needed_functions.py
@@ -0,0 +1,364 @@
+
+import  torch, pickle
+import  numpy           as np
+from    nltk.tokenize   import sent_tokenize
+from    tqdm            import tqdm
+
+class InputExample(object):
+    """A single training/test example for simple sequence classification."""
+    def __init__(self, guid, text_a, text_b=None, label=None):
+        """Constructs a InputExample.
+
+        Args:
+            guid: Unique id for the example.
+            text_a: string. The untokenized text of the first sequence. For single
+            sequence tasks, only this sequence must be specified.
+            text_b: (Optional) string. The untokenized text of the second sequence.
+            Only must be specified for sequence pair tasks.
+            label: (Optional) string. The label of the example. This should be
+            specified for train and dev examples, but not for test examples.
+        """
+        self.guid = guid
+        self.text_a = text_a
+        self.text_b = text_b
+        self.label = label
+
+class InputFeatures(object):
+    """A single set of features of data."""
+    def __init__(self, input_ids, input_mask, segment_ids, label_id):
+        self.input_ids = input_ids
+        self.input_mask = input_mask
+        self.segment_ids = segment_ids
+        self.label_id = label_id
+
+def tf(term, document):
+    tf = 0
+    for word in document:
+        if word == term:
+            tf += 1
+    if len(document) == 0:
+        return tf
+    else:
+        return tf / len(document)
+
+def similarity_score(query, document, k1, b, idf_scores, avgdl, normalize, mean, deviation, rare_word):
+    score = 0
+    for query_term in query:
+        if query_term not in idf_scores:
+            score += rare_word * (
+                    (tf(query_term, document) * (k1 + 1)) /
+                    (
+                            tf(query_term, document) +
+                            k1 * (1 - b + b * (len(document) / avgdl))
+                    )
+            )
+        else:
+            score += idf_scores[query_term] * ((tf(query_term, document) * (k1 + 1)) / (
+                        tf(query_term, document) + k1 * (1 - b + b * (len(document) / avgdl))))
+    if normalize:
+        return ((score - mean) / deviation)
+    else:
+        return score
+
+def compute_avgdl(documents):
+    total_words = 0
+    for document in documents:
+        total_words += len(document)
+    avgdl = total_words / len(documents)
+    return avgdl
+
+def weighted_binary_cross_entropy(output, target, weights=None):
+    if weights is not None:
+        assert len(weights) == 2
+        loss = weights[1] * (target * torch.log(output)) + weights[0] * ((1 - target) * torch.log(1 - output))
+    else:
+        loss = target * torch.log(output) + (1 - target) * torch.log(1 - output)
+    return torch.neg(torch.mean(loss))
+
+def RemoveTrainLargeYears(data, doc_text):
+    for i in range(len(data['queries'])):
+        hyear = 1900
+        for j in range(len(data['queries'][i]['retrieved_documents'])):
+            if data['queries'][i]['retrieved_documents'][j]['is_relevant']:
+                doc_id = data['queries'][i]['retrieved_documents'][j]['doc_id']
+                year = doc_text[doc_id]['publicationDate'].split('-')[0]
+                if year[:1] == '1' or year[:1] == '2':
+                    if int(year) > hyear:
+                        hyear = int(year)
+        j = 0
+        while True:
+            doc_id = data['queries'][i]['retrieved_documents'][j]['doc_id']
+            year = doc_text[doc_id]['publicationDate'].split('-')[0]
+            if (year[:1] == '1' or year[:1] == '2') and int(year) > hyear:
+                del data['queries'][i]['retrieved_documents'][j]
+            else:
+                j += 1
+            if j == len(data['queries'][i]['retrieved_documents']):
+                break
+    return data
+
+def RemoveBadYears(data, doc_text, train):
+    for i in range(len(data['queries'])):
+        j = 0
+        while True:
+            doc_id = data['queries'][i]['retrieved_documents'][j]['doc_id']
+            year = doc_text[doc_id]['publicationDate'].split('-')[0]
+            ##########################
+            # Skip 2017/2018 docs always. Skip 2016 docs for training.
+            # Need to change for final model - 2017 should be a train year only.
+            # Use only for testing.
+            if year == '2017' or year == '2018' or (train and year == '2016'):
+                # if year == '2018' or (train and year == '2017'):
+                del data['queries'][i]['retrieved_documents'][j]
+            else:
+                j += 1
+            ##########################
+            if j == len(data['queries'][i]['retrieved_documents']):
+                break
+    return data
+
+def get_words(s, idf, max_idf):
+    sl = tokenize(s)
+    sl = [s for s in sl]
+    sl2 = [s for s in sl if idf_val(s, idf, max_idf) >= 2.0]
+    return sl, sl2
+
+def fix_bert_tokens(tokens):
+    ret = []
+    for t in tokens:
+        if (t.startswith('##')):
+            ret[-1] = ret[-1] + t[2:]
+        else:
+            ret.append(t)
+    return ret
+
+def tokenize(x, bert_tokenizer):
+    x_tokens = bert_tokenizer.tokenize(x)
+    x_tokens = fix_bert_tokens(x_tokens)
+    return x_tokens
+
+def idf_val(w, idf, max_idf):
+    if w in idf:
+        return idf[w]
+    return max_idf
+
+def get_embeds(tokens, wv):
+    ret1, ret2 = [], []
+    for tok in tokens:
+        if (tok in wv):
+            ret1.append(tok)
+            ret2.append(wv[tok])
+    return ret1, np.array(ret2, 'float64')
+
+def get_embeds_use_unk(tokens, wv, embedding_dim):
+    ret1, ret2 = [], []
+    for tok in tokens:
+        if (tok in wv):
+            ret1.append(tok)
+            ret2.append(wv[tok])
+        else:
+            wv[tok] = np.random.randn(embedding_dim)
+            ret1.append(tok)
+            ret2.append(wv[tok])
+    return ret1, np.array(ret2, 'float64')
+
+def get_embeds_use_only_unk(tokens, wv, embedding_dim):
+    ret1, ret2 = [], []
+    for tok in tokens:
+        wv[tok] = np.random.randn(embedding_dim)
+        ret1.append(tok)
+        ret2.append(wv[tok])
+    return ret1, np.array(ret2, 'float64')
+
+def load_idfs(idf_path, words):
+    print('Loading IDF tables')
+    #
+    # with open(dataloc + 'idf.pkl', 'rb') as f:
+    with open(idf_path, 'rb') as f:
+        idf = pickle.load(f)
+    ret = {}
+    for w in words:
+        if w in idf:
+            ret[w] = idf[w]
+    max_idf = 0.0
+    for w in idf:
+        if idf[w] > max_idf:
+            max_idf = idf[w]
+    idf = None
+    print('Loaded idf tables with max idf {}'.format(max_idf))
+    #
+    return ret, max_idf
+
+def uwords(words):
+    uw = {}
+    for w in words:
+        uw[w] = 1
+    return [w for w in uw]
+
+def ubigrams(words):
+    uw = {}
+    prevw = "<pw>"
+    for w in words:
+        uw[prevw + '_' + w] = 1
+        prevw = w
+    return [w for w in uw]
+
+def query_doc_overlap(qwords, dwords, idf, max_idf):
+    # % Query words in doc.
+    qwords_in_doc = 0
+    idf_qwords_in_doc = 0.0
+    idf_qwords = 0.0
+    for qword in uwords(qwords):
+        idf_qwords += idf_val(qword, idf, max_idf)
+        for dword in uwords(dwords):
+            if qword == dword:
+                idf_qwords_in_doc += idf_val(qword, idf, max_idf)
+                qwords_in_doc += 1
+                break
+    if len(qwords) <= 0:
+        qwords_in_doc_val = 0.0
+    else:
+        qwords_in_doc_val = (float(qwords_in_doc) /
+                             float(len(uwords(qwords))))
+    if idf_qwords <= 0.0:
+        idf_qwords_in_doc_val = 0.0
+    else:
+        idf_qwords_in_doc_val = float(idf_qwords_in_doc) / float(idf_qwords)
+    # % Query bigrams  in doc.
+    qwords_bigrams_in_doc = 0
+    idf_qwords_bigrams_in_doc = 0.0
+    idf_bigrams = 0.0
+    for qword in ubigrams(qwords):
+        wrds = qword.split('_')
+        idf_bigrams += idf_val(wrds[0], idf, max_idf) * idf_val(wrds[1], idf, max_idf)
+        for dword in ubigrams(dwords):
+            if qword == dword:
+                qwords_bigrams_in_doc += 1
+                idf_qwords_bigrams_in_doc += (idf_val(wrds[0], idf, max_idf) * idf_val(wrds[1], idf, max_idf))
+                break
+    if len(qwords) <= 0:
+        qwords_bigrams_in_doc_val = 0.0
+    else:
+        qwords_bigrams_in_doc_val = (float(qwords_bigrams_in_doc) / float(len(ubigrams(qwords))))
+    if idf_bigrams <= 0.0:
+        idf_qwords_bigrams_in_doc_val = 0.0
+    else:
+        idf_qwords_bigrams_in_doc_val = (float(idf_qwords_bigrams_in_doc) / float(idf_bigrams))
+    return [
+        qwords_in_doc_val,
+        qwords_bigrams_in_doc_val,
+        idf_qwords_in_doc_val,
+        idf_qwords_bigrams_in_doc_val
+    ]
+
+def GetScores(qtext, dtext, bm25, idf, max_idf):
+    qwords, qw2 = get_words(qtext, idf, max_idf)
+    dwords, dw2 = get_words(dtext, idf, max_idf)
+    qd1 = query_doc_overlap(qwords, dwords, idf, max_idf)
+    bm25 = [bm25]
+    return qd1[0:3] + bm25
+
+def GetWords(data, doc_text, words):
+    for i in tqdm(range(len(data['queries'])), ascii=True):
+        qwds = tokenize(data['queries'][i]['query_text'])
+        for w in qwds:
+            words[w] = 1
+        for j in range(len(data['queries'][i]['retrieved_documents'])):
+            doc_id = data['queries'][i]['retrieved_documents'][j]['doc_id']
+            dtext = (
+                    doc_text[doc_id]['title'] + ' <title> ' + doc_text[doc_id]['abstractText'] +
+                    ' '.join(
+                        [
+                            ' '.join(mm) for mm in
+                            get_the_mesh(doc_text[doc_id])
+                        ]
+                    )
+            )
+            dwds = tokenize(dtext)
+            for w in dwds:
+                words[w] = 1
+
+def get_gold_snips(quest_id, bioasq6_data):
+    gold_snips = []
+    if ('snippets' in bioasq6_data[quest_id]):
+        for sn in bioasq6_data[quest_id]['snippets']:
+            gold_snips.extend(sent_tokenize(sn['text']))
+    return list(set(gold_snips))
+
+def prep_extracted_snippets(extracted_snippets, docs, qid, top10docs, quest_body):
+    ret = {
+        'body': quest_body,
+        'documents': top10docs,
+        'id': qid,
+        'snippets': [],
+    }
+    for esnip in extracted_snippets:
+        pid = esnip[2].split('/')[-1]
+        the_text = esnip[3]
+        esnip_res = {
+            # 'score'     : esnip[1],
+            "document": "http://www.ncbi.nlm.nih.gov/pubmed/{}".format(pid),
+            "text": the_text
+        }
+        try:
+            ind_from = docs[pid]['title'].index(the_text)
+            ind_to = ind_from + len(the_text)
+            esnip_res["beginSection"] = "title"
+            esnip_res["endSection"] = "title"
+            esnip_res["offsetInBeginSection"] = ind_from
+            esnip_res["offsetInEndSection"] = ind_to
+        except:
+            # print(the_text)
+            # pprint(docs[pid])
+            ind_from = docs[pid]['abstractText'].index(the_text)
+            ind_to = ind_from + len(the_text)
+            esnip_res["beginSection"] = "abstract"
+            esnip_res["endSection"] = "abstract"
+            esnip_res["offsetInBeginSection"] = ind_from
+            esnip_res["offsetInEndSection"] = ind_to
+        ret['snippets'].append(esnip_res)
+    return ret
+
+def get_snips(quest_id, gid, bioasq6_data):
+    good_snips = []
+    if ('snippets' in bioasq6_data[quest_id]):
+        for sn in bioasq6_data[quest_id]['snippets']:
+            if (sn['document'].endswith(gid)):
+                good_snips.extend(sent_tokenize(sn['text']))
+    return good_snips
+
+def get_the_mesh(the_doc):
+    good_meshes = []
+    if ('meshHeadingsList' in the_doc):
+        for t in the_doc['meshHeadingsList']:
+            t = t.split(':', 1)
+            t = t[1].strip()
+            t = t.lower()
+            good_meshes.append(t)
+    elif ('MeshHeadings' in the_doc):
+        for mesh_head_set in the_doc['MeshHeadings']:
+            for item in mesh_head_set:
+                good_meshes.append(item['text'].strip().lower())
+    if ('Chemicals' in the_doc):
+        for t in the_doc['Chemicals']:
+            t = t['NameOfSubstance'].strip().lower()
+            good_meshes.append(t)
+    good_mesh = sorted(good_meshes)
+    good_mesh = ['mesh'] + good_mesh
+    # good_mesh = ' # '.join(good_mesh)
+    # good_mesh = good_mesh.split()
+    # good_mesh = [gm.split() for gm in good_mesh]
+    good_mesh = [gm for gm in good_mesh]
+    return good_mesh
+
+def snip_is_relevant(one_sent, gold_snips):
+    return int(
+        any(
+            [
+                (one_sent.encode('ascii', 'ignore') in gold_snip.encode('ascii', 'ignore'))
+                or
+                (gold_snip.encode('ascii', 'ignore') in one_sent.encode('ascii', 'ignore'))
+                for gold_snip in gold_snips
+            ]
+        )
+    )
