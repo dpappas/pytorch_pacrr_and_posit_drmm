03753d6099acac6f62a01ce0bd9c328697d2e2e2
dvpappas <dvpappas89@gmail.com>
2019-08-22 14:27:13 +0300

    message

--- BioASQ7_competition/bert_based/train_bert_jpdrmm.py
+++ BioASQ7_competition/bert_based/train_bert_jpdrmm.py
@@ -1490,7 +1490,9 @@ b_size              = 32
 max_epoch           = 4
 #####################
 
-(dev_data, dev_docs, train_data, train_docs, idf, max_idf, bioasq6_data) = load_all_data(dataloc=dataloc, idf_pickle_path=idf_pickle_path)
+(dev_data, dev_docs, train_data, train_docs, idf, max_idf, bioasq6_data) = load_all_data(
+    dataloc=dataloc, idf_pickle_path=idf_pickle_path, bert_all_words_path=bert_all_words_path
+)
 
 hdlr = None
 for run in range(0, 5):
