18750bf3c3119d861d840e6d3f66a1c64fff61e3
dvpappas <dvpappas89@gmail.com>
2019-08-22 14:14:27 +0300

    message

--- BioASQ7_competition/bert_based/bert_needed_functions.py
+++ BioASQ7_competition/bert_based/bert_needed_functions.py
@@ -9,22 +9,22 @@ from    sklearn.preprocessing   import OneHotEncoder
 from    difflib                 import SequenceMatcher
 from    pprint                  import pprint
 
-retrieval_jar_path = None
-bert_tokenizer  = None
-bert_model      = None
-model           = None
-idf             = None
-device          = None
-bioasq6_data    = None
-logger          = None
-train_docs      = None
-odir            = ''
-avgdl           = 0.0
-mean            = 0.0
-deviation       = 0.0
-max_idf         = 0.0
-embedding_dim   = 0
-max_seq_length  = 0
+retrieval_jar_path  = None
+bert_tokenizer      = None
+bert_model          = None
+model               = None
+idf                 = None
+device              = None
+bioasq6_data        = None
+logger              = None
+train_docs          = None
+odir                = ''
+avgdl               = 0.0
+mean                = 0.0
+deviation           = 0.0
+max_idf             = 0.0
+embedding_dim       = 0
+max_seq_length      = 0
 
 softmax         = lambda z: np.exp(z) / np.sum(np.exp(z))
 stopwords       = nltk.corpus.stopwords.words("english")
