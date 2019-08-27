baefbcaf13741d777fea724f7f856cc7d70c8a0f
dvpappas <dvpappas89@gmail.com>
2019-08-27 14:06:15 +0300

    message

--- BioASQ7_competition/pytorch_transformers_based/train_jbert.py
+++ BioASQ7_competition/pytorch_transformers_based/train_jbert.py
@@ -2,14 +2,23 @@
 import torch
 from pytorch_transformers import *
 
-# (model_class, tokenizer_class, pretrained_weights) = (BertModel, BertTokenizer, 'bert-base-uncased')
-(model_class, tokenizer_class, pretrained_weights) = (RobertaModel, RobertaTokenizer, 'roberta-base')
+(model_class, tokenizer_class, pretrained_weights) = (BertModel, BertTokenizer, 'bert-base-uncased')
+# (model_class, tokenizer_class, pretrained_weights) = (RobertaModel, RobertaTokenizer, 'roberta-base')
 
 tokenizer   = tokenizer_class.from_pretrained(pretrained_weights)
 model       = model_class.from_pretrained(pretrained_weights)
 
-# Encode text
-input_ids = torch.tensor([tokenizer.encode("Here is some text to encode")])
+sents           = ["Here is some text to encode", "Here is another text to see whether size matters"]
+tokenized_sents = [tokenizer.encode(sent) for sent in sents]
+max_len         = max(len(sent) for sent in tokenized_sents)
+pad_id          = tokenizer.convert_tokens_to_ids(tokenizer.pad_token)
+input_ids       = torch.tensor(
+    [
+        sent_ids + ([pad_id] * (max_len - len(sent_ids)))
+        for sent_ids in tokenized_sents
+    ]
+)
+
 with torch.no_grad():
     last_hidden_states = model(input_ids)[0]  # Models outputs are now tuples
 
