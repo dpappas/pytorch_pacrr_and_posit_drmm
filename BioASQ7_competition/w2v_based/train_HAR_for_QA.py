3c217554f752ce2e52287d934495355bb71a3dbd
dvpappas <dvpappas89@gmail.com>
2019-08-28 16:51:31 +0300

    message

--- BioASQ7_competition/w2v_based/train_HAR_for_QA.py
+++ BioASQ7_competition/w2v_based/train_HAR_for_QA.py
@@ -36,19 +36,19 @@ use_cuda    = torch.cuda.is_available()
 device      = torch.device("cuda") if(use_cuda) else torch.device("cpu")
 
 class HAR_Modeler(nn.Module):
-    def __init__(self, embedding_dim = 30, H = 10):
+    def __init__(self, embedding_dim = 30,  gru_hidden = 5):
         super(HAR_Modeler, self).__init__()
         #
-        self.q_h0       = autograd.Variable(torch.randn(2, 1, H)).to(device)
-        self.d_h0       = autograd.Variable(torch.randn(2, 1, H)).to(device)
-        self.bigru_q    = nn.GRU(input_size=embedding_dim, hidden_size=H, bidirectional=True, batch_first=False).to(device)
-        self.bigru_d    = nn.GRU(input_size=embedding_dim, hidden_size=H, bidirectional=True, batch_first=False).to(device)
-        self.wc         = torch.nn.Parameter(torch.randn(1, 6*H))
+        self.H          = 2 * gru_hidden
+        self.q_h0       = autograd.Variable(torch.randn(2, 1, gru_hidden)).to(device)
+        self.d_h0       = autograd.Variable(torch.randn(2, 1, gru_hidden)).to(device)
+        self.bigru_q    = nn.GRU(input_size=embedding_dim, hidden_size=gru_hidden, bidirectional=True, batch_first=False).to(device)
+        self.bigru_d    = nn.GRU(input_size=embedding_dim, hidden_size=gru_hidden, bidirectional=True, batch_first=False).to(device)
+        self.wc         = torch.nn.Parameter(torch.randn(1, 3*self.H))
         #
     def forward(self, doc_sents_embeds, question_embeds):
         q_context           = self.bigru_q(question_embeds.unsqueeze(1), self.q_h0)[0].squeeze(1)
         d_contexts          = [self.bigru_d(sent_embs.unsqueeze(1), self.d_h0)[0].squeeze(1) for sent_embs in doc_sents_embeds]
-        sxy_per_sent = []
         for sent_embed in d_contexts:
             cols = []
             for uyq in q_context:
@@ -59,8 +59,15 @@ class HAR_Modeler(nn.Module):
                     row.append(sxy)
                 row = torch.cat(row).squeeze()
                 cols.append(row)
-            cols = torch.stack(cols)
-            sxy_per_sent.append(cols)
+            sxy     = torch.stack(cols).transpose(0,1)
+            ########
+            sd2q    = F.softmax(sxy, dim=1)
+            sq2d    = F.softmax(sxy, dim=0)
+            ########
+            ad2q    = torch.mm(sd2q, q_context)
+            aq2d    = torch.mm(sd2q, sq2d.transpose(0, 1))
+            aq2d    = torch.mm(aq2d, sent_embed)
+            ########
 
 
 
