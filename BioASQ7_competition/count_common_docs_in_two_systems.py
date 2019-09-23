78532c8009e2d1e14cbc00280f3087cdae1fc4e4
dvpappas <dvpappas89@gmail.com>
2019-09-23 16:55:50 +0300

    message

--- BioASQ7_competition/count_common_docs_in_two_systems.py
+++ BioASQ7_competition/count_common_docs_in_two_systems.py
@@ -7,7 +7,6 @@ import numpy as np
 
 fpath1  = "/home/dpappas/bioasq_all/bioasq7/document_results/b12345_joined/jpdrmm.json"
 fpath2  = "/home/dpappas/bioasq_all/bioasq7/document_results/b12345_joined/JBERT.json"
-
 goldf   = '/home/dpappas/bioasq_all/bioasq7/data/BioASQ-task7bPhaseB-testset12345'
 
 d1      = json.load(open(fpath1))
@@ -22,19 +21,23 @@ gold_common     = []
 gold_not_common = []
 for k in d1:
     ######################################################
-    docs1       = d1[k]['documents']
-    docs2       = d2[k]['documents']
-    docsg       = gold_d[k]['documents']
+    docs1           = d1[k]['documents']
+    docs2           = d2[k]['documents']
+    docsg           = gold_d[k]['documents']
     ######################################################
-    koina       = set(docs1).intersection(set(docs2))
-    gold_koina  = set(koina).intersection(set(docsg))
+    koina           = set(docs1).intersection(set(docs2))
+    not_koina       = set(docs1).union(set(docs2)) - koina
+    gold_koina      = set(koina).intersection(set(docsg))
+    gold_not_koina  = set(not_koina).intersection(set(docsg))
     ######################################################
+    gold_not_common.append(float(len(gold_not_koina)) / float(len(not_koina)))
     gold_common.append(float(len(gold_koina)) / float(len(koina)))
     common.append(float(len(koina)) / float(10.))
     ######################################################
     # break
 
 print(np.average(gold_common))
+print(np.average(gold_not_common))
 print(np.average(common))
 
 # jpdrmm jbert-NOT-frozen           0.7326
