fe8af59178dae61971cc63be0a2439cd2377d8ba
dvpappas <dvpappas89@gmail.com>
2019-09-26 14:16:21 +0300

    message

--- the_graph/create_graph_and_insert.py
+++ the_graph/create_graph_and_insert.py
@@ -1,6 +1,14 @@
 
+from py2neo import Graph, Node
+graph = Graph(password="nlp.aueb.group!")
+
+
+alice = Node("Person", name="Alice")
+graph.create(alice)
+german, speaks = graph.create({"name": "German"}, (alice, "SPEAKS", 0))
 
 
 
-from py2neo import Graph
+
+
 
