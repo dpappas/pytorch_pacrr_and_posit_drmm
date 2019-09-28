74ec36bd9066e6e5f6af80843852065768721cd1
dvpappas <dvpappas89@gmail.com>
2019-09-28 14:17:44 +0300

    message

--- BioASQ7_competition/bm25_baseline.py
+++ BioASQ7_competition/bm25_baseline.py
@@ -6,20 +6,12 @@ w2v_bin_path        = '/home/dpappas/bioasq_all/pubmed2018_w2v_30D.bin'
 idf_pickle_path     = '/home/dpappas/bioasq_all/idf.pkl'
 dataloc             = '/home/dpappas/bioasq_all/bioasq7_data/'
 
-bm25_data           = {
-    'questions' : [
-        {
-            "body": "n/a",
-            "id": "5c5607aa07647bbc4b00000e",
-            "documents": []
-        },
-    ]
-}
 
 b       = 1 #sys.argv[1]
 f_in1   = '/home/dpappas/bioasq_all/bioasq7/data/test_batch_{}/BioASQ-task7bPhaseA-testset{}'.format(b, b)
 f_in2   = '/home/dpappas/bioasq_all/bioasq7/data/test_batch_{}/bioasq7_bm25_top100/bioasq7_bm25_top100.test.pkl'.format(b)
 f_in3   = '/home/dpappas/bioasq_all/bioasq7/data/test_batch_{}/bioasq7_bm25_top100/bioasq7_bm25_docset_top100.test.pkl'.format(b)
+f_out   = '/home/dpappas/bioasq_all/bioasq7/document_results/test_batch_{}/bm25.json'.format(b)
 
 ###########################################################
 print('loading pickle data')
@@ -39,6 +31,21 @@ with open(f_in3, 'rb') as f:
     test_docs = pickle.load(f)
 
 ###########################################################
+
+bm25_data   = {'questions': []}
+for q in test_data['queries']:
+    docs = ["http://www.ncbi.nlm.nih.gov/pubmed/{}".format(d['doc_id']) for d in q['retrieved_documents']]
+    bm25_data   = ['questions'].append(
+        {
+            "body"      : "n/a",
+            "id"        : q['query_id'],
+            "documents" : docs[:10]
+        }
+    )
+
+with open(f_out, 'w') as f:
+    f.write(json.dumps(bm25_data, indent=4, sort_keys=False))
+    f.close()
 
 
 
