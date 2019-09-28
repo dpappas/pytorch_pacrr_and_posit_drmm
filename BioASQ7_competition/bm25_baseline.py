57b04348afb882baf3371768d6560d381f9bff51
dvpappas <dvpappas89@gmail.com>
2019-09-28 14:08:36 +0300

    message

--- BioASQ7_competition/bm25_baseline.py
+++ BioASQ7_competition/bm25_baseline.py
@@ -1,30 +1,44 @@
 
-import pickle, json
+import pickle, json, sys
+from pprint import pprint
 
 w2v_bin_path        = '/home/dpappas/bioasq_all/pubmed2018_w2v_30D.bin'
 idf_pickle_path     = '/home/dpappas/bioasq_all/idf.pkl'
 dataloc             = '/home/dpappas/bioasq_all/bioasq7_data/'
 
-def load_all_data(dataloc):
-    print('loading pickle data')
-    #
-    with open(dataloc+'trainining7b.json', 'r') as f:
-        bioasq7_data = json.load(f)
-        bioasq7_data = dict((q['id'], q) for q in bioasq7_data['questions'])
-    #
-    with open(dataloc + 'bioasq7_bm25_top100.dev.pkl', 'rb') as f:
-        dev_data = pickle.load(f)
-    with open(dataloc + 'bioasq7_bm25_docset_top100.dev.pkl', 'rb') as f:
-        dev_docs = pickle.load(f)
-    with open(dataloc + 'bioasq7_bm25_top100.train.pkl', 'rb') as f:
-        train_data = pickle.load(f)
-    with open(dataloc + 'bioasq7_bm25_docset_top100.train.pkl', 'rb') as f:
-        train_docs = pickle.load(f)
-    print('loading words')
-    return dev_data, dev_docs, train_data, train_docs, bioasq7_data
+bm25_data           = {
+    'questions' : [
+        {
+            "body": "n/a",
+            "id": "5c5607aa07647bbc4b00000e",
+            "documents": []
+        },
+    ]
+}
 
+b       = 1 #sys.argv[1]
+f_in1   = '/home/dpappas/bioasq_all/bioasq7/data/test_batch_{}/BioASQ-task7bPhaseA-testset{}'.format(b, b)
+f_in2   = '/home/dpappas/bioasq_all/bioasq7/data/test_batch_{}/bioasq7_bm25_top100/bioasq7_bm25_top100.test.pkl'.format(b)
+f_in3   = '/home/dpappas/bioasq_all/bioasq7/data/test_batch_{}/bioasq7_bm25_top100/bioasq7_bm25_docset_top100.test.pkl'.format(b)
 
-(dev_data, dev_docs, train_data, train_docs, bioasq7_data) = load_all_data(dataloc)
+###########################################################
+print('loading pickle data')
+with open(f_in1, 'r') as f:
+    bioasq7_data = json.load(f)
+    for q in bioasq7_data['questions']:
+        if("documents" not in q):
+            q["documents"]  = []
+        if("snippets" not in q):
+            q["snippets"]   = []
+    bioasq7_data = dict((q['id'], q) for q in bioasq7_data['questions'])
+
+with open(f_in2, 'rb') as f:
+    test_data = pickle.load(f)
+
+with open(f_in3, 'rb') as f:
+    test_docs = pickle.load(f)
+
+###########################################################
 
 
 
