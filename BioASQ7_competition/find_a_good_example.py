72446b8f57e8a6e07667cf6c5d4970c19764f04d
dvpappas <dvpappas89@gmail.com>
2019-10-07 12:41:13 +0300

    message

--- BioASQ7_competition/find_a_good_example.py
+++ BioASQ7_competition/find_a_good_example.py
@@ -1,6 +1,6 @@
 
 from collections import Counter
-import  json, re
+import  json, re, pickle
 from    pprint          import pprint
 from    nltk.tokenize   import sent_tokenize
 bioclean = lambda t: re.sub('[.,?;*!%^&_+():-\[\]{}]', '', t.replace('"', '').replace('/', '').replace('\\', '').replace("'", '').strip().lower()).split()
@@ -21,6 +21,11 @@ batch = 1
 fg = '/home/dpappas/bioasq_all/bioasq7/data/test_batch_{}/BioASQ-task7bPhaseB-testset{}'.format(batch, batch)
 f1 = '/home/dpappas/bioasq_all/bioasq7/document_results/test_batch_{}/jpdrmm.json'.format(batch)
 f2 = '/home/dpappas/bioasq_all/bioasq7/snippet_results/test_batch_{}/pdrmm_pdrmm.json'.format(batch)
+f3 = '/home/dpappas/bioasq_all/test_batch_{}/bioasq7_bm25_top100/bioasq7_bm25_top100.test.pkl'.format(batch)
+
+with open(f3, 'rb') as f:
+    test_data = pickle.load(f)
+    test_data = dict((item['query_id'], item) for item in test_data['queries'])
 
 gt = json.load(open(fg))
 gt = dict((item['id'], item) for item in gt['questions'])
@@ -32,6 +37,13 @@ s2 = dict((item['id'], item) for item in s2['questions'])
 len_max = 30
 held_out = {}
 for k in s1:
+    ########################################
+    # could not be retrieved by IR
+    gold_snips_doc                  = set([sn['document'].split('/')[-1].strip() for sn in gt[k]['snippets']])
+    retrieved_docs_from_ir          = set(t['doc_id'] for t in test_data[k]['retrieved_documents'])
+    print(gt[k]['body'], gold_snips_doc-retrieved_docs_from_ir)
+    could_not_be_retrieved_by_ir    = [sn for sn in gt[k]['snippets'] if sn['document'].split('/')[-1].strip() in (gold_snips_doc - retrieved_docs_from_ir)]
+    ########################################
     gt_snips = []
     for item in gt[k]['snippets']:
         gt_snips.extend(list(zip(sent_tokenize(item['text'].strip()), len(sent_tokenize(item['text'].strip()))*[item['document']])))
@@ -61,7 +73,7 @@ for k in s1:
     found_by_1      = set(found_by_1) - common_snips
     found_by_2      = set(found_by_2) - common_snips
     if(len(found_by_1)!=0 and len(found_by_2)!=0):
-        held_out[qtext] = (gt_snips, found_by_1, found_by_2, common_docs)
+        held_out[qtext] = (gt_snips, found_by_1, found_by_2, common_docs, could_not_be_retrieved_by_ir)
     if(qtext == 'Is ibudilast effective for multiple sclerosis?'):
         break
 
