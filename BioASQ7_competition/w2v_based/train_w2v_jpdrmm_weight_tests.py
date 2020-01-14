37dbd11a5f39f8cb55d210ca3eece8ef310f42aa
dvpappas <dvpappas89@gmail.com>
2020-01-14 17:57:55 +0200

    message

--- BioASQ7_competition/w2v_based/train_w2v_jpdrmm_weight_tests.py
+++ BioASQ7_competition/w2v_based/train_w2v_jpdrmm_weight_tests.py
@@ -777,9 +777,7 @@ def train_one(epoch, bioasq6_data, two_losses, use_sent_tokenizer):
         if(two_losses):
             sn_d1_l, sn_d2_l                = get_two_snip_losses(good_sent_tags, gs_emits_, bs_emits_)
             snip_loss                       = sn_d1_l + sn_d2_l
-            # snip_loss = sn_d1_l
-            l                               = 0.5
-            cost_                           = ((1 - l) * snip_loss) + (l * cost_)
+            cost_                           = weight * snip_loss + cost_
         #
         batch_acc.append(float(doc1_emit_ > doc2_emit_))
         epoch_acc.append(float(doc1_emit_ > doc2_emit_))
@@ -1459,7 +1457,7 @@ for run in range(run_from, run_to):
             break
 
 # CUDA_VISIBLE_DEVICES=1 python3.6 weight_tune.py 0.1
-# CUDA_VISIBLE_DEVICES=1 python3.6 weight_tune.py 0.5
+# CUDA_VISIBLE_DEVICES=1 python3.6 weight_tune.py 0.2
 # CUDA_VISIBLE_DEVICES=1 python3.6 weight_tune.py 1
 # CUDA_VISIBLE_DEVICES=1 python3.6 weight_tune.py 5
 # CUDA_VISIBLE_DEVICES=1 python3.6 weight_tune.py 10
