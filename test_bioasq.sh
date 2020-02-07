48abf210b35e5e4758e3fd705e9d695d08a6cff8
dvpappas <dvpappas89@gmail.com>
2020-02-07 13:36:13 +0200

    message

--- test_bioasq.sh
+++ test_bioasq.sh
@@ -23,3 +23,17 @@ grep -E '^MAP snippets:|^MAP documents:' \
 #sh test_bioasq.sh ablation_1111111_0p01_9_bioasq_jpdrmm_2L_0p01_run_0
 
 
+#DIRI=$1
+#
+#CUDA_VISIBLE_DEVICES=0 python3.6 eval_test.py 12345 \
+#/media/dpappas/dpappas_data/models_out/${DIRI}/best_dev_checkpoint.pth.tar \
+#/media/dpappas/dpappas_data/models_out/${DIRI}/test/
+#
+#java -Xmx10G -cp \
+#"/home/DISK_1/dpappas/bioasq_all/dist/my_bioasq_eval_2.jar evaluation.EvaluatorTask1b" \
+#-phaseA -e 5 \
+#"/home/DISK_1/dpappas/bioasq_all/bioasq7/data/test_batch_12345/BioASQ-task7bPhaseB-testset12345" \
+#"/media/dpappas/dpappas_data/models_out/${DIRI}/test/v3 test_emit_bioasq.json" | \
+#grep -E '^MAP snippets:|^MAP documents:' \
+#> "/home/DISK_1/dpappas/bioasq_all/models_out/${DIRI}/test/results.txt"
+
