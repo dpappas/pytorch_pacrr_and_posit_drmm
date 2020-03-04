c145d3d44b87c9e2ee1838af0d98b135c193fc68
dvpappas <dvpappas89@gmail.com>
2020-03-04 14:42:42 +0200

    message

--- bioasq_2020/system_3.py
+++ bioasq_2020/system_3.py
@@ -1,10 +1,9 @@
 
 __author__ = 'Dimitris'
 
-import  json
-import  numpy                       as np
-from    tqdm                        import tqdm
-import  pickle, os
+import  numpy as np
+from    tqdm  import tqdm
+import  pickle, os, json
 from adhoc_vectorizer import get_sentence_vecs
 from my_sentence_splitting import get_sents
 
