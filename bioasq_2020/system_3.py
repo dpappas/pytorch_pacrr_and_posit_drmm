a1f4c193ac010159d0bb4308c1b31178c9e5e3f7
dvpappas <dvpappas89@gmail.com>
2020-03-04 14:38:58 +0200

    message

--- bioasq_2020/system_3.py
+++ bioasq_2020/system_3.py
@@ -8,5 +8,11 @@ import  pickle, os
 from adhoc_vectorizer import get_sentence_vecs
 from my_sentence_splitting import get_sents
 
+in_dir = '/home/dpappas/bioasq_all/bioasq8/data/test_batch_1/bioasq8_bm25_top100/'
+
+docs_data   = os.path.join(in_dir, )
+ret_data    = os.path.join(in_dir, )
+
+
 
 
