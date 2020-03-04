a758b52c6b837d584c13ea2cf3b981ff36beb0c6
dvpappas <dvpappas89@gmail.com>
2020-03-04 15:06:07 +0200

    message

--- bioasq_2020/system_3.py
+++ bioasq_2020/system_3.py
@@ -6,12 +6,18 @@ from    tqdm  import tqdm
 import  pickle, os, json
 from adhoc_vectorizer import get_sentence_vecs
 from my_sentence_splitting import get_sents
+from pprint import pprint
 
 in_dir      = '/home/dpappas/bioasq_all/bioasq8/data/test_batch_1/bioasq8_bm25_top100/'
 
-docs_data   = os.path.join(in_dir, 'bioasq8_bm25_docset_top100.test.pkl')
-ret_data    = os.path.join(in_dir, 'bioasq8_bm25_top100.test.pkl')
+docs_data   = pickle.load(open(os.path.join(in_dir, 'bioasq8_bm25_docset_top100.test.pkl'), 'rb'))
+ret_data    = pickle.load(open(os.path.join(in_dir, 'bioasq8_bm25_top100.test.pkl'), 'rb'))
 
+pprint(ret_data['queries'][0])
+
+for quer in ret_data['queries']:
+    qid     = quer['query_id']
+    qtext   = quer['query_text']
 
 
 
