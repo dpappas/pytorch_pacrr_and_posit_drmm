da1e54dfd62e73de601d92d7228829bd4b75b1c5
dvpappas <dvpappas89@gmail.com>
2020-03-17 23:27:42 +0200

    message

--- BioASQ7_competition/w2v_based/graph_based/train_graph_jpdrmm.py
+++ BioASQ7_competition/w2v_based/graph_based/train_graph_jpdrmm.py
@@ -18,6 +18,7 @@ from    pprint                      import pprint
 from    gensim.models.keyedvectors  import KeyedVectors
 from    nltk.tokenize               import sent_tokenize
 from    difflib                     import SequenceMatcher
+import string
 
 bioclean    = lambda t: re.sub('[.,?;*!%^&_+():-\[\]{}]', '', t.replace('"', '').replace('/', '').replace('\\', '').replace("'", '').strip().lower()).split()
 softmax     = lambda z: np.exp(z) / np.sum(np.exp(z))
@@ -402,17 +403,22 @@ def idf_val(w, idf, max_idf):
         return idf[w]
     return max_idf
 
-def get_embeds(tokens, wv):
+def randomString(stringLength=10):
+    """Generate a random string of fixed length """
+    letters = string.ascii_lowercase + string.ascii_uppercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+def get_embeds(tokens, graph_embeds):
     ret1, ret2 = [], []
     for tok in tokens:
-        if (tok in wv):
+        if (tok in graph_embeds):
             ret1.append(tok)
-            ret2.append(wv[tok])
+            ret2.append(graph_embeds[tok])
         else:
             pass
     if(len(ret1)==0):
-        ret1.append('UNKN')
-        ret2.append(wv['UNKN'])
+        ret1.append(randomString(stringLength=10))
+        ret2.append(graph_embeds['UNKN'])
     return ret1, np.array(ret2, 'float64')
 
 def get_embeds_use_unk(tokens, wv):
