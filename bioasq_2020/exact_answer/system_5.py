028f4eb4978d1fbeb3d0979434d99f480f1f9c97
dvpappas <dvpappas89@gmail.com>
2020-03-19 16:17:06 +0200

    message

--- bioasq_2020/exact_answer/system_5.py
+++ bioasq_2020/exact_answer/system_5.py
@@ -82,8 +82,53 @@ def check_answer(ans, quest):
     return True
 
 data            = json.load(open('/home/dpappas/BioASQ-task8bPhaseB-testset2'))
-fpath3          = '/home/dpappas/bioasq8_batch2_system1_results.json'
-f3              = '/home/dpappas/system_1_jpdrmm_sgrank.json'
+f1              = '/home/dpappas/system_5_gold_sgrank.json'
 idf_pickle_path = '/home/dpappas/bioasq_all/idf.pkl'
 
 idf, max_idf    = load_idfs(idf_pickle_path)
+
+qid2type = dict((quest['id'], quest['type']) for quest in data['questions'])
+
+for quest in data['questions']:
+    if(quest['type'] not in ['factoid', 'list']):
+        continue
+    # print(quest['body'])
+    # print(20*'-')
+    # my_ents     = []
+    # my_kt       = []
+    # for snip in quest['snippets']:
+    #     my_kt.extend(
+    #         [
+    #             ph[0].lower() for ph in get_keyphrases_sgrank(snip['text'], idfs=idf)
+    #             if(check_answer(ph[0], quest['body']))
+    #         ]
+    #     )
+    #     my_ents.extend(
+    #         [
+    #             ph.lower() for ph in get_phrases(snip['text'])
+    #             if(check_answer(ph, quest['body']))
+    #         ]
+    #     )
+    ########################################
+    my_kt_ens   = []
+    all_snips = ' \n'.join(snip['text'] for snip in quest['snippets'])
+    my_kt_ens.extend(
+        [
+            ph for ph in get_keyphrases_sgrank(all_snips, idfs=idf)
+            if(check_answer(ph[0], quest['body']))
+        ]
+    )
+    ########################################
+    # pprint(Counter(my_kt).most_common(5))
+    # # pprint(Counter(my_ents).most_common(5))
+    # pprint(sorted(my_kt_ens, key= lambda x: x[1], reverse= True))
+    # print(20*'=')
+    ########################################
+    if (quest['type'] == 'list'):
+        quest['exact_answer'] = [[kt[0]] for kt in my_kt_ens]
+    else:
+        quest['exact_answer'] = [kt[0] for kt in my_kt_ens]
+
+with open(f1, 'w') as of:
+    of.write(json.dumps(data, indent=4, sort_keys=True))
+    of.close()
