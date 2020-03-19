82c828bfd6cdef01b77a8720a35b686662c5f093
dvpappas <dvpappas89@gmail.com>
2020-03-19 21:39:16 +0200

    message

--- bioasq_2020/exact_answer/prepare_submition.py
+++ bioasq_2020/exact_answer/prepare_submition.py
@@ -1,53 +1,45 @@
 
+from pprint import pprint
 import json
 
 fpaths = [
     (
-        'C:/Users/dvpap/OneDrive/Desktop/bioasq8_submitted/bioasq8_batch1_system1_results.json',
-        'C:/Users/dvpap/OneDrive/Desktop/bioasq8_submitted factoid/system_1_jpdrmm_sgrank.json'
+        'C:\\Users\\dvpap\\OneDrive\\Desktop\\batch2_submit_files\\ir_results\\system1.json',
+        'C:\\Users\\dvpap\\OneDrive\\Desktop\\batch2_submit_files\\exact_answer\\system_3.json',
+        'C:\\Users\\dvpap\\OneDrive\\Desktop\\system_1.json'
     ),
     (
-        'C:/Users/dvpap/OneDrive/Desktop/bioasq8_submitted/bioasq8_batch1_system3_results.json',
-        'C:/Users/dvpap/OneDrive/Desktop/bioasq8_submitted factoid/system_3_jpdrmm_bertreader.json'
-    ),
-    (
-        'C:/Users/dvpap/OneDrive/Desktop/bioasq8_submitted/bioasq8_batch1_system4_results.json',
-        'C:/Users/dvpap/OneDrive/Desktop/bioasq8_submitted factoid/system_4_gold_bertreader.json'
-    ),
-    (
-        'C:/Users/dvpap/OneDrive/Desktop/bioasq8_submitted/bioasq8_batch1_system5_results.json',
-        'C:/Users/dvpap/OneDrive/Desktop/bioasq8_submitted factoid/system_5_gold_sgrank.json'
+        'C:\\Users\\dvpap\\OneDrive\\Desktop\\batch2_submit_files\\BioASQ-task8bPhaseB-testset2.json',
+        # 'C:\\Users\\dvpap\\OneDrive\\Desktop\\batch2_submit_files\\exact_answer\\system_3.json',
+        'C:\\Users\\dvpap\\OneDrive\\Desktop\\system_5.json'
     )
 ]
 
-c = 0
-for f1, f2 in fpaths:
-    c += 1
-    #####################################################################
-    d2 = json.load(open(f2))
-    tt = dict(
-        (
-            q['id'],
-            (
-                q['type'],
-                q['exact_answer'] if 'exact_answer' in q else None
-            )
-        )
-        for q in d2['questions']
-    )
-    #####################################################################
+fff = 'C:\\Users\\dvpap\\OneDrive\\Desktop\\batch2_submit_files\\BioASQ-task8bPhaseB-testset2.json'
+d2  = json.load(open(fff))
+tt  = dict((q['id'], q['type']) for q in d2['questions'])
+# pprint(tt)
+
+for f1, f2, opath in fpaths:
+    print(f1)
+    print(f2)
     d1 = json.load(open(f1))
-    for quest in d1['questions']:
-        if(quest['id'] in tt):
-            quest['type'] = tt[quest['id']][0]
-            if(tt[quest['id']][1] is not None):
-                quest['exact_answer'] = tt[quest['id']][1]
-            if(quest['type'] == 'yesno'):
-                quest['exact_answer'] = 'yes'
-            if(quest['type'] == 'summary'):
-                quest['ideal_answer'] = [' '.join(s['text'] for s in quest['snippets'])]
-    #####################################################################
-    with open('C:/Users/dvpap/OneDrive/Desktop/system_{}.json'.format(c), 'w') as of:
+    d2 = json.load(open(f2))
+    # pprint(d2)
+    for quest, res in zip(d1['questions'], d2):
+        #######################################
+        quest['type'] = tt[quest['id']]
+        #######################################
+        if(quest['type'] == 'factoid'):
+            quest['exact_answer'] = [t['text'] for t in res['ngrams']]
+        if(quest['type'] == 'list'):
+            quest['exact_answer'] = [[t['text']] for t in res['ngrams']]
+        if(quest['type'] == 'yesno'):
+            quest['exact_answer'] = 'yes'
+        if(quest['type'] == 'summary'):
+            quest['ideal_answer'] = [' '.join(s['text'] for s in quest['snippets'])]
+        #######################################
+    with open(opath, 'w') as of:
         of.write(json.dumps(d1, indent=4, sort_keys=True))
         of.close()
 
