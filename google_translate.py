708e228d0af1ddc881d0a1a7803c9a5bafde928b
dvpappas <dvpappas89@gmail.com>
2020-03-26 15:12:15 +0200

    message

---
+++ google_translate.py
@@ -0,0 +1,27 @@
+
+# pip install googletrans
+
+import googletrans
+from googletrans import Translator
+print(googletrans.LANGUAGES)
+translator  = Translator()
+
+question    = "Is modified vaccinia Ankara effective for smallpox?"
+print(question)
+
+
+def get_different(question, lang='fr'):
+    result      = translator.translate(question, src='en', dest=lang)
+    transl_quest = result.text
+    result      = translator.translate(transl_quest, src=lang, dest='en')
+    return result.text
+
+for  lang in googletrans.LANGUAGES.keys():
+    trans = get_different(question, lang=lang)
+    if(trans != question):
+        print(lang)
+        print(trans)
+
+
+
+
