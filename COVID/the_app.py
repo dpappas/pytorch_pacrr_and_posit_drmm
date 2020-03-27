a4df88807c6c355546cbb27a7d49f31899fa123e
dvpappas <dvpappas89@gmail.com>
2020-03-27 13:40:46 +0200

    message

--- COVID/the_app.py
+++ COVID/the_app.py
@@ -1,8 +1,6 @@
 
 from retrieve_and_rerank import retrieve_given_question, pprint
-
 quest   = 'A pneumonia outbreak associated with a new coronavirus of probable bat origin'
-
 results = retrieve_given_question(quest, n=100, max_year=2021)
 pprint(results[:2])
 
