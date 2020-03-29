13dae36f214dc01ed65f1bf737927d0882d523bb
dvpappas <dvpappas89@gmail.com>
2020-03-29 13:42:39 +0300

    message

--- COVID/project_new_w2v_embeds.py
+++ COVID/project_new_w2v_embeds.py
@@ -1,10 +1,25 @@
 
 
 from gensim.models import KeyedVectors
+from gensim.models import Word2Vec
+from gensim.models import translation_matrix
+from gensim.models import BackMappingTranslationMatrix
 
-w2v_bin_path_old                = '/home/dpappas/COVID/COVID/pubmed2018_w2v_30D.bin'
-w2v_bin_path_new                = '/home/dpappas/COVID/covid_19_w2v_embeds_30.model'
-wv_old = KeyedVectors.load_word2vec_format(w2v_bin_path_old, binary=True)
-wv_new = KeyedVectors.load_word2vec_format(w2v_bin_path_new, binary=True)
+w2v_bin_path_old    = '/home/dpappas/COVID/COVID/pubmed2018_w2v_30D.bin'
+w2v_bin_path_new    = '/home/dpappas/COVID/covid_19_w2v_embeds_30.model'
+wv_old              = KeyedVectors.load_word2vec_format(w2v_bin_path_old, binary=True)
+wv_new              = Word2Vec.load(w2v_bin_path_new)
+
+common_tokens       = set(wv_old.vocab.keys()).intersection(set(wv_new.wv.vocab.keys()))
+common_tokens       = [ (tok, tok) for tok in common_tokens]
+
+transmat = translation_matrix.TranslationMatrix(wv_new.wv, wv_old, common_tokens)
+transmat.train(common_tokens)
+
+# transmat.apply_transmat(transmat.source_space)
+
+transmat.translate('covid-19')
+
+
 
 
