7b7d4dd65daf3281ab9fde340dc65a1e1f897aeb
dvpappas <dvpappas89@gmail.com>
2020-04-01 21:02:32 +0300

    message

--- bioasq_2020/IR/combine_systems_1and2.py
+++ bioasq_2020/IR/combine_systems_1and2.py
@@ -2,12 +2,11 @@
 import json
 from pprint import pprint
 
-d1 = json.load(open('C:\\Users\\dvpap\\OneDrive\\Desktop\\batch3_submit_files\\ir_results\\system1_output_b3\\v3 test_emit_bioasq.json'))
-d2 = json.load(open('C:\\Users\\dvpap\\OneDrive\\Desktop\\batch3_submit_files\\ir_results\\system2_output_b3\\v3 test_emit_bioasq.json'))
+d1 = json.load(open('C:\\Users\\dvpap\\OneDrive\\Desktop\\batch3_submit_files\\ir_results\\system1_output_b3\\v3 test_data_for_revision.json'))
 
-for q in d1['questions']:
-    pprint(q)
+for qid, data in d1.items():
+    pprint(data)
     exit()
 
-
+d2 = json.load(open('C:\\Users\\dvpap\\OneDrive\\Desktop\\batch3_submit_files\\ir_results\\system2_output_b3\\v3 test_emit_bioasq.json'))
 
