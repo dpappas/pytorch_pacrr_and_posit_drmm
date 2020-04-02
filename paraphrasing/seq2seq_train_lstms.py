a5f42b4a607e592405415d6eda4c93566de88c51
dvpappas <dvpappas89@gmail.com>
2020-04-02 16:31:55 +0300

    message

--- paraphrasing/seq2seq_train_lstms.py
+++ paraphrasing/seq2seq_train_lstms.py
@@ -138,17 +138,12 @@ class Encoder(nn.Module):
         return src
 
 class EncoderLayer(nn.Module):
-    def __init__(self,
-                 hid_dim,
-                 n_heads,
-                 pf_dim,
-                 dropout,
-                 device):
+    def __init__(self, hid_dim, n_heads, pf_dim, dropout, device):
         super().__init__()
-        self.layer_norm = nn.LayerNorm(hid_dim)
-        self.self_attention = MultiHeadAttentionLayer(hid_dim, n_heads, dropout, device)
-        self.positionwise_feedforward = PositionwiseFeedforwardLayer(hid_dim, pf_dim, dropout)
-        self.dropout = nn.Dropout(dropout)
+        self.layer_norm                 = nn.LayerNorm(hid_dim)
+        self.self_attention             = MultiHeadAttentionLayer(hid_dim, n_heads, dropout, device)
+        self.positionwise_feedforward   = PositionwiseFeedforwardLayer(hid_dim, pf_dim, dropout)
+        self.dropout                    = nn.Dropout(dropout)
     def forward(self, src, src_mask):
         # src = [batch size, src len, hid dim]
         # src_mask = [batch size, src len]
