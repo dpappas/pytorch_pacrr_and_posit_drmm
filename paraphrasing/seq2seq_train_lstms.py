ee9d713cff5ea37517b5e7991a50281d8ffa7f58
dvpappas <dvpappas89@gmail.com>
2020-04-02 16:49:25 +0300

    message

--- paraphrasing/seq2seq_train_lstms.py
+++ paraphrasing/seq2seq_train_lstms.py
@@ -69,19 +69,19 @@ def train(model, iterator, optimizer, criterion, clip):
         src = batch.src
         trg = batch.trg
         optimizer.zero_grad()
-        output, _ = model(src, trg[:, :-1])
+        output, _   = model(src, trg[:, :-1])
         # output = [batch size, trg len - 1, output dim]
         # trg = [batch size, trg len]
-        output_dim = output.shape[-1]
-        output = output.contiguous().view(-1, output_dim)
-        trg = trg[:, 1:].contiguous().view(-1)
+        output_dim  = output.shape[-1]
+        output      = output.contiguous().view(-1, output_dim)
+        trg         = trg[:, 1:].contiguous().view(-1)
         # output = [batch size * trg len - 1, output dim]
         # trg = [batch size * trg len - 1]
-        loss = criterion(output, trg)
+        loss        = criterion(output, trg)
         loss.backward()
         torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
         optimizer.step()
-        epoch_loss += loss.item()
+        epoch_loss  += loss.item()
     return epoch_loss / len(iterator)
 
 def evaluate(model, iterator, criterion):
