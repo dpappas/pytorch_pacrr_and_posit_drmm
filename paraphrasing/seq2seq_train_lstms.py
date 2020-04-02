465fbb8e3e0c092875c2c24967cebe2821eecc45
dvpappas <dvpappas89@gmail.com>
2020-04-02 16:28:23 +0300

    message

--- paraphrasing/seq2seq_train_lstms.py
+++ paraphrasing/seq2seq_train_lstms.py
@@ -118,16 +118,11 @@ class Encoder(nn.Module):
     def __init__(self, input_dim, hid_dim, n_layers, n_heads, pf_dim, dropout, device, max_length=150):
         super().__init__()
         self.device = device
-        self.tok_embedding = nn.Embedding(input_dim, hid_dim)
-        self.pos_embedding = nn.Embedding(max_length, hid_dim)
-        self.layers = nn.ModuleList([EncoderLayer(hid_dim,
-                                                  n_heads,
-                                                  pf_dim,
-                                                  dropout,
-                                                  device)
-                                     for _ in range(n_layers)])
-        self.dropout = nn.Dropout(dropout)
-        self.scale = torch.sqrt(torch.FloatTensor([hid_dim])).to(device)
+        self.tok_embedding  = nn.Embedding(input_dim, hid_dim)
+        self.pos_embedding  = nn.Embedding(max_length, hid_dim)
+        self.layers         = nn.ModuleList([EncoderLayer(hid_dim, n_heads, pf_dim, dropout, device) for _ in range(n_layers)])
+        self.dropout        = nn.Dropout(dropout)
+        self.scale          = torch.sqrt(torch.FloatTensor([hid_dim])).to(device)
     def forward(self, src, src_mask):
         # src = [batch size, src len]
         # src_mask = [batch size, src len]
@@ -135,7 +130,7 @@ class Encoder(nn.Module):
         src_len     = src.shape[1]
         pos         = torch.arange(0, src_len).unsqueeze(0).repeat(batch_size, 1).to(self.device)
         # pos = [batch size, src len]
-        src = self.dropout((self.tok_embedding(src) * self.scale) + self.pos_embedding(pos))
+        src         = self.dropout((self.tok_embedding(src) * self.scale) + self.pos_embedding(pos))
         # src = [batch size, src len, hid dim]
         for layer in self.layers:
             src = layer(src, src_mask)
