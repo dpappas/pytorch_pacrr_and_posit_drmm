9e61f6c6e522b833f98c5ab41380fae673a63f2b
dvpappas <dvpappas89@gmail.com>
2020-04-03 22:14:22 +0300

    message

--- paraphrasing/my_data_handling.py
+++ paraphrasing/my_data_handling.py
@@ -6,13 +6,13 @@ from collections import Counter
 bioclean = lambda t: re.sub('[.,?;*!%^&_+():-\[\]{}]', '', t.replace('"', '').replace('/', '').replace('\\', '').replace("'", '').strip().lower())
 
 class DataHandler:
-   def __init__(self, data_path= '/home/dpappas/quora_duplicate_questions.tsv', occur_thresh=5):
+   def __init__(self, data_path= '/home/dpappas/quora_duplicate_questions.tsv', occur_thresh=3):
        self.to_text, self.from_text = [], []
        self.occur_thresh            = occur_thresh
        self.vocab                   = Counter()
        with open(data_path, 'rt', encoding='utf8') as tsvin:
             tsvin = csv.reader(tsvin, delimiter='\t')
-            for row in tqdm(tsvin, total=404291):
+            for row in tqdm(tsvin, total=404291, desc='Reading file'):
                 ################################################
                 text1 = bioclean(row[3])
                 text2 = bioclean(row[4])
@@ -33,15 +33,15 @@ class DataHandler:
        self.vocab                   = sorted(
            [
                word
-               for word in self.vocab
+               for word in tqdm(self.vocab, desc='Building VOCAB')
                if(self.vocab[word]>=self.occur_thresh)
            ],
            key= lambda x: self.vocab[x]
        )
        self.vocab                   = ['<PAD>', '<UNK>', '<SOS>', '<EOS>'] + self.vocab
        self.stoi                    = dict(enumerate(self.vocab))
-       self.itos                    = dict((v, k) for k,v in self.stoi)
-       print(len(self.vocab))
+       self.itos                    = dict((v, k) for k,v in self.stoi.items())
+       print('Kept {} total words'.format(len(self.vocab)))
    def displayEmployee(self):
       print("Name : ", self.name,  ", Salary: ", self.salary)
 
