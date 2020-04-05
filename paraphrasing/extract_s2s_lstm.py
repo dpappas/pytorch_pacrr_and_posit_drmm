b99b1c21c78c3002ea8f5b5cc8a97136e749094a
dvpappas <dvpappas89@gmail.com>
2020-04-06 02:42:49 +0300

    message

--- paraphrasing/extract_s2s_lstm.py
+++ paraphrasing/extract_s2s_lstm.py
@@ -1,3 +1,29 @@
 
+import torch, re, random, spacy, time, pickle
+import torch.nn as nn
+import numpy as np
+from tqdm import tqdm
+from torch import optim
+import torch.nn.functional as F
+from torch import FloatTensor as FT
+from my_data_handling import DataHandler
+from pprint import pprint
+
+my_seed = 1989
+random.seed(my_seed)
+torch.manual_seed(my_seed)
+
+bioclean = lambda t: re.sub('[.,?;*!%^&_+():-\[\]{}]', '', t.replace('"', '').replace('/', '').replace('\\', '').replace("'", '').strip().lower()).split()
+
+######################################################################################################
+use_cuda    = torch.cuda.is_available()
+device      = torch.device("cuda") if(use_cuda) else torch.device("cpu")
+######################################################################################################
+en          = spacy.load('en_core_web_sm')
+
+data_path       = '/home/dpappas/quora_duplicate_questions.tsv'
+data_handler    = DataHandler(data_path)
+data_handler.load_model('datahandler_model.p')
+
 
 
