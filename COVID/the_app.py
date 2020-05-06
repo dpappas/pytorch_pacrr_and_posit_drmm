3b606df205b06b60936f4064a63e2cc3a3c38a42
dvpappas <dvpappas89@gmail.com>
2020-05-06 15:36:15 +0300

    message

--- COVID/the_app.py
+++ COVID/the_app.py
@@ -276,4 +276,4 @@ def submit_question():
 if __name__ == '__main__':
     # app.run(port=5000, debug=True)
     # app.add_url_rule('/favicon.ico', redirect_to=url_for('static', filename='aueb_favicon.png'))
-    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False, ssl_context='adhoc')
