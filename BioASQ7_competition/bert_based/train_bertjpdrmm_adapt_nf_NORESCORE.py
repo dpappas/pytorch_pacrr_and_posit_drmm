641aa2a615a1bd5c56e09b72364014abee758a78
dvpappas <dvpappas89@gmail.com>
2020-06-18 16:11:23 +0300

    message

--- BioASQ7_competition/bert_based/train_bertjpdrmm_adapt_nf_NORESCORE.py
+++ BioASQ7_competition/bert_based/train_bertjpdrmm_adapt_nf_NORESCORE.py
@@ -1143,7 +1143,7 @@ def train_one(epoch, bioasq6_data, two_losses, use_sent_tokenizer):
     start_time = time.time()
     pbar = tqdm(
         iterable= train_data_step2(train_instances, train_docs, bioasq6_data, use_sent_tokenizer),
-        total   = 17850, #9684, # 378,
+        total   = 14295, #17850, #9684, # 378,
         ascii   = True
     )
     for datum in pbar:
