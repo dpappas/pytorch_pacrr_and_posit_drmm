810efc619aa512702f973af5c389407d0af2d730
dvpappas <dvpappas89@gmail.com>
2020-06-27 16:25:39 +0300

    message

--- BioASQ7_competition/statistical_significance/join_all.py
+++ BioASQ7_competition/statistical_significance/join_all.py
@@ -38,7 +38,6 @@ java -Xmx10G -cp '/home/dpappas/bioasq_all/dist/my_bioasq_eval_2.jar' evaluation
 "/home/dpappas/ablation_1111111_0p01_0_bioasq_jpdrmm_2L_0p01_run_0/all_res_12345.json" \
 | grep "^MAP documents:\|^MAP snippets:"
 
-
 java -Xmx10G -cp '/home/dpappas/bioasq_all/dist/my_bioasq_eval_2.jar' evaluation.EvaluatorTask1b -phaseA -e 5 \
 "/home/dpappas/bioasq_all/bioasq7/data/test_batch_12345/BioASQ-task7bPhaseB-testset12345" \
 "/media/dpappas/dpappas_data/models_out/bioasq7_bertjpdrmadaptnf_maxsentasdoc_adapt_frozen_run_0_WL_0.1_1.0/all_res_12345.json" \
