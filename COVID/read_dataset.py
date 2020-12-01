7f2aa4277dc51737adce3e33a0db5887687abaa0
dvpappas <dvpappas89@gmail.com>
2020-12-01 19:14:52 +0200

    message

---
+++ COVID/read_dataset.py
@@ -0,0 +1,23 @@
+
+
+from tqdm import tqdm
+from pprint import pprint
+import tarfile, json
+targz_path = "/media/dpappas/dpappas_data/CORD_allenai_datasets/2020-11-29/document_parses.tar.gz"
+tar = tarfile.open(targz_path, "r:gz")
+total_items         = 0
+total_paragraphs    = 0
+for member in tqdm(tar, total=234500):
+    # print(member.name)
+    total_items += 1
+    f = tar.extractfile(member)
+    if f is not None:
+        d = json.loads(f.read())
+        if('PMC' in member.name):
+            total_paragraphs += 1
+            total_paragraphs += len(d['body_text'])
+        else:
+            total_paragraphs += 1
+            total_paragraphs += len(d['body_text'])
+
+
