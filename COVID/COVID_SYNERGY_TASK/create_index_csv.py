7cb8520814edfe08ce88236a186df178017e0483
dvpappas <dvpappas89@gmail.com>
2020-12-14 20:51:38 +0200

    message

---
+++ COVID/COVID_SYNERGY_TASK/create_index_csv.py
@@ -0,0 +1,42 @@
+
+from elasticsearch import Elasticsearch
+from pprint import pprint
+
+index       = 'allenai_covid_index_2020_11_29_csv'
+
+elastic_con = Elasticsearch(['127.0.0.1:9200'], verify_certs=True, timeout=150, max_retries=10, retry_on_timeout=True)
+elastic_con.indices.delete(index=index, ignore=[400,404])
+
+mapping     = {
+    "settings": {
+     "index": {
+      "similarity": {
+        "my_similarity": {
+            "type"              : "BM25",
+            "k1"                : 0.6,
+            "b"                 : 0.3,
+            'discount_overlaps' : True
+        }
+      }
+     }
+    },
+    "mappings":{
+        "properties":{
+            'joint_text'    : {
+                "type"              : "text",
+                "analyzer"          : "standard",
+                "similarity"        : "my_similarity",
+            },
+            'cord_uid'      : {"type": "keyword"},
+            'doi'           : {"type": "keyword"},
+            'pubmed_id'     : {"type": "keyword"},
+            'url'           : {"type": "keyword"},
+            'publish_time'       : {
+                "type"      : "date",
+                "format"    : "yyyy-MM-dd||yyyy"
+            },
+        }
+    }
+}
+
+pprint(elastic_con.indices.create(index = index, ignore=400, body=mapping))
---
+++ COVID/COVID_SYNERGY_TASK/read_dataset_CSV.py
@@ -0,0 +1,60 @@
+
+from tqdm import tqdm
+from pprint import pprint
+import tarfile, json
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
+import csv, os
+from csv import reader
+
+def create_an_action(elk_dato, id):
+    elk_dato['_id']      = id
+    elk_dato['_op_type'] = 'index'
+    elk_dato['_index']   = index
+    return elk_dato
+
+def upload_to_elk():
+    global actions
+    flag = True
+    while (flag):
+        try:
+            result = bulk(es, iter(actions))
+            pprint(result)
+            flag = False
+        except Exception as e:
+            if ('ConnectionTimeout' in str(e) or 'rejected execution of' in str(e)):
+                print('Retrying')
+            else:
+                print(e)
+                flag = False
+    actions         = []
+
+index       = 'allenai_covid_index_2020_11_29_csv'
+es          = Elasticsearch(['127.0.0.1:9200'], verify_certs=True, timeout=150, max_retries=10, retry_on_timeout=True)
+
+total_items         = 0
+total_paragraphs    = 0
+database_instances  = []
+actions             = []
+csv_path            = '/media/dpappas/dpappas_data/CORD_allenai_datasets/2020-11-29/metadata.csv'
+with open(csv_path, 'r') as read_obj:
+    csv_reader = reader(read_obj)
+    for row in tqdm(csv_reader, total=368619):
+        (
+            cord_uid, sha, source_x, title, doi, pmcid, pubmed_id, license, abstract, publish_time, authors, journal,
+            mag_id, who_covidence_id, arxiv_id, pdf_json_files, pmc_json_files, url, s2_id
+        ) = row
+        datum       = {
+            '_id'           : doi,
+            'joint_text'    : title + '--------------------' + abstract,
+            'cord_uid'      : cord_uid,
+            'doi'           : doi,
+            'pubmed_id'     : pubmed_id,
+            'url'           : url,
+            'publish_time'  : publish_time
+        }
+        actions.append(create_an_action(datum, datum['_id']))
+        if(len(actions) == 50):
+            upload_to_elk()
+
+upload_to_elk()
