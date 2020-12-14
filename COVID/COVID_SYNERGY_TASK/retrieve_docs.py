34c11e7047969f5a6afe84acee87b4a0ffaef469
dvpappas <dvpappas89@gmail.com>
2020-12-14 15:01:32 +0200

    message

--- COVID/COVID_SYNERGY_TASK/retrieve_docs.py
+++ COVID/COVID_SYNERGY_TASK/retrieve_docs.py
@@ -110,84 +110,8 @@ es      = Elasticsearch(['127.0.0.1:9200'], verify_certs=True, timeout=150, max_
 
 #####################################################################################
 
-def get_first_n_1(qtext, n, section=None, max_year=None, exclude_pmids=None, must_include=None, must_exclude=None):
-    tokenized_body  = bioclean_mod(qtext)
-    tokenized_body  = [t for t in tokenized_body if t not in stopwords]
-    question        = ' '.join(tokenized_body)
-    print(question)
-    if(question is None or len(question.strip())==0):
-        question = 'the'
-    ################################################
-    bod             = {
-        "size": n,
-        "query": {
-            "bool": {
-                "must"                  : [],
-                "should"                : [{"match": {"joint_text": {"query": question, "boost": 1}}}],
-                "minimum_should_match"  : 1
-            }
-        }
-    }
-    if(max_year):
-        bod["query"]["bool"]["must"].append(
-            {"range": {"DateCompleted": {"gte": "1800", "lte": str(max_year), "format": "dd/MM/yyyy||yyyy"}}}
-        )
-    if(must_include):
-        ttemp = {"bool": {"should": []}}
-        for phr in must_include:
-            ttemp["bool"]["should"].append({"match_phrase": {"joint_text": {"query": phr}}})
-        bod["query"]["bool"]["must"].append(ttemp)
-    if(must_exclude):
-        pass
-    if(exclude_pmids):
-        bod["query"]["bool"]["must_not"] = [{"ids": {"values": exclude_pmids}}]
-    if(section is not None):
-        bod['query']['bool']['must'].append(
-            {"match": {"section": {"query": section, "boost": 1}}}
-        )
-    res = es.search(index=doc_index, body=bod, request_timeout=120)
-    ################################################
-    results = res['hits']['hits']
-    #######################################################
-    temp_1 = {
-        'num_rel': 0,
-        'num_rel_ret': 0,
-        'num_ret': -1,
-        'query_id': 1234567890,
-        'query_text': qtext,
-        'relevant_documents': [],
-        'retrieved_documents': []
-    }
-    #######################################################
-    all_scores = [res['_score'] for res in results]
-    # print(all_scores)
-    if(len(all_scores)==0):
-        return temp_1
-    scaler = StandardScaler().fit(np.array(all_scores).reshape(-1, 1))
-    temp_1['num_ret'] = len(all_scores)
-    #######################################################
-    for res, rank in zip(results, range(1, len(results) + 1)):
-        temp_1['retrieved_documents'].append({
-            'bm25_score'        : res['_score'],
-            'doc_id'            : res['_id'],
-            'is_relevant'       : False,
-            'norm_bm25_score'   : scaler.transform([[res['_score']]])[0][0],
-            'rank'              : rank,
-            'doc'               : {
-                'title'             : res['_source']['joint_text'].split('--------------------', 1)[0].strip(),
-                'abstractText'      : res['_source']['joint_text'].split('--------------------', 1)[1].strip(),
-                # 'doi'               : res['_source']['doi'],
-                # 'pmcid'             : res['_source']['pmcid'],
-                # 'pmid'              : res['_source']['pmid'],
-                'pmid'              : res['_id'],
-                # 'section'           : res['_source']['section'],
-                'date'     : res['_source']['DateCompleted']
-            }
-        })
-    return temp_1
-
 def get_first_n(qtext, n, exclude_pmids=None):
-    results = retrieve_some_docs(qtext, n=n, exclude_pmids=exclude_pmids)
+    results = retrieve_some_docs(qtext, n=n, exclude_pmids=exclude_pmids)['hits']['hits']
     #######################################################
     temp_1 = {
         'num_rel': 0,
@@ -214,18 +138,20 @@ def get_first_n(qtext, n, exclude_pmids=None):
             'norm_bm25_score'   : scaler.transform([[res['_score']]])[0][0],
             'rank'              : rank,
             'doc'               : {
-                'title'             : res['_source']['joint_text'].split('--------------------', 1)[0].strip(),
-                'abstractText'      : res['_source']['joint_text'].split('--------------------', 1)[1].strip(),
-                # 'doi'               : res['_source']['doi'],
-                # 'pmcid'             : res['_source']['pmcid'],
-                # 'pmid'              : res['_source']['pmid'],
+                'title'             : 'title',
+                'abstractText'      : res['_source']['section_text'],
                 'pmid'              : res['_id'],
-                # 'section'           : res['_source']['section'],
-                'date'     : res['_source']['DateCompleted']
+                'date'              : res['_source']['DateCompleted']
             }
         })
     return temp_1
 
 
+if __name__ == '__main__':
+    qtext   = 'Which diagnostic test is approved for coronavirus infection screening?'
+    res     = get_first_n(qtext, n=100, exclude_pmids=None)
+    pprint(res)
 
-#####################################################################################
+
+
+
