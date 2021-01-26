e8d95621644790d66c1c9de94e9c5e55705b1354
dvpappas <dvpappas89@gmail.com>
2021-01-26 15:33:27 +0200

    message

--- COVID/COVID_SYNERGY_TASK/retrieve_docs.py
+++ COVID/COVID_SYNERGY_TASK/retrieve_docs.py
@@ -113,7 +113,7 @@ def retrieve_some_docs(qtext, n=100, exclude_pmids=None):
 #####################################################################################
 
 # index   = 'allenai_covid_index_2020_11_29_csv'
-index   = 'allenai_covid_index_2021_01_10_csv'
+index   = 'allenai_covid_index_2021_01_25_csv'
 es      = Elasticsearch(['127.0.0.1:9200'], verify_certs=True, timeout=150, max_retries=10, retry_on_timeout=True)
 
 #####################################################################################
