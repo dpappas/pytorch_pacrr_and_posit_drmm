fef2c04778c1e260b76ddfb9dac5b8d969782b59
dvpappas <dvpappas89@gmail.com>
2021-01-29 22:49:08 +0200

    message

--- BioASQ7_competition/recalls_using_models.py
+++ BioASQ7_competition/recalls_using_models.py
@@ -3,18 +3,25 @@ import pickle, json
 import numpy as np
 
 for batch_no in range(1,6):
-    d 	= pickle.load(open('/home/dpappas/bioasq_all/bioasq7/data/test_batch_{}/bioasq7_bm25_top100/bioasq7_bm25_top100.test.pkl'.format(batch_no), 'rb'))
-    d2 	= json.load(open('bioasq_all/bioasq7/document_results/test_batch_{}/jpdrmm.json'.format(batch_no,batch_no)))
+    # d 	= json.load(open('bioasq_all/bioasq7/document_results/test_batch_{}/JBERT.json'.format(batch_no,batch_no)))
+    # d 	= json.load(open('/media/dpappas/dpappas_data/models_out/bioasq7_bertjpdrmadaptnf_adapt_run_frozen/batch_{}/v3 test_emit_bioasq.json'.format(batch_no)))
+    d = json.load(open('/media/dpappas/dpappas_data/models_out/bioasq7_bertjpdrmadaptnf_adapt_frozen_run_0_WL_0.1/batch_{}/v3 test_emit_bioasq.json'.format(batch_no)))
+    d2 = json.load(open('bioasq_all/bioasq7/data/test_batch_{}/BioASQ-task7bPhaseB-testset{}'.format(batch_no, batch_no)))
     id2rel = {}
     for q in d2['questions']:
         id2rel[q['id']] = [doc.replace('http://www.ncbi.nlm.nih.gov/pubmed/', '') for doc in q['documents']]
+    ################################################################################################################################
     recalls = []
-    for quer in d['queries']:
-        retr_ids = [doc['doc_id'] for doc in quer['retrieved_documents'][:10] if doc['doc_id'] in id2rel[quer['query_id']]]
-        recalls.append(float(len(retr_ids))/float(len(id2rel[quer['query_id']])))
+    for quer in d['questions']:
+        quer['documents'] = [doc.replace('http://www.ncbi.nlm.nih.gov/pubmed/', '') for doc in quer['documents']]
+        retr_ids    = [
+            doc for doc in quer['documents'][:10]
+            if doc in id2rel[quer['id']]
+        ]
+        found 		= float(len(retr_ids))
+        tot 		= float(len(id2rel[quer['id']]))
+        recalls.append(found/tot)
     print(np.average(recalls))
-    # break
-
-
+    ################################################################################################################################
 
 
