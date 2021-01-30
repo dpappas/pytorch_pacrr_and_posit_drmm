366dc64d1f7587844daee3b351a1a67f82e35f89
dvpappas <dvpappas89@gmail.com>
2021-01-30 15:47:32 +0200

    message

--- BioASQ7_competition/best_recall_using_bm25.py
+++ BioASQ7_competition/best_recall_using_bm25.py
@@ -137,3 +137,49 @@ for batch_no in range(1,6):
     ##################################################################################################
     print(np.average(recalls))
     print(np.average(recalls_snip))
+    print(30 *'-')
+    ##################################################################################################
+
+
+for batch_no in range(1,6):
+    le_docs = pickle.load(open('/home/dpappas/bioasq_all/bioasq7/data/test_batch_{}/bioasq7_bm25_top100/bioasq7_bm25_docset_top100.test.pkl'.format(batch_no),'rb'))
+    d   = pickle.load(open('/home/dpappas/bioasq_all/bioasq7/data/test_batch_{}/bioasq7_bm25_top100/bioasq7_bm25_top100.test.pkl'.format(batch_no), 'rb'))
+    d2  = json.load(open('bioasq_all/bioasq7/data/test_batch_{}/BioASQ-task7bPhaseB-testset{}'.format(batch_no,batch_no)))
+    ##################################################################################################
+    id2rel      = {}
+    id2relsnip  = {}
+    for q in d2['questions']:
+        id2rel[q['id']]     = [doc.replace('http://www.ncbi.nlm.nih.gov/pubmed/', '') for doc in q['documents']]
+        id2relsnip[q['id']] = [bioclean(snip['text'].strip()) for snip in q['snippets']]
+    ##################################################################################################
+    recalls         = []
+    recalls_snip    = []
+    for quer in d['queries']:
+        retr_doc_ids    = [doc['doc_id'] for doc in quer['retrieved_documents'][:100]]
+        retr_ids        = [doc_id for doc_id in retr_doc_ids if doc_id in id2rel[quer['query_id']]]
+        found           = float(len(retr_ids))
+        if(found>10):
+            found = 10.0
+        tot             = float(len(id2rel[quer['query_id']]))
+        recalls.append(found/tot)
+        #################
+        all_sents       = []
+        for doc_id in retr_doc_ids:
+            all_sents.extend(sent_tokenize(le_docs[doc_id]['title']))
+            all_sents.extend(sent_tokenize(le_docs[doc_id]['abstractText']))
+        retr_snips          = list(set(all_sents))
+        #################
+        if(len(id2relsnip[quer['query_id']]) != 0):
+            retr_snips      = [t for t in retr_snips if snip_is_relevant(bioclean(t), id2relsnip[quer['query_id']])][:10]
+            found           = float(len(retr_snips))
+            if(found>10):
+                found = 10.0
+            tot             = float(len(id2relsnip[quer['query_id']]))
+            recalls_snip.append(found/tot)
+        #################
+    ##################################################################################################
+    print(np.average(recalls))
+    print(np.average(recalls_snip))
+    print(30 *'-')
+    ##################################################################################################
+
