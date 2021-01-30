3ad308c29ec156e502c5aebfab0ad3b5706b6dae
dvpappas <dvpappas89@gmail.com>
2021-01-30 13:02:19 +0200

    message

--- BioASQ7_competition/best_recall_using_bm25.py
+++ BioASQ7_competition/best_recall_using_bm25.py
@@ -20,25 +20,25 @@ def snip_is_relevant(one_sent, gold_snips):
     )
 
 for batch_no in range(1,6):
-	d 	= pickle.load(open('/home/dpappas/bioasq_all/bioasq7/data/test_batch_{}/bioasq7_bm25_top100/bioasq7_bm25_top100.test.pkl'.format(batch_no), 'rb'))
-	d2 	= json.load(open('bioasq_all/bioasq7/data/test_batch_{}/BioASQ-task7bPhaseB-testset{}'.format(batch_no,batch_no)))
-	##############
-	id2rel 		= {}
-	id2relsnip	= {}
-	for q in d2['questions']:
-		id2rel[q['id']] = [doc.replace('http://www.ncbi.nlm.nih.gov/pubmed/', '') for doc in q['documents']]
-	##############
-	recalls = []
-	# for quer in d['queries']:
-	# 	retr_ids 	= [doc['doc_id'] for doc in quer['retrieved_documents'][:10] if doc['doc_id'] in id2rel[quer['query_id']]]
-	# 	found 		= float(len(retr_ids))
-	# 	tot 		= float(len(id2rel[quer['query_id']]))
-	# 	recalls.append(found/tot)
-	for quer in d['queries']:
-		retr_ids 	= [doc['doc_id'] for doc in quer['retrieved_documents'][:100] if doc['doc_id'] in id2rel[quer['query_id']]]
-		found 		= float(len(retr_ids))
-		tot 		= float(len(id2rel[quer['query_id']]))
-		recalls.append(found/tot)
-	##############
-	print(np.average(recalls))
-
+    d     = pickle.load(open('/home/dpappas/bioasq_all/bioasq7/data/test_batch_{}/bioasq7_bm25_top100/bioasq7_bm25_top100.test.pkl'.format(batch_no), 'rb'))
+    d2     = json.load(open('bioasq_all/bioasq7/data/test_batch_{}/BioASQ-task7bPhaseB-testset{}'.format(batch_no,batch_no)))
+    ##############
+    id2rel         = {}
+    id2relsnip    = {}
+    for q in d2['questions']:
+        id2rel[q['id']] = [doc.replace('http://www.ncbi.nlm.nih.gov/pubmed/', '') for doc in q['documents']]
+        id2relsnip[q['id']] = [bioclean(snip['text'].strip()) for snip in q['snippets']]
+    ##############
+    recalls = []
+    # for quer in d['queries']:
+    #     retr_ids     = [doc['doc_id'] for doc in quer['retrieved_documents'][:10] if doc['doc_id'] in id2rel[quer['query_id']]]
+    #     found         = float(len(retr_ids))
+    #     tot         = float(len(id2rel[quer['query_id']]))
+    #     recalls.append(found/tot)
+    for quer in d['queries']:
+        retr_ids     = [doc['doc_id'] for doc in quer['retrieved_documents'][:100] if doc['doc_id'] in id2rel[quer['query_id']]]
+        found         = float(len(retr_ids))
+        tot         = float(len(id2rel[quer['query_id']]))
+        recalls.append(found/tot)
+    ##############
+    print(np.average(recalls))
