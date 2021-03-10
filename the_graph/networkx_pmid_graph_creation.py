cdd1a27b2e80e05fa1f4e73b92b27e844e5adc80
dvpappas <dvpappas89@gmail.com>
2021-03-10 16:25:11 +0200

    message

--- the_graph/networkx_pmid_graph_creation.py
+++ the_graph/networkx_pmid_graph_creation.py
@@ -7,10 +7,11 @@ from networkx.algorithms.shortest_paths.generic import shortest_path
 fpaths = [
     'C:\\Users\\dvpap\\Downloads\\citations_1900_1989.txt',
     'C:\\Users\\dvpap\\Downloads\\citations_1990_1999.txt',
+    'C:\\Users\\dvpap\\Downloads\\citations_2000_2004.txt',
+    # '',
     'C:\\Users\\dvpap\\Downloads\\citations_2010_2015.txt',
     'C:\\Users\\dvpap\\Downloads\\citations_2016_2018.txt',
-    'C:\\Users\\dvpap\\Downloads\\citations_2019_2021.txt',
-    'C:\\Users\\dvpap\\Downloads\\citations_2000_2004.txt'
+    'C:\\Users\\dvpap\\Downloads\\citations_2019_2021.txt'
 ]
 
 G   = nx.Graph()
@@ -25,4 +26,19 @@ for fpath in fpaths:
             continue
         G.add_edge(from_, to_)
 
-print(shortest_path(G, source='5335695', target='2503568', weight=None, method="dijkstra"))
+some_nodes = ['27806047', '15975141', '7713947', '17141428']
+
+import numpy as np
+
+dists = np.zeros((len(some_nodes), len(some_nodes)))
+for i in range(len(some_nodes)):
+    for j in range(i, len(some_nodes)):
+        try:
+            dists[i, j] = shortest_path(G, source=some_nodes[i], target=some_nodes[j], weight=None, method="dijkstra")
+        except Exception as e:
+            print(e)
+            dists[i, j] = 100000
+
+print(dists)
+
+
