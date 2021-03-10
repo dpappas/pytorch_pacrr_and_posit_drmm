83c7b907332f4d016337ddb93f7bf95971a8510e
dvpappas <dvpappas89@gmail.com>
2021-03-10 20:21:33 +0200

    message

--- the_graph/networkx_pmid_graph_creation.py
+++ the_graph/networkx_pmid_graph_creation.py
@@ -1,5 +1,5 @@
 
-import os
+import os, pickle
 import networkx as nx
 import numpy as np
 from tqdm import tqdm
@@ -22,26 +22,31 @@ fpaths = [
     'pmid2issn_2005_2009.txt',
     'pmid2issn_2010_2015.txt',
     'pmid2issn_2016_2018.txt',
-    #'pmid2issn_2019_2021.txt',
+    'pmid2issn_2019_2021.txt',
 ]
 
-G   = nx.Graph()
+save_path = '/media/dpappas/pubmed_pmid_issn_graph.nx'
 
-for fpath in fpaths:
-    print(fpath)
-    for l in tqdm(open(os.path.join(dir_path, fpath))):
-        # print((fpath, l.strip()))
-        try:
-            from_, to_ = l.strip().split(':', 1)
-            from_, to_ = from_.strip(), to_.strip()
-            if len(to_.strip()) == 0:
-                continue
-            G.add_node(from_)
-            G.add_node(to_)
-            G.add_edge(from_, to_)
-            G.add_edge(to_, from_)
-        except Exception as ex:
-            print(ex)
+if os.path.exists(save_path):
+    G = pickle.load(open(save_path, 'rb'))
+else:
+    G   = nx.Graph()
+    for fpath in fpaths:
+        print(fpath)
+        for l in tqdm(open(os.path.join(dir_path, fpath))):
+            # print((fpath, l.strip()))
+            try:
+                from_, to_ = l.strip().split(':', 1)
+                from_, to_ = from_.strip(), to_.strip()
+                if len(to_.strip()) == 0:
+                    continue
+                G.add_node(from_)
+                G.add_node(to_)
+                G.add_edge(from_, to_)
+                G.add_edge(to_, from_)
+            except Exception as ex:
+                print(ex)
+    pickle.dump(G, open(save_path, 'wb'))
 
 some_nodes = ['30587508', '24850885', '30047297', '31130811', '22621377', '21075122', '23870312', '29386389', '20237445', '22700207', '28122498', '29229824', '29146912', '24430003', '23527029', '25122230', '9760921', '21175058', '10483442', '23690948', '30074895', '29283327', '28950715', '25686628', '24524678', '12582577', '21480512', '30957403', '31622055', '32469335', '29226112', '23608633', '24987733', '17972643', '19449654', '29672266', '28499256', '8924901', '26981015', '21998654', '22713628', '27419175', '23816508', '18510017', '9720616', '26623746', '30774505', '20876996', '9869276', '27491753', '21659002', '21951598', '19493196', '27706109', '30736275', '29703496', '28009851', '22835152', '23226548', '31538676', '27087849', '24695515', '22022518', '23633469', '24273257', '32901512', '21112751', '21383140', '25410991', '29375639', '24178352', '30559756', '22983293', '31274421', '31271501', '27816677', '21698096', '24518360', '26945390', '28286698', '8892565', '26818000', '12587877', '27806047', '15975141', '7713947', '17141428', '11393615', '8099083', '32222657', '8489164', '12175947', '29479363', '3304661', '17912598', '12916667', '25472866', '27071261', '15074617', '1325443']
 print(len(some_nodes))
