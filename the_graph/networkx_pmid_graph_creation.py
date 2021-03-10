adaa981ba72316d051c4c6400b7979f89c51bb71
dvpappas <dvpappas89@gmail.com>
2021-03-10 16:13:57 +0200

    message

--- the_graph/networkx_pmid_graph_creation.py
+++ the_graph/networkx_pmid_graph_creation.py
@@ -4,8 +4,11 @@ import networkx as nx
 from networkx.algorithms.shortest_paths.generic import shortest_path
 
 fpaths = [
+    'C:\\Users\\dvpap\\Downloads\\citations_1900_1989.txt',
+    'C:\\Users\\dvpap\\Downloads\\citations_1990_1999.txt',
+    'C:\\Users\\dvpap\\Downloads\\citations_2010_2015.txt',
     'C:\\Users\\dvpap\\Downloads\\citations_2016_2018.txt',
-    'C:\\Users\\dvpap\\Downloads\\citations_1900_1989.txt'
+    'C:\\Users\\dvpap\\Downloads\\citations_2019_2021.txt',
 ]
 
 G   = nx.Graph()
@@ -14,7 +17,7 @@ for fpath in fpaths:
     for l in open(fpath):
         from_, to_ = l.strip().split(':')
         from_, to_ = from_.strip(), to_.strip()
-        if len(to_.strip() == 0):
+        if len(to_.strip()) == 0:
             continue
         print(from_, to_)
         G.add_edge(from_, to_)
