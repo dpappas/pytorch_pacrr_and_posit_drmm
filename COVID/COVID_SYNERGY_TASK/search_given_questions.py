176e7fe4e5909d6c726e5c7d106deb0411a8b76e
dvpappas <dvpappas89@gmail.com>
2021-05-27 12:24:51 +0300

    message

--- COVID/COVID_SYNERGY_TASK/search_given_questions.py
+++ COVID/COVID_SYNERGY_TASK/search_given_questions.py
@@ -73,12 +73,12 @@ for feedback_fpath in feedback_fpaths:
         # print(quest.keys())
         print(quest['body'])
         qtext_to_qid[quest['body']]     = quest['id']
-        #################################################
+        ##################################################################################################
         neg_snips                       = [sn['text'] for sn in quest['snippets'] if(not sn['golden'])]
         pos_snips                       = [sn['text'] for sn in quest['snippets'] if(sn['golden'])]
         neg_chunks                      = Counter([t.lower() for t in flattened([get_noun_chunks(sn) for sn in neg_snips])])
         pos_chunks                      = Counter([t.lower() for t in flattened([get_noun_chunks(sn) for sn in pos_snips])])
-        #################################################
+        ##################################################################################################
         try:
             qid_to_pos_chunks[quest['id']].update(pos_chunks)
         except:
@@ -87,7 +87,7 @@ for feedback_fpath in feedback_fpaths:
             qid_to_neg_chunks[quest['id']].update(neg_chunks)
         except:
             qid_to_neg_chunks[quest['id']]  = neg_chunks
-        #################################################
+        ##################################################################################################
         try:
             qid_to_pos_snips[quest['id']].extend(pos_snips)
         except:
@@ -96,32 +96,19 @@ for feedback_fpath in feedback_fpaths:
             qid_to_neg_snips[quest['id']].extend(neg_snips)
         except:
             qid_to_neg_snips[quest['id']]   = neg_snips
-        #################################################
+        ##################################################################################################
         pos_docids                      = [sn['id'] for sn in quest['documents'] if(sn['golden'])]
         try:
             qid_to_pos_docids[quest['id']].extend(pos_docids)
         except:
             qid_to_pos_docids[quest['id']]  = pos_docids
-        # es_res                          = es.mget(body={'ids': pos_docids}, index=doc_index)
-        # posdocs                         = [t['_source']['joint_text'].replace('--------------------',' ') for t in es_res['docs'] if '_source' in t]
-        # posdocs_chunks                  = Counter([t.lower() for t in flattened([get_keyphrases_sgrank(sn) for sn in posdocs])])
-        # #################################################
+        ##################################################################################################
         neg_docids                      = [sn['id'] for sn in quest['documents'] if(not sn['golden'])]
         try:
             qid_to_neg_docids[quest['id']].extend(neg_docids)
         except:
             qid_to_neg_docids[quest['id']] = neg_docids
-        # es_res                          = es.mget(body={'ids': neg_docids}, index=doc_index)
-        # negdocs                         = [t['_source']['joint_text'].replace('--------------------',' ') for t in es_res['docs'] if '_source' in t]
-        # negdocs_chunks                  = Counter([t.lower() for t in flattened([get_keyphrases_sgrank(sn) for sn in negdocs])])
-        # #################################################
-        # pprint(
-        #     [
-        #         c for c in posdocs_chunks
-        #         if c not in negdocs_chunks
-        #      ]
-        # )
-        # break
+        ##################################################################################################
 
 nonos           = ['sars', 'sars - cov', 'cov - 2', 'coronavirus', 'covid - 19', 'covid']
 nonos_2         = ['et al', 'et. al', '>']
